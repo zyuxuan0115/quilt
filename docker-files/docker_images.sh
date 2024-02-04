4b0442d9bb0791547898539c66ed3eec6556cffe
zyuxuan <zyuxuan@localhost>
2024-02-03 21:36:20 -0500

    update

--- docker-files/docker_images.sh
+++ docker-files/docker_images.sh
@@ -3,36 +3,17 @@
 ROOT_DIR=`realpath $(dirname $0)/..`
 echo $ROOT_DIR
 
-function build_nightcore {
-    rm -rf nightcore
-    git clone --recurse-submodules https://github.com/ut-osa/nightcore.git nightcore 
-    sudo docker build -t zyuxuan0115/nightcore:main \
-        -f $ROOT_DIR/docker-files/Dockerfile.nightcore \
-        $ROOT_DIR/docker-files/nightcore
+function build_gg_func {
+    sudo docker build --no-cache -t zyuxuan0115/google-func-frame:main \
+        -f $ROOT_DIR/docker-files/Dockerfile.google \
+        $ROOT_DIR/docker-files/tmp
 }
 
 function push_nightcore {
     sudo docker push zyuxuan0115/nightcore:main
 }
 
-function build_deathstarbench {
-    sudo docker build --no-cache -t zyuxuan0115/nightcore-socialnetwork:main \
-        -f $ROOT_DIR/docker-files/Dockerfile.socialnetwork \
-        --build-arg NUM_CPUS=$(nproc) \
-        $ROOT_DIR/DeathStarBench
-}
 
-function push_deathstarbench {
-    sudo docker push zyuxuan0115/nightcore-socialnetwork:main
-}
-
-function build_push_cpp_dep {
-    sudo docker build -t zyuxuan0115/cpp-microservice-deps:main \
-    	--build-arg NUM_CPUS=$(nproc) \
-        -f $ROOT_DIR/DeathStarBench/socialNetwork/docker/cpp-microservice-deps/Dockerfile \
-	$ROOT_DIR/DeathStarBench/socialNetwork/docker/cpp-microservice-deps
-    sudo docker push zyuxuan0115/cpp-microservice-deps:main
-}
 
 HIPSTERSHOP_SERVICES="frontend-api \
 productcatalogservice \
@@ -45,13 +26,11 @@ recommendationservice \
 cartservice"
 
 function build {
-    build_nightcore
-    build_deathstarbench
+    build_gg_func
 }
 
 function push {
-    push_nightcore
-    push_deathstarbench
+    push_gg_func
 }
 
 case "$1" in
@@ -60,8 +39,5 @@ build)
     ;;
 push)
     push
-    ;;
-pre)
-    build_prerequisite
     ;;
 esac
