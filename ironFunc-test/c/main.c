face54c4b09da72d320a24d5e5c966a0f895a727
zyuxuan <zyuxuan@localhost>
2024-02-05 21:02:48 -0500

    update

--- ironFunc-test/c/main.c
+++ ironFunc-test/c/main.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(void){
-  int             retval;
-  fd_set          rfds;
-  struct timeval  tv;
-  /* Watch stdin (fd 0) to see when it has input. */
-  FD_ZERO(&rfds);
-  FD_SET(STDIN_FILENO, &rfds);
-  /* Wait up to five seconds. */
-  //tv.tv_sec = 5;
-  //tv.tv_usec = 0;
-
-  retval = select(STDIN_FILENO+1, &rfds, NULL, NULL, NULL);
-
-  //retval = select(1, &rfds, NULL, NULL, &tv);
-  /* Don't rely on the value of tv now! */
-
-  if (retval == -1){
-    perror("select()");
-    exit(EXIT_SUCCESS);
-  }
-  else if (retval == STDIN_FILENO){
-    printf("Data is available now.\n");
-    exit(EXIT_SUCCESS);
-  }
-
+  char buf[1000];
+  memset(buf, 0, 1000);
+  ssize_t read_len = read(STDIN_FILENO, (void*)buf, 1000*sizeof(char));
+  printf("@@@ %s\n", buf);
+  return 0;
 }
