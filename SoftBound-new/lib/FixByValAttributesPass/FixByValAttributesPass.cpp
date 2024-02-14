0f276bddca11c1eb776701bcf28a5497f62915da
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-02-14 10:15:56 -0500

    update

--- SoftBound-new/lib/FixByValAttributesPass/FixByValAttributesPass.cpp
+++ SoftBound-new/lib/FixByValAttributesPass/FixByValAttributesPass.cpp
@@ -37,7 +37,80 @@
 // WITH THE SOFTWARE.
 //===---------------------------------------------------------------------===//
 
-#include "llvm/Transforms/SoftBoundCETS/FixByValAttributes.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Operator.h"
+#include "llvm/ADT/FoldingSet.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/Support/Compiler.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/MathExtras.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/GetElementPtrTypeIterator.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/STLExtras.h"
+#include <algorithm>
+#include <cstdarg>
+
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/CallingConv.h"
+#include "llvm/IR/LLVMContext.h"
+
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm-c/Target.h"
+#include "llvm-c/TargetMachine.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/ADT/DepthFirstIterator.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/Analysis/CallGraph.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/CallSite.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include<queue>
+
+using namespace llvm;
+
+namespace {
+class FixByValAttributesPass: public ModulePass{
+ private:
+  bool m_is_64bit;
+  bool runOnModule(Module &) override;
+  bool transformFunction(Function*);
+  bool checkTypeHasPtrs(Argument*);
+  bool checkPtrsInST(StructType*);
+  void createGEPStores(Value*, Value*,StructType*, Instruction*, 
+                       std::vector<Value*>);
+
+ public:
+  static char ID;
+ FixByValAttributesPass(): ModulePass(ID){
+  }
+  StringRef getPassName() const override{ return "FixByValAttributes";}
+  // @@@ comment out legacy code
+  //const char* getPassName() const{ return "FixByValAttributes";}
+  
+};
+
+
+
 
 static cl::opt<bool>
 fix_all_byval
@@ -51,10 +124,6 @@ fix_specific_byval
  cl::desc("Transform only pointer byval attributes"),
  cl::init(false));
 
-char FixByValAttributesPass:: ID = 0;
-
-static RegisterPass<FixByValAttributesPass> P ("FixByValAttributesPass",
-                                               "Transform all byval Attributes");
 
 void 
 FixByValAttributesPass::createGEPStores(Value* result_alloca, 
@@ -347,4 +416,11 @@ bool FixByValAttributesPass::runOnModule(Module & module) {
   return true;
 }
 
+}
+
+
+char FixByValAttributesPass:: ID = 0;
+
+static RegisterPass<FixByValAttributesPass> P ("FixByValAttributesPass",
+                                               "Transform all byval Attributes");
 
