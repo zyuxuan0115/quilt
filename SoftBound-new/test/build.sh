796a9209f9c3229a44c61b5bba5560babebdd5df
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-02-15 19:52:09 -0500

    add script

---
+++ SoftBound-new/test/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clang -fPIC -emit-llvm -S test.c -c -o test0.ll
+opt -load /proj/zyuxuanssf-PG0/llvm-project-10/build/lib/InitializeSoftBoundCETS.so -InitializeSoftBoundCETS test0.ll -S -o test1.ll
+opt -load /proj/zyuxuanssf-PG0/llvm-project-10/build/lib/LLVMSoftBoundCETS.so -SoftBoundCETSPass test1.ll -S -o test2.ll
+llc -filetype=obj test2.ll -o test.o
+clang test.o -o test-softbound -L../linker-lib/ -lsoftboundcets_rt -lm
+clang test.c -o test-original
