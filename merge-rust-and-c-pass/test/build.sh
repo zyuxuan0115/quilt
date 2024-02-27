f239ff3d5d9d1c25bed5b0a8b756fe1492e5028f
Yuxuan Zhang <kate0115.zyx@gmail.com>
2024-02-26 22:27:33 -0500

    Update build.sh

--- merge-rust-and-c-pass/test/build.sh
+++ merge-rust-and-c-pass/test/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# compile rust code into IR
 rustc rust_code/src/main.rs --emit=llvm-ir -O -C no-prepopulate-passes -C codegen-units=1
-clang -fPIC -emit-llvm -S c_code/main.c -c -o main_c.ll 
+# compile c code into IR
+clang -fPIC -emit-llvm -S c_code/main.c -c -o main_c.ll
+# rename function name (main -> main_rust)
 opt -load /proj/zyuxuanssf-PG0/llvm-project-10/build/lib/LLVMMergeRustAndC.so -FuncRename main.ll -S -o main_rename.ll
+# merge c function and rust function into the same address space
 llvm-link main_c.ll main_rename.ll -o main_c_rust.ll -S
+#
 opt -load /proj/zyuxuanssf-PG0/llvm-project-10/build/lib/LLVMMergeRustAndC.so -MergeRustAndC main_c_rust.ll -S -o main_final.ll
 llc -filetype=obj main_final.ll -o main.o
 clang -L/users/zyuxuan/.rustup/toolchains/1.46-x86_64-unknown-linux-gnu/lib main.o -o main -lstd-cf0f33af3a901778
