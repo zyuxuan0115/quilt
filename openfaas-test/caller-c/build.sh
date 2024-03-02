2d02422153858a1a40ac319b31086558b1b03528
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-03-02 17:40:25 -0500

    update

---
+++ openfaas-test/caller-c/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#ROOT_DIR=`realpath $(dirname $0)/..`
+ROOT_DIR=$(pwd)
+echo $ROOT_DIR
+
+function build_c_test {
+    sudo docker build -t zyuxuan0115/caller-c:latest \
+        -f Dockerfile --no-cache \
+        $ROOT_DIR
+}
+
+function push_c_test {
+    sudo docker push zyuxuan0115/hello-c:latest
+}
+
+function build {
+    build_c_test
+}
+
+function push {
+    push_c_test
+}
+
+case "$1" in
+build)
+    build
+    ;;
+push)
+    push
+    ;;
+esac
