5877aae141c3332ea6d0f70e8ba121bc85690de7
zyuxuan <zyuxuan@localhost>
2024-03-02 12:36:32 -0500

    indent

--- merge-rust-and-c-pass/test_c_RPC/main.c
+++ merge-rust-and-c-pass/test_c_RPC/main.c
@@ -4,25 +4,25 @@
 #include <net/if.h>
  
 int main(void){
-CURL *curl;
-CURLcode res;
+  CURL *curl;
+  CURLcode res;
 	     
-curl = curl_easy_init();
-if(curl) {
-long my_scope_id;
-curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+  curl = curl_easy_init();
+  if(curl) {
+    long my_scope_id;
+    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
 
-my_scope_id = if_nametoindex("eth0");
-curl_easy_setopt(curl, CURLOPT_ADDRESS_SCOPE, my_scope_id);
+    my_scope_id = if_nametoindex("eth0");
+    curl_easy_setopt(curl, CURLOPT_ADDRESS_SCOPE, my_scope_id);
 
-/* Perform the request, res gets the return code */
-res = curl_easy_perform(curl);
-/* Check for errors */
-if(res != CURLE_OK)
-fprintf(stderr, "curl_easy_perform() failed: %s\n",
-curl_easy_strerror(res));
-/* always cleanup */
-curl_easy_cleanup(curl);
-}
-return 0;
+    /* Perform the request, res gets the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if (res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+    curl_easy_strerror(res));
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  return 0;
 }
