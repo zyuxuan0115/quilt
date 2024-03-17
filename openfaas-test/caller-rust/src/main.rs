21985db8970ed563121e953cb82b06680cd08317
zyuxuan <zyuxuan@localhost>
2024-03-17 00:05:07 -0400

    update

--- openfaas-test/caller-rust/src/main.rs
+++ openfaas-test/caller-rust/src/main.rs
@@ -1,30 +1,36 @@
 use curl::easy::{Easy, WriteError};
 use std::io::{stdin, Read, stdout, Write};
 
+fn make_RPC(func_name: &str, mut input: &[u8]) -> String {
+  let mut easy = Easy::new();
+  let mut url = String::from("http://127.0.0.1:8080/function/");
+  url.push_str(func_name);
+  easy.url(&url).unwrap();
+  easy.post(true).unwrap();
+  easy.post_field_size(input.len() as u64).unwrap();
+
+  let mut html_data = String::new();
+  {
+    let mut transfer = easy.transfer();
+    transfer.read_function(|buf| {
+      Ok(input.read(buf).unwrap_or(0))
+    }).unwrap();
+
+    transfer.write_function(|data| {
+      html_data = String::from_utf8(Vec::from(data)).unwrap();
+      Ok(data.len())
+    }).unwrap();
+
+    transfer.perform().unwrap();
+  }
+  html_data
+}
+
 fn main() {
 
   let mut data = "this is the body".as_bytes();
 
-  let mut easy = Easy::new();
-  easy.url("http://127.0.0.1:8080/function/hello-rust").unwrap();
-  easy.post(true).unwrap();
-  easy.post_field_size(data.len() as u64).unwrap();
+  let result = make_RPC("hello-rust", data);
 
-  let mut transfer = easy.transfer();
-  transfer.read_function(|buf| {
-    Ok(data.read(buf).unwrap_or(0))
-  }).unwrap();
-
-  transfer.write_function(|data| {
-    //html_data = String::from_utf8(Vec::from(data)).unwrap();
-    //println!("{}", html_data);
-    stdout().write_all(data).unwrap();
-    Ok(data.len())
-  }).unwrap();
-
-  transfer.perform().unwrap();
-
-//  assert_eq!(easy.response_code().unwrap(), 200);
-//  let contents = easy.get_ref();
-//  println!("{}", String::from_utf8_lossy(&contents.0));
+  println!("{}", result);
 }
