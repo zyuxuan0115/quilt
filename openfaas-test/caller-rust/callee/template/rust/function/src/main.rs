411ddfdfc8215460bce740b9d0d6a6f01cd7dbfc
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-03-17 12:17:20 -0400

    update

--- openfaas-test/caller-rust/callee/template/rust/function/src/main.rs
+++ openfaas-test/caller-rust/callee/template/rust/function/src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 fn main()-> io::Result<()> {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer);
-    let mut prefix: String = "From Rust Function: ".to_owned();
+    let mut prefix: String = "From Rust Callee: ".to_owned();
     prefix.push_str(&buffer);
     println!("{}", prefix);
     Ok(())
