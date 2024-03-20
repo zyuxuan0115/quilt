e2a6e58809913639c43875885f53b972826525a6
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-03-19 20:37:09 -0400

    update

--- merge-rust-func/test/build.sh
+++ merge-rust-func/test/build.sh
@@ -10,7 +10,7 @@ cd caller && cargo clean \
 && cd ../
 
 $LLVM_DIR/opt -S callee/target/debug/deps/function-62ae499746779c3b.ll -passes=rename-func -o callee_rename.ll
-$LLVM_DIR/opt callee_rename.ll -strip-debug -o output.ll -S
 cp callee_rename.ll caller/target/debug/deps/
 $LLVM_DIR/llvm-link caller/target/debug/deps/*.ll -S -o merge.ll
-
+$LLVM_DIR/llc -filetype=obj merge.ll -o main.o
+$LLVM_DIR/clang -no-pie -L/users/zyuxuan/.rustup/toolchains/1.76-x86_64-unknown-linux-gnu/lib  main.o -o main -lstd-66d8041607d2929b -lcurl -lcrypto -lm -lssl
