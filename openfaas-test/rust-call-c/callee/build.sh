36557f29188dc522fee9081ae900cf26987cde40
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-03-24 14:02:23 -0400

    update

---
+++ openfaas-test/rust-call-c/callee/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#ROOT_DIR=`realpath $(dirname $0)/..`
+ROOT_DIR=$(pwd)
+echo $ROOT_DIR
+
+function build_caller {
+    sudo docker build -t zyuxuan0115/caller-c:latest \
+        -f Dockerfile.caller \
+        $ROOT_DIR
+}
+
+function build_callee {
+    sudo docker build -t zyuxuan0115/callee-c:latest \
+        -f Dockerfile.callee --no-cache \
+        $ROOT_DIR
+}
+
+
+
+function push {
+    sudo docker push zyuxuan0115/caller-c:latest
+    sudo docker push zyuxuan0115/callee-c:latest
+}
+
+function build {
+    build_caller
+    build_callee
+}
+
+case "$1" in
+build)
+    build
+    ;;
+push)
+    push
+    ;;
+esac
---
+++ openfaas-test/rust-call-c/caller/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#ROOT_DIR=`realpath $(dirname $0)/..`
+ROOT_DIR=$(pwd)
+echo $ROOT_DIR
+
+function build_rust_test {
+    sudo docker build -t zyuxuan0115/caller-rust:latest \
+        -f Dockerfile --no-cache \
+        $ROOT_DIR/template/rust
+}
+
+function push_rust_test {
+    sudo docker push zyuxuan0115/caller-rust:latest
+}
+
+function build {
+    build_rust_test
+}
+
+function push {
+    push_rust_test
+}
+
+case "$1" in
+build)
+    build
+    ;;
+push)
+    push
+    ;;
+esac
