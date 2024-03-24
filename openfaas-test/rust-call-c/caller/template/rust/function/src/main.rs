040cabf9748e06c38d577cce684af787ef33c03c
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-03-24 14:25:16 -0400

    update

--- openfaas-test/rust-call-c/caller/template/rust/function/src/main.rs
+++ openfaas-test/rust-call-c/caller/template/rust/function/src/main.rs
@@ -42,7 +42,6 @@ fn main() {
   let buffer = get_arg_from_caller();
   let mut prefix: String = "From Rust Caller: ".to_owned();
   prefix.push_str(&buffer);
-  //let data = (&prefix).as_bytes();
-  let result = make_rpc("callee-rust", prefix);
+  let result = make_rpc("callee-c", prefix);
   send_return_value_to_caller(result);
 }
