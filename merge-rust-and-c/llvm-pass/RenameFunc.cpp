e9e97846c83d8195f4373dbd4d7af6dd906f9742
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-03-26 16:56:11 -0400

    update

--- merge-rust-and-c/llvm-pass/RenameFunc.cpp
+++ merge-rust-and-c/llvm-pass/RenameFunc.cpp
@@ -36,31 +36,6 @@ PreservedAnalyses RenameFuncPass::run(Module &M,
     mainFunc->setName("main_callee_rust");
     rustRTFunc->setName("_std_rt_lang_start_callee");
   }
-
   return PreservedAnalyses::all();
 }
 
-
-
-/*
-PreservedAnalyses RenameFuncPass::run(Module &M,
-                                      ModuleAnalysisManager &AM) {
-  Function *mainFunc = M.getFunction("main");
-  Function *rustRTFunc = M.getFunction("_ZN3std2rt10lang_start17h3039573b3cb9f466E");
-  for (Function::iterator BBB = mainFunc->begin(), BBE = mainFunc->end(); BBB != BBE; ++BBB){
-    for (BasicBlock::iterator IB = BBB->begin(), IE = BBB->end(); IB != IE; IB++){
-      if(isa<CallInst>(IB)){
-        CallInst *ci = dyn_cast<CallInst>(IB);
-	Function* realMainFunc = dyn_cast<Function>(ci->getArgOperand(0));
-	realMainFunc->setName("callee");
-      }
-    }
-  }
-  mainFunc->setName("main_callee");
-  rustRTFunc->setName("_ZN3std2rt10lang_start17h3039573b3cb9f466E_callee");
-  //mainFunc->eraseFromParent();
-  //rustRTFunc->eraseFromParent();
-
-  return PreservedAnalyses::all();
-}
-*/
