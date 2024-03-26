554b7b221da1e40b36a7b4f2a200be35429b281d
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-03-25 21:57:37 -0400

    update

--- merge-rust-and-c/test/wrapper/src/main.rs
+++ merge-rust-and-c/test/wrapper/src/main.rs
@@ -3,16 +3,17 @@ extern crate libc;
 use libc::{c_char};
 use std::ffi::{CStr,CString};
 
-fn dummy_func(){
+fn dummy_func(input: *const c_char) -> *const c_char {
   println!("I'm the dummy function!");
+  input
 }
 
 fn callee_rust_to_c (input: String) -> String {
   let input_cstring: CString = CString::new(&input[..]).unwrap();
   let input_c: *const c_char = input_cstring.as_ptr();   
   // TODO: test calling a C function
-  dummy_func();
-  let output: String = unsafe{ CStr::from_ptr(input_c).to_str().unwrap().to_owned()};
+  let output_c: *const c_char = dummy_func(input_c);
+  let output: String = unsafe{ CStr::from_ptr(output_c).to_str().unwrap().to_owned()};
   output
 }
 
