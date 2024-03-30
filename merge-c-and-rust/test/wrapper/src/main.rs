b55dfe3bec3b1d615148a362bd710e763f5d1cd4
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-03-30 10:34:50 -0400

    test if drop drop() workds

--- merge-c-and-rust/test/wrapper/src/main.rs
+++ merge-c-and-rust/test/wrapper/src/main.rs
@@ -3,23 +3,22 @@ extern crate libc;
 use libc::{c_char};
 use std::ffi::{CStr,CString};
 
-fn dummy_func(input: *const c_char) -> *const c_char {
+fn dummy_func(input: String) -> String {
   println!("I'm the dummy function!");
   input
 }
 
-fn callee_rust_to_c (input: String) -> String {
-  let input_cstring: CString = CString::new(&input[..]).unwrap();
-  let input_c: *const c_char = input_cstring.as_ptr();   
-  // TODO: test calling a C function
-  let output_c: *const c_char = dummy_func(input_c);
-  let output: String = unsafe{ CStr::from_ptr(output_c).to_str().unwrap().to_owned()};
-  output
+fn callee_c_to_rust(input_c: *const c_char) -> *const c_char {
+  let input: String = unsafe{ CStr::from_ptr(input_c).to_str().unwrap().to_owned()};
+  let output: String = dummy_func(input);
+  let output_cstring: CString = CString::new(&output[..]).unwrap();
+  let output_c: *const c_char = output_cstring.as_ptr();
+  return output_c
 }
 
 fn main() {
-  let input: String = String::from("hello world!");
-  let output: String = callee_rust_to_c(input);
+  let input: CString = CString::new("hello world!").unwrap();
+  let output_c: *const c_char = callee_c_to_rust(input.as_ptr());
+  let output: String = unsafe{ CStr::from_ptr(output_c).to_str().unwrap().to_owned()};
   println!("{}", output);
- 
 }
