1e2702311e248f3205e2c8a791f6eb622053bda8
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-08 20:44:57 -0400

    delete useless

--- socialnetwork_faas/UniqueIdService/rust/UniqueIdService/src/main.rs
+++
@@ -1,8 +0,0 @@
-use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller};
-use short_uuid::ShortUuid;
-
-fn main() {
-  let input: String = get_arg_from_caller();
-  let uuid = ShortUuid::generate().to_string();
-  send_return_value_to_caller(uuid);
-}
--- socialnetwork_faas/social_network_types.h
+++
@@ -1,9 +0,0 @@
-typedef struct Media {
-  int64_t media_id;
-  char* media_type; 
-} Media;
-
-typedef struct Url {
-  char* shortened_url;
-  char* expanded_url;
-} Url;
