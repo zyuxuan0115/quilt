6751065c0fae7ed392e4c20149c897035923acf3
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-09 14:32:13 -0400

    update

--- DeathStarBench/socialMedia_rust/UrlShortenService/template/rust/function/src/main.rs
+++ DeathStarBench/socialMedia_rust/UrlShortenService/template/rust/function/src/main.rs
@@ -1,6 +1,12 @@
 extern crate rand;
 use rand::{distributions::Alphanumeric, Rng}; // 0.8
-use mongodb::{Client, options::ClientOptions};
+use mongodb::{bson::doc,sync::Client};
+//use serde::{Deserialize, Serialize};
+
+struct url{
+  shortened_url: String,
+  expanded_url: String,
+} 
 
 fn gen_rand_str()->String{
   let s: String = rand::thread_rng()
@@ -13,14 +19,25 @@ fn gen_rand_str()->String{
 
 fn main() {
   let s = gen_rand_str();
-  let mut client_ret = Client::with_uri_str("mongodb://mongodb.default.svc.cluster.local:27017");
-  let client = match client_ret {
-    Ok(x) => x,
-    Err(e) => {
-      println!("Fail to connect to mongodb server");
-      panic!("Fail to connect to mongodb server");
-    }
-  }
+  let client = Client::with_uri_str("mongodb://mongodb.default.svc.cluster.local:27017").unwrap();
+  let database = client.database("url-shorten");
+  let collection = database.collection::<url>("url-shorten");
   println!("{}", s);
 }
 
+//extern crate mongodb;
+//use mongodb::{ bson::doc, options::{ ClientOptions, ServerApi, ServerApiVersion }, sync::Client };
+//fn main() -> mongodb::error::Result<()> {
+    // Replace the placeholder with your Atlas connection string
+//    let uri = "<connection string>";
+//    let mut client_options = ClientOptions::parse(uri)?;
+    // Set the server_api field of the client_options object to Stable API version 1
+//    let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
+//    client_options.server_api = Some(server_api);
+    // Create a new client and connect to the server
+//    let client = Client::with_options(client_options)?;
+    // Send a ping to confirm a successful connection
+//    client.database("admin").run_command(doc! { "ping": 1 }, None)?;
+//    println!("Pinged your deployment. You successfully connected to MongoDB!");
+//    Ok(())
+//}
