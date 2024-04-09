39bcb07f33c406052650131b055527d33e3bffd1
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-09 14:45:11 -0400

    update

--- DeathStarBench/socialMedia_rust/UrlShortenService/template/rust/function/src/main.rs
+++ DeathStarBench/socialMedia_rust/UrlShortenService/template/rust/function/src/main.rs
@@ -1,8 +1,9 @@
 extern crate rand;
 use rand::{distributions::Alphanumeric, Rng}; // 0.8
 use mongodb::{bson::doc,sync::Client};
-//use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
 struct url{
   shortened_url: String,
   expanded_url: String,
@@ -22,22 +23,23 @@ fn main() {
   let client = Client::with_uri_str("mongodb://mongodb.default.svc.cluster.local:27017").unwrap();
   let database = client.database("url-shorten");
   let collection = database.collection::<url>("url-shorten");
+
+  let docs = vec![
+    url {
+        shortened_url: "1984".to_string(),
+        expanded_url: "George Orwell".to_string(),
+    },
+    url {
+        shortened_url: "Animal Farm".to_string(),
+        expanded_url: "George Orwell".to_string(),
+    },
+    url {
+        shortened_url: "The Great Gatsby".to_string(),
+        expanded_url: "F. Scott Fitzgerald".to_string(),
+    },
+  ];
+  collection.insert_many(docs, None).unwrap();
+
   println!("{}", s);
 }
 
-//extern crate mongodb;
-//use mongodb::{ bson::doc, options::{ ClientOptions, ServerApi, ServerApiVersion }, sync::Client };
-//fn main() -> mongodb::error::Result<()> {
-    // Replace the placeholder with your Atlas connection string
-//    let uri = "<connection string>";
-//    let mut client_options = ClientOptions::parse(uri)?;
-    // Set the server_api field of the client_options object to Stable API version 1
-//    let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
-//    client_options.server_api = Some(server_api);
-    // Create a new client and connect to the server
-//    let client = Client::with_options(client_options)?;
-    // Send a ping to confirm a successful connection
-//    client.database("admin").run_command(doc! { "ping": 1 }, None)?;
-//    println!("Pinged your deployment. You successfully connected to MongoDB!");
-//    Ok(())
-//}
