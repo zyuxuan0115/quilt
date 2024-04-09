98d34b147be3a54e1245a5e71c52303e0062426b
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-09 11:05:41 -0400

    update

--- DeathStarBench/socialMedia_rust/UrlShortenService/template/rust/function/src/main.rs
+++ DeathStarBench/socialMedia_rust/UrlShortenService/template/rust/function/src/main.rs
@@ -13,7 +13,14 @@ fn gen_rand_str()->String{
 
 fn main() {
   let s = gen_rand_str();
-  let mut client_options = Client::with_uri_str("mongodb://mongodb.default.svc.cluster.local:27017");
+  let mut client_ret = Client::with_uri_str("mongodb://mongodb.default.svc.cluster.local:27017");
+  let client = match client_ret {
+    Ok(x) => x,
+    Err(e) => {
+      println!("Fail to connect to mongodb server");
+      panic!("Fail to connect to mongodb server");
+    }
+  }
   println!("{}", s);
 }
 
