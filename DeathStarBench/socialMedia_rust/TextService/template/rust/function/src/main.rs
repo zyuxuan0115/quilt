c89b7c205883e282072891c55ebd7e68f23f1e9e
zyuxuan <zyuxuan@localhost>
2024-04-12 21:15:46 -0400

    update

--- DeathStarBench/socialMedia_rust/TextService/template/rust/function/src/main.rs
+++ DeathStarBench/socialMedia_rust/TextService/template/rust/function/src/main.rs
@@ -1,7 +1,5 @@
-use mongodb::{bson::doc,sync::Client};
 use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller};
-use std::fs::read_to_string;
 use regex::Regex;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,40 +14,18 @@ struct user_mention {
   user_name: String,
 }
 
-fn read_lines(filename: &str) -> Vec<String> {
-    read_to_string(filename) 
-        .unwrap()  // panic on possible file-reading errors
-        .lines()  // split the string into an iterator of string slices
-        .map(String::from)  // make each slice into a string
-        .collect()  // gather them together into a vector
-}
- 
-
-fn get_uri() -> String{
-  let passwords: Vec<String> = read_lines("/var/openfaas/secrets/mongo-db-password");
-  if passwords.len() == 0 {
-    println!("no password found!");
-  } 
-  if passwords.len() > 1 {
-    println!("more than 1 passwords found!");
-  }
-  let password = passwords[0].to_owned();
-  let mut uri: String = String::from("mongodb://root:");
-  uri.push_str(&password[..]);
-  uri.push_str("@mongodb.default.svc.cluster.local:27017");
-  uri
-}
-
 fn main() {
   let input: String = get_arg_from_caller();
-  let text = input;
+  let mut text = input;
+
+  println!("{}", text);
 
   let re = Regex::new(r"@[a-zA-Z0-9-_]+").unwrap();
 
   let mut mentioned_usernames: Vec<String> = Vec::new();
   let mut urls : Vec<String> = Vec::new();
   for username in re.captures_iter(&text[..]).map(|m| m.get(0).unwrap().as_str()) {
-    mentioned_usernames.push(username.to_string());
+    mentioned_usernames.push(username.strip_prefix("@").unwrap().to_string());
   }
 
   let re2 = Regex::new(r"(http://|https://)([a-zA-Z0-9_!~*'().&=+$%-]+)").unwrap();
@@ -57,29 +33,21 @@ fn main() {
     urls.push(url.to_string());
   }
 
-  println!("{:?}",  mentioned_usernames);
-  println!("{:?}",  urls);
   let mentioned_usernames_serialized = serde_json::to_string(&mentioned_usernames).unwrap();
   let user_mentions_str: String = make_rpc("user-mention-service", mentioned_usernames_serialized);
   let user_mentions: Vec<user_mention> = serde_json::from_str(&user_mentions_str).unwrap();
-//  let urls: Vec<String> = serde_json::from_str(&input).unwrap();
 
-//  let uri = get_uri();
-//  let client = Client::with_uri_str(&uri[..]).unwrap();
-//  let database = client.database("url-shorten");
-//  let collection = database.collection::<url_pair>("url-shorten");
+  let urls_serialized = serde_json::to_string(&urls).unwrap();
+  let urls_str: String = make_rpc("url-shorten-service", urls_serialized);
+  let url_pairs: Vec<url_pair> = serde_json::from_str(&urls_str).unwrap();
 
-//  let mut docs: Vec<url_pair> = Vec::new();
-//  for url in urls {
-//    let s = gen_short_url();
-//    let new_pair = url_pair{
-//      shortened_url: s.clone(), 
-//      expanded_url: url.clone(),
-//    };
- //   docs.push(new_pair);
- // }
- // let serialized = serde_json::to_string(&docs).unwrap();
- // collection.insert_many(docs, None).unwrap();
- // send_return_value_to_caller(serialized);
+  println!("{:?}", user_mentions);
+  println!("{:?}", urls);
+
+  for item in url_pairs {
+    let text_str: &str = &text[..];
+    text = text_str.replace(&item.expanded_url[..], &item.shortened_url[..]).to_string();
+  }
+  send_return_value_to_caller(text);
 }
 
