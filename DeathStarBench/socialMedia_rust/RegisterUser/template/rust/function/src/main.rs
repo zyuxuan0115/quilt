e0fa6c146e2053dca1b9ac7226aee249174a9ac3
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-13 20:32:28 -0400

    update

--- DeathStarBench/socialMedia_rust/RegisterUser/template/rust/function/src/main.rs
+++ DeathStarBench/socialMedia_rust/RegisterUser/template/rust/function/src/main.rs
@@ -2,6 +2,7 @@ use mongodb::{bson::doc,sync::Client};
 use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use std::fs::read_to_string;
+use rand::{distributions::Alphanumeric, Rng};
 
 fn read_lines(filename: &str) -> Vec<String> {
     read_to_string(filename) 
@@ -29,11 +30,12 @@ fn get_uri() -> String{
 fn main() {
   let input: String = get_arg_from_caller();
   let new_user_info: register_user_with_id_get = serde_json::from_str(&input).unwrap();
-  println!("{:?}", new_user_info);
   let uri = get_uri();
   let client = Client::with_uri_str(&uri[..]).unwrap();
   let database = client.database("user");
   let collection = database.collection::<register_user_with_id_get>("user");
+
+  let num: i64 = rand::thread_rng().gen();
 
   let result = collection.find_one(doc! { "username": &new_user_info.username[..] }, None).unwrap();
 
@@ -43,22 +45,9 @@ fn main() {
       panic!("User {} already existed", new_user_info.username);
     },
     None => (),
-  }
- 
+  } 
+  let user_id_str = serde_json::to_string(&new_user_info.user_id).unwrap();
   collection.insert_one(new_user_info, None).unwrap();
-
-//  println!("{:?}", result);
-//  let mut docs: Vec<url_pair> = Vec::new();
-//  for url in urls {
-//    let s = gen_short_url();
-//    let new_pair = url_pair{
-//      shortened_url: s.clone(), 
-//      expanded_url: url.clone(),
-//    };
-//    docs.push(new_pair);
-//  }
-//  let serialized = serde_json::to_string(&docs).unwrap();
-//  collection.insert_many(docs, None).unwrap();
+  make_rpc("social-graph-insert-user", user_id_str);
   send_return_value_to_caller("".to_string());
 }
-
