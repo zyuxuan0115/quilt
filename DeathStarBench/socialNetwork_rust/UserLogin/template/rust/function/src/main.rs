28aded57ac246bfd98994d3cb88fcc04a542f745
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-14 21:17:33 -0400

    update

--- DeathStarBench/socialNetwork_rust/UserLogin/template/rust/function/src/main.rs
+++ DeathStarBench/socialNetwork_rust/UserLogin/template/rust/function/src/main.rs
@@ -66,12 +66,10 @@ fn main() {
   let result_str: Option<String> = memcache_client.get(&username[..]).unwrap();
   username = username[..].strip_suffix(":login").unwrap().to_string();
 
-  let mut memcache_has_username: bool = false;
   let mut jwt_encode_msg: String = String::new();
   match result_str {
     Some(x) => {
       let result: memcached_userlogin_info = serde_json::from_str(&x).unwrap();
-      memcache_has_username = true;
       let password_stored: String = result.password;
       let salt_stored: String = result.salt;
       password.push_str(&salt_stored[..]);
@@ -92,44 +90,42 @@ fn main() {
         panic!("");
       }
     },
-    None => (),
+    None => {
+      let uri = get_mongodb_uri();
+      let client = Client::with_uri_str(&uri[..]).unwrap();
+      let database = client.database("user");
+      let collection = database.collection::<user_info>("user");
+      let mongodb_result = collection.find_one(doc! { "username": &username[..] }, None).unwrap();
+
+      match mongodb_result {
+        Some(y) => {
+          let password_stored: String = y.password;
+          let salt_stored: String = y.salt;
+          password.push_str(&salt_stored[..]);
+          let user_id_stored: i64 = y.user_id;
+          let auth: bool = digest(&password) == password_stored;
+          if auth == true {
+            let payload_struct = user_login_return {
+              user_id: user_id_stored,
+              username: username,
+              timestamp: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
+              ttl: 3600,
+            };
+            let payload = serde_json::to_string(&payload_struct).unwrap();
+            jwt_encode_msg  = jwt_encode(&secret[..], &payload[..]);
+          }
+          else {
+            println!("");
+            panic!("");
+          }
+        },
+        None => {
+          println!("User: {} doesn't exist in MongoDB", username);
+          panic!("User: {} doesn't exist in MongoDB", username);
+        },
+      } 
+    },
   } 
-
-  if memcache_has_username == false {
-    let uri = get_mongodb_uri();
-    let client = Client::with_uri_str(&uri[..]).unwrap();
-    let database = client.database("user");
-    let collection = database.collection::<user_info>("user");
-    let mongodb_result = collection.find_one(doc! { "username": &username[..] }, None).unwrap();
-
-    match mongodb_result {
-      Some(x) => {
-        let password_stored: String = x.password;
-        let salt_stored: String = x.salt;
-        password.push_str(&salt_stored[..]);
-        let user_id_stored: i64 = x.user_id;
-        let auth: bool = digest(&password) == password_stored;
-        if auth == true {
-          let payload_struct = user_login_return {
-            user_id: user_id_stored,
-            username: username,
-            timestamp: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
-            ttl: 3600,
-          };
-          let payload = serde_json::to_string(&payload_struct).unwrap();
-          jwt_encode_msg  = jwt_encode(&secret[..], &payload[..]);
-        }
-        else {
-          println!("");
-          panic!("");
-        }
-      },
-      None => {
-        println!("User: {} doesn't exist in MongoDB", username);
-        panic!("User: {} doesn't exist in MongoDB", username);
-      },
-    } 
-  }    
   send_return_value_to_caller(jwt_encode_msg);
 }
 
