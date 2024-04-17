b09285b83a8f0ba56e2ab8c78cf875adea651e83
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-17 18:25:25 -0400

    update

--- DeathStarBench/socialNetwork_rust/build.sh
+++ DeathStarBench/socialNetwork_rust/build.sh
@@ -2,5 +2,17 @@
 search_dir=$(pwd)
 for entry in "$search_dir"/*
 do
-  echo "$entry"
+  BASE_NAME=$(basename $entry)
+  if [[ "$BASE_NAME" = "OpenFaaSRPC" ]] ; then
+    continue
+  elif [[ "$BASE_NAME" = "build.sh" ]] ; then 
+    continue
+  else
+    echo "$entry"
+    cd $entry
+    ./build.sh build
+    ./build.sh push
+    YAML_FILE=$(ls *.yml)
+    faas-cli deploy -f $YAML_FILE
+  fi
 done
