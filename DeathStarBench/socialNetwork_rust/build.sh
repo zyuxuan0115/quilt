ddc73c91ac4f41d2388e2e494a3de5edf204a16b
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-17 18:38:18 -0400

    update

--- DeathStarBench/socialNetwork_rust/build.sh
+++ DeathStarBench/socialNetwork_rust/build.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
+  
 search_dir=$(pwd)
-for entry in "$search_dir"/*
-do
-  BASE_NAME=$(basename $entry)
-  if [[ "$BASE_NAME" = "OpenFaaSRPC" ]] ; then
-    continue
-  elif [[ "$BASE_NAME" = "build.sh" ]] ; then 
-    continue
-  else
-    echo "$entry"
-    cd $entry
-    ./build.sh build
-    ./build.sh push
-    YAML_FILE=$(ls *.yml)
-    faas-cli deploy -f $YAML_FILE
-  fi
-done
+
+function build_and_deploy {
+  for entry in "$search_dir"/*
+  do
+    BASE_NAME=$(basename $entry)
+    if [[ "$BASE_NAME" = "OpenFaaSRPC" ]] ; then
+      continue
+    elif [[ "$BASE_NAME" = "build.sh" ]] ; then 
+      continue
+    else
+      cd $entry
+      ./build.sh build
+      ./build.sh push
+      YAML_FILE=$(ls *.yml)
+      faas-cli deploy -f $YAML_FILE
+    fi
+  done
+}
+
+function deploy {
+  for entry in "$search_dir"/*
+  do
+    BASE_NAME=$(basename $entry)
+    if [[ "$BASE_NAME" = "OpenFaaSRPC" ]] ; then
+      continue
+    elif [[ "$BASE_NAME" = "build.sh" ]] ; then 
+      continue
+    else
+      cd $entry
+      YAML_FILE=$(ls *.yml)
+      faas-cli deploy -f $YAML_FILE
+    fi
+  done
+}
+
+case "$1" in
+build)
+    build_and_deploy
+    ;;
+deploy)
+    deploy
+    ;;
+esac
