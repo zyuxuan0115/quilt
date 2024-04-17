9afe7dd142af733b0878a4873d521d93abb36062
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-17 11:22:02 -0400

    update

--- DeathStarBench/socialNetwork_rust/SocialGraphGetFollowers/template/rust/function/src/main.rs
+++ DeathStarBench/socialNetwork_rust/SocialGraphGetFollowers/template/rust/function/src/main.rs
@@ -60,43 +60,21 @@ fn get_redis_ro_uri() -> String{
 
 fn main() {
   let input: String = get_arg_from_caller();
-  println!("{}", input);
-  let follow_info: social_graph_follow_get = serde_json::from_str(&input).unwrap();
+  let user_id: i64 = serde_json::from_str(&input).unwrap();
 
-  let time_stamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+  // get redis connection
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
+
+  let mut user_id_str: String = user_id.to_string();
+  user_id_str.push_str(":followers"); 
+  let res: isize = con.zrange(&user_id_str[..], 0, -1).unwrap();
 
   let uri = get_mongodb_uri();
   let client = Client::with_uri_str(&uri[..]).unwrap();
   let database = client.database("social-graph");
   let collection = database.collection::<social_graph_entry>("social-graph");
-
-  // Update follower->followee edges
-  let search_query = doc!{"$and": [doc!{"user_id":follow_info.user_id},doc!{"followees": doc!{"$not":doc!{"$elemMatch":doc!{"user_id":follow_info.followee_id}}}}]};  
-
-  let update_query = doc!{"$push": doc!{"followees":doc!{"user_id":follow_info.followee_id, "timestamp":(time_stamp as i64)} }};
-
-  let res = collection.update_many(search_query, update_query, None).unwrap();
-
-  // Update followee->follower edges
-  let search_query =  doc!{"$and": [doc!{"user_id":follow_info.followee_id},doc!{"followers": doc!{"$not":doc!{"$elemMatch":doc!{"user_id":follow_info.user_id}}}}]};  
-
-  let update_query = doc!{"$push": doc!{"followers":doc!{"user_id":follow_info.user_id, "timestamp":(time_stamp as i64)} }};
-
-  let res = collection.update_many(search_query, update_query, None).unwrap();
-
-  // update redis
-  let mut user_id_str: String = follow_info.user_id.to_string();
-  user_id_str.push_str(":followees");
-  let mut followee_id_str: String = follow_info.followee_id.to_string();
-  let redis_uri = get_redis_rw_uri();
-
-  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
-
-  let mut con = redis_client.get_connection().unwrap();
-  let res: isize = con.zadd(&user_id_str[..], &followee_id_str[..], (time_stamp as i64)).unwrap();
-  user_id_str = (&user_id_str[..]).strip_suffix(":followees").unwrap().to_string();
-  followee_id_str.push_str(":followers");
-  let res: isize = con.zadd(&followee_id_str[..], &user_id_str[..], (time_stamp as i64)).unwrap();
 
   send_return_value_to_caller("".to_string());
 }
