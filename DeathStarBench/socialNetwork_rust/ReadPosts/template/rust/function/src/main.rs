609e95d09667466f4903e6fd3078b63a281582c4
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-19 10:50:11 -0400

    update

--- DeathStarBench/socialNetwork_rust/ReadPosts/template/rust/function/src/main.rs
+++ DeathStarBench/socialNetwork_rust/ReadPosts/template/rust/function/src/main.rs
@@ -1,7 +1,7 @@
 use mongodb::{bson::doc,sync::Client};
 use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
-use std::fs::read_to_string;
+use std::{fs::read_to_string, collections::HashMap};
 use memcache::Client as memcached_client;
 
 fn read_lines(filename: &str) -> Vec<String> {
@@ -34,18 +34,47 @@ fn get_memcached_uri() -> String {
 fn main() {
   let input: String = get_arg_from_caller();
   let post_ids: Vec<i64> = serde_json::from_str(&input).unwrap();
+  let post_ids_str: Vec<String> = post_ids.iter().map(|x| x.to_string()).collect();
+  let post_ids_strslice: Vec<&str> = post_ids_str.iter().map(|x| &x[..]).collect();
 
   let memcache_uri = get_memcached_uri();
   let memcache_client = memcache::connect(&memcache_uri[..]).unwrap(); 
 
-  let result: std::collections::HashMap<i64, String> = memcache_client.gets(&post_ids, 0).unwrap();
+  let result: std::collections::HashMap<String, String> = memcache_client.gets(&post_ids_strslice).unwrap();
 
-  println!("{}", result);
+  let mut post_not_cached: HashMap<String, bool> = HashMap::new();
+  for post_id in &post_ids {
+    let post_id_str = post_id.to_string();
+    post_not_cached.insert(post_id_str, false);
+  }
+
+  println!("{:?}", result);
 
   let uri = get_mongodb_uri();
   let client = Client::with_uri_str(&uri[..]).unwrap();
   let database = client.database("post");
   let collection = database.collection::<Post>("post");
 
-  send_return_value_to_caller("".to_string());
+  let mut posts: Vec<Post> = Vec::new();
+  for (key, value) in &result {
+    post_not_cached.remove(key); 
+    let post: Post = serde_json::from_str(&input).unwrap();
+    posts.push(post);
+  }
+
+  let mut pid_not_cached: Vec<&str> = Vec::new();
+  for (key, _) in &post_not_cached {
+    pid_not_cached.push(&key[..]);
+  }
+
+  let query = doc!{"post_id": doc!{"$in": &pid_not_cached}};
+  let mut cursor = collection.find(query, None).unwrap();
+  
+  for doc in cursor {
+    let doc_ = doc.unwrap();
+    posts.push(doc_);    
+  }
+
+  let serialized = serde_json::to_string(&posts).unwrap();
+  send_return_value_to_caller(serialized);
 }
