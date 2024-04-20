a54abb43b77db2e2a7a524f969f57bfe25f1b00a
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-04-20 15:41:47 -0400

    update

--- DeathStarBench/socialNetwork_rust/SocialGraphFollow/template/rust/function/src/main.rs
+++ DeathStarBench/socialNetwork_rust/SocialGraphFollow/template/rust/function/src/main.rs
@@ -73,11 +73,15 @@ fn main() {
   let search_query = doc!{"$and": [doc!{"user_id":follow_info.user_id},doc!{"followees": doc!{"$not":doc!{"$elemMatch":doc!{"user_id":follow_info.followee_id}}}}]};  
   let update_query = doc!{"$push": doc!{"followees":doc!{"user_id":follow_info.followee_id, "timestamp":(time_stamp as i64)} }};
   let res = collection.update_many(search_query, update_query, None).unwrap();
+  
+  // TODO: check if the update is successful
 
   // Update followee->follower edges
   let search_query =  doc!{"$and": [doc!{"user_id":follow_info.followee_id},doc!{"followers": doc!{"$not":doc!{"$elemMatch":doc!{"user_id":follow_info.user_id}}}}]};  
   let update_query = doc!{"$push": doc!{"followers":doc!{"user_id":follow_info.user_id, "timestamp":(time_stamp as i64)} }};
   let res = collection.update_many(search_query, update_query, None).unwrap();
+
+  // TODO: check if the update is successful
 
   // update redis
   let mut user_id_str: String = follow_info.user_id.to_string();
--- DeathStarBench/socialNetwork_rust/WriteUserTimeline/template/rust/function/src/main.rs
+++ DeathStarBench/socialNetwork_rust/WriteUserTimeline/template/rust/function/src/main.rs
@@ -1,4 +1,4 @@
-use mongodb::{bson::doc,sync::Client};
+use mongodb::{bson::doc,sync::Client,options::UpdateOptions};
 use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use std::{fs::read_to_string, collections::HashMap};
@@ -66,13 +66,17 @@ fn main() {
   let uri = get_mongodb_uri();
   let client = Client::with_uri_str(&uri[..]).unwrap();
   let database = client.database("user-timeline");
-  let collection = database.collection::<social_graph_entry>("user-timeline");
+  let collection = database.collection::<user_timeline_entry>("user-timeline");
 
   // update mongodb
   let query = doc!{"user_id":timeline_info.user_id};
   let update = doc!{"$push":doc!{"posts":doc!{"$each":[doc!{"post_id":timeline_info.post_id,"timestamp":timeline_info.timestamp}], "$position":0}}};
 
-  let res = collection.update_many(query, update, None).unwrap();
+  let options = UpdateOptions::builder()
+        .upsert(Some(true))
+        .build();
+
+  let res = collection.update_many(query, update, options).unwrap();
 
   println!("{:?}",res);
 
