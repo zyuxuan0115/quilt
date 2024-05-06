06c26d9e90b1d1e65166b7adf4c6cd994a111131
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-05 23:56:14 -0400

    update

--- DeathStarBench/social_network_rust/UrlShortenService/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust/UrlShortenService/template/rust/function/src/main.rs
@@ -2,6 +2,7 @@ use rand::{distributions::Alphanumeric, Rng}; // 0.8
 use mongodb::{bson::doc,sync::Client};
 use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
+use DbInterface::*;
 use std::fs::read_to_string;
 
 fn gen_short_url()->String{
@@ -15,35 +16,11 @@ fn gen_short_url()->String{
   short_url
 }
 
-fn read_lines(filename: &str) -> Vec<String> {
-    read_to_string(filename) 
-        .unwrap()  // panic on possible file-reading errors
-        .lines()  // split the string into an iterator of string slices
-        .map(String::from)  // make each slice into a string
-        .collect()  // gather them together into a vector
-}
- 
-
-fn get_uri() -> String{
-  let passwords: Vec<String> = read_lines("/var/openfaas/secrets/mongo-db-password");
-  if passwords.len() == 0 {
-    println!("no password found!");
-  } 
-  if passwords.len() > 1 {
-    println!("more than 1 passwords found!");
-  }
-  let password = passwords[0].to_owned();
-  let mut uri: String = String::from("mongodb://root:");
-  uri.push_str(&password[..]);
-  uri.push_str("@mongodb.default.svc.cluster.local:27017");
-  uri
-}
-
 fn main() {
   let input: String = get_arg_from_caller();
   let urls: Vec<String> = serde_json::from_str(&input).unwrap();
 
-  let uri = get_uri();
+  let uri = get_mongodb_uri();
   let client = Client::with_uri_str(&uri[..]).unwrap();
   let database = client.database("url-shorten");
   let collection = database.collection::<url_pair>("url-shorten");
