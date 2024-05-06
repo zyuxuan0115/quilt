1b95e5ed79ce38b39c58466e9561d5fcb3f966f2
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-05 21:39:15 -0400

    update

--- DeathStarBench/social_network_rust/RegisterUser/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust/RegisterUser/template/rust/function/src/main.rs
@@ -2,17 +2,8 @@ use mongodb::{bson::doc,sync::Client};
 use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
-use std::fs::read_to_string;
 use rand::{distributions::Alphanumeric, Rng};
 use sha256::digest;
-
-fn read_lines(filename: &str) -> Vec<String> {
-    read_to_string(filename) 
-        .unwrap()  // panic on possible file-reading errors
-        .lines()  // split the string into an iterator of string slices
-        .map(String::from)  // make each slice into a string
-        .collect()  // gather them together into a vector
-}
 
 fn gen_random_string()->String{
   let salt: String = rand::thread_rng()
@@ -23,25 +14,10 @@ fn gen_random_string()->String{
   salt
 }
  
-fn get_uri() -> String{
-  let passwords: Vec<String> = read_lines("/var/openfaas/secrets/mongo-db-password");
-  if passwords.len() == 0 {
-    println!("no password found!");
-  } 
-  if passwords.len() > 1 {
-    println!("more than 1 passwords found!");
-  }
-  let password = passwords[0].to_owned();
-  let mut uri: String = String::from("mongodb://root:");
-  uri.push_str(&password[..]);
-  uri.push_str("@mongodb.default.svc.cluster.local:27017");
-  uri
-}
-
 fn main() {
   let input: String = get_arg_from_caller();
   let new_user_info: register_user_get = serde_json::from_str(&input).unwrap();
-  let uri = get_uri();
+  let uri = get_mongodb_uri();
   let client = Client::with_uri_str(&uri[..]).unwrap();
   let database = client.database("user");
   let collection = database.collection::<user_info>("user");
