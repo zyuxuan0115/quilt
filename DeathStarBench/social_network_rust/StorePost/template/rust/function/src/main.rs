811b57ca9645c537749c002b248b86387131d217
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-05 23:42:23 -0400

    update

--- DeathStarBench/social_network_rust/StorePost/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust/StorePost/template/rust/function/src/main.rs
@@ -1,36 +1,13 @@
 use mongodb::{bson::doc,sync::Client};
 use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
-use std::fs::read_to_string;
-
-fn read_lines(filename: &str) -> Vec<String> {
-    read_to_string(filename) 
-        .unwrap()  // panic on possible file-reading errors
-        .lines()  // split the string into an iterator of string slices
-        .map(String::from)  // make each slice into a string
-        .collect()  // gather them together into a vector
-}
- 
-fn get_uri() -> String{
-  let passwords: Vec<String> = read_lines("/var/openfaas/secrets/mongo-db-password");
-  if passwords.len() == 0 {
-    println!("no password found!");
-  } 
-  if passwords.len() > 1 {
-    println!("more than 1 passwords found!");
-  }
-  let password = passwords[0].to_owned();
-  let mut uri: String = String::from("mongodb://root:");
-  uri.push_str(&password[..]);
-  uri.push_str("@mongodb.default.svc.cluster.local:27017");
-  uri
-}
+use DbInterface::*;
 
 fn main() {
   let input: String = get_arg_from_caller();
   let new_post: Post = serde_json::from_str(&input).unwrap();
 
-  let uri = get_uri();
+  let uri = get_mongodb_uri();
   let client = Client::with_uri_str(&uri[..]).unwrap();
   let database = client.database("post");
   let collection = database.collection::<Post>("post");
