a4ba06c711c3d26fcf49f1686963d303dca0f7a7
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-08 00:04:43 -0400

    update

--- DeathStarBench/social_network_rust_test/Caller/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust_test/Caller/template/rust/function/src/main.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
-use DbInterface::*;
 use std::time::{Duration, Instant};
 
 fn main() {
