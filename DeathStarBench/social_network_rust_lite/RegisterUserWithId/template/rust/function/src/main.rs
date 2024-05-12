1a0635f81385c0af19fe6f2f0dd2747f1c8ce683
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-11 20:32:50 -0400

    update

--- DeathStarBench/social_network_rust_lite/RegisterUserWithId/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust_lite/RegisterUserWithId/template/rust/function/src/main.rs
@@ -18,7 +18,7 @@ fn gen_random_string()->String{
 
 fn main() {
   let input: String = get_arg_from_caller();
-  let now = Instant::now();
+//  let now = Instant::now();
   let new_user_info: RegisterUserWithIdArgs = serde_json::from_str(&input).unwrap();
 
   // update redis
@@ -51,8 +51,8 @@ fn main() {
   ret = con.hset(&real_username[..],"password",pw_sha).unwrap();
 
   let user_id_str = serde_json::to_string(&new_user_info.user_id).unwrap();
-  let new_now =  Instant::now();
-  println!("{:?}", new_now.duration_since(now));
+//  let new_now =  Instant::now();
+//  println!("{:?}", new_now.duration_since(now));
   make_rpc("social-graph-insert-user", user_id_str);
   send_return_value_to_caller("".to_string());
 }
