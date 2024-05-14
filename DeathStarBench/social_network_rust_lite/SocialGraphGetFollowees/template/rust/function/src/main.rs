f713025dcddf5f054032857ffc4544f452236a8f
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-14 16:49:14 -0400

    update

--- DeathStarBench/social_network_rust_lite/SocialGraphGetFollowees/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust_lite/SocialGraphGetFollowees/template/rust/function/src/main.rs
@@ -7,59 +7,43 @@ use redis::{Commands, RedisResult};
 
 fn main() {
   let input: String = get_arg_from_caller();
-//  let now = Instant::now();
+  let now = Instant::now();
   let user_id: i64 = serde_json::from_str(&input).unwrap();
 
-  // get redis connection
-  let redis_uri = get_redis_rw_uri();
-  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
-  let mut con = redis_client.get_connection().unwrap();
-
+  // get memcache connection
+  let memcache_uri = get_memcached_uri();
+  let memcache_client = memcache::connect(&memcache_uri[..]).unwrap();
   let mut user_id_str: String = user_id.to_string();
   user_id_str.push_str(":followees"); 
-  let res: RedisResult<Vec<String>> = con.zrange(&user_id_str[..], 0, -1);
-  
-  let mut followee_ids: Vec<i64> = Vec::new();
-  match res {
-    Ok (followees_list) => {
-      if followees_list.len() > 0 {
-        for followee in &followees_list {
-          let followee_id = followee[..].parse::<i64>().unwrap();
-          followee_ids.push(followee_id);
-        }
-      }
-      else {
-        let uri = get_mongodb_uri();
-        let client = Client::with_uri_str(&uri[..]).unwrap();
-        let database = client.database("social-graph");
-        let collection = database.collection::<SocialGraphEntry>("social-graph");
-        let query = doc!{"user_id": user_id };
-        let mut result = collection.find_one(query, None).unwrap();
+  let result_str: Option<String> = memcache_client.get(&user_id_str[..]).unwrap();
+  let mut return_value: String = String::new();
+  match result_str {
+    Some(result) => {
+      return_value = result;
+    },
+    None => {
+      // get redis connection
+      let redis_uri = get_redis_rw_uri();
+      let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+      let mut con = redis_client.get_connection().unwrap();
 
-        match result {
-          Some(doc_) => { 
-            followee_ids = doc_.followees.iter().map(|x| x.followee_id).collect();
-            // insert to redis
-            let mut new_compound: Vec<(i64, String)> = doc_.followees.iter().map(|x| (x.timestamp, x.followee_id.to_string())).collect();
-            let mut new_compound_slice: Vec<(i64, &str)> = new_compound.iter().map(|x| (x.0, &x.1[..]) ).collect();
-            let res: isize = con.zadd_multiple(&user_id_str[..], &new_compound_slice).unwrap();
-          }
-          None => {
-            println!("user_id: {} not found", user_id);
-            panic!("user_id: {} not found", user_id);
-          }
-        }
+      let mut real_name: String = "social-graph:".to_string();
+      real_name.push_str(&(user_id.to_string()));
+      let followees_str_redis_result: redis::RedisResult<String> = con.hget(&real_name[..], "followees");
+      match followees_str_redis_result {
+        Ok(followees_str) => {
+          return_value = followees_str;
+        },
+        Err(_) => {
+          println!("user_id: {} not found", user_id);
+          panic!("user_id: {} not found", user_id);
+        },
       }
-    }
-    Err(e) => {
-      println!("cannnot connect with Redis");
-      panic!("cannnot connect with Redis");
-    }
+    },
   }
-  
-  let serialized = serde_json::to_string(&followee_ids).unwrap();
-//  let new_now =  Instant::now();
-//  println!("{:?}", new_now.duration_since(now));
-  send_return_value_to_caller(serialized);
+
+  let new_now =  Instant::now();
+  println!("{:?}", new_now.duration_since(now));
+  send_return_value_to_caller(return_value);
 }
 
