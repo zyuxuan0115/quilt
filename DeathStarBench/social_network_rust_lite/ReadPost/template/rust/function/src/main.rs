4c5eb75d827c7a6b2ac39641bf010492c161ce3e
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-15 14:39:27 -0400

    update

--- DeathStarBench/social_network_rust_lite/ReadPost/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust_lite/ReadPost/template/rust/function/src/main.rs
@@ -8,7 +8,7 @@ use redis::{Commands};
 
 fn main() {
   let input: String = get_arg_from_caller();
-//  let time_0 = Instant::now();
+  let time_0 = Instant::now();
   let post_id: i64 = serde_json::from_str(&input).unwrap();
 
   let redis_uri = get_redis_rw_uri();
@@ -22,20 +22,35 @@ fn main() {
     = redis::cmd("HMGET").arg("post_id").arg("creator").arg("text").arg("user_mentions")
                          .arg("media").arg("urls").arg("timestamp").arg("post_type")
                          .query(&mut con);
+  let mut result_str: String = String::new();
   match result {
     Ok((post_id, creator, text, user_mentions, media, urls, timestamp, post_type)) => {
       // insert to memcached
       let memcache_uri = get_memcached_uri();
       let memcache_client = memcache::connect(&memcache_uri[..]).unwrap();
       let post_id_str: String = post_id.to_string();
-      //memcache_client.set(&post_id_str[..], &return_result[..], 0).unwrap();
+
+      let post = Post {
+        post_id: post_id,
+        creator: serde_json::from_str(&creator).unwrap(),
+        text: text,
+        user_mentions: serde_json::from_str(&user_mentions).unwrap(),
+        media: serde_json::from_str(&media).unwrap(),
+        urls: serde_json::from_str(&urls).unwrap(),
+        timestamp: timestamp,
+        post_type: serde_json::from_str(&post_type).unwrap(),
+      };
+
+      result_str = serde_json::to_string(&post).unwrap();
+      memcache_client.set(&post_id_str[..], &result_str[..], 0).unwrap();
     },
-    Err(_){
+    Err(_) => {
       println!("Post_id:{} doesn't exist in MongoDB", post_id);
       panic!("Post_id:{} doesn't exist in MongoDB", post_id);
     },
-  }
-//  let time_1 = Instant::now();
-//  println!("{:?}", time_1.duration_since(time_0));
-  send_return_value_to_caller(return_result);
+  };
+
+  let time_1 = Instant::now();
+  println!("{:?}", time_1.duration_since(time_0));
+  send_return_value_to_caller(result_str);
 }
