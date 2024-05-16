b81e1fbf54b8ffa94c6fad194a33d9c16906ae0b
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-16 11:23:55 -0400

    update

--- DeathStarBench/social_network_rust_lite/ReadHomeTimeline/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust_lite/ReadHomeTimeline/template/rust/function/src/main.rs
@@ -2,28 +2,47 @@ use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::{fs::read_to_string, collections::HashMap, time::{SystemTime,Duration, Instant}};
-use redis::{Commands};
+use memcache::Client as memcached_client;
 
 fn main() {
   let input: String = get_arg_from_caller();
-  let now = Instant::now();
+ // let now = Instant::now();
   let mut timeline_info: ReadTimelineArgs = serde_json::from_str(&input).unwrap();
 
-  let user_id_str: String = timeline_info.user_id.to_string();
+  let mut user_id_str: String = timeline_info.user_id.to_string();
+  user_id_str.push_str(":home-timeline");
 
-  // update redis
-  let redis_uri = get_redis_rw_uri();
-  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
-  let mut con = redis_client.get_connection().unwrap();
+  // update memcached
+  let memcache_uri = get_memcached_uri();
+  let memcache_client = memcache::connect(&memcache_uri[..]).unwrap();
 
-  let res: Vec<String> = con.zrevrange(&user_id_str[..], timeline_info.start as isize, (timeline_info.stop) as isize).unwrap();
+  let post_id_and_timestamp_str: String = memcache_client.get(&user_id_str[..]).unwrap().unwrap();
+  println!("{}",post_id_and_timestamp_str);
+
+  let mut post_id_and_timestamp: Vec<(i64, i64)> = serde_json::from_str(&post_id_and_timestamp_str).unwrap();
+  post_id_and_timestamp.sort_by(|a, b| b.1.cmp(&a.1));
+
+  if timeline_info.start<0 {timeline_info.start = 0;}
+  if timeline_info.stop>=post_id_and_timestamp.len() as i64 {
+      timeline_info.stop = post_id_and_timestamp.len() as i64 -1;
+  }
+
+  let post_ids: Vec<i64> = post_id_and_timestamp.into_iter().map(|(k,_)| k).collect();
+  let mut post_id_slice: Vec<i64> = Vec::new(); 
+  if timeline_info.start != timeline_info.stop {
+    post_id_slice = post_ids[timeline_info.start as usize .. timeline_info.stop as usize].to_owned();
+  }
+  else {
+    let post_id = post_ids[timeline_info.start as usize];
+    post_id_slice.push(post_id);
+  }
   
-  let post_ids: Vec<i64> = res.iter().map(|x| x[..].parse::<i64>().unwrap()).collect();
-  let post_ids_str = serde_json::to_string(&post_ids).unwrap(); 
+  let post_ids_str = serde_json::to_string(&post_id_slice).unwrap(); 
+
+  //let new_now =  Instant::now();
+  //println!("{:?}", new_now.duration_since(now));
 
   let result = make_rpc("read-posts", post_ids_str);
-  let new_now =  Instant::now();
-  println!("{:?}", new_now.duration_since(now));
   send_return_value_to_caller(result);
 }
 
