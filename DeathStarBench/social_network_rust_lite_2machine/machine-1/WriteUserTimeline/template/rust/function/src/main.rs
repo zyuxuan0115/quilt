75d0bafc94af0d7d37ac310068f078bfdc0802cf
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-21 22:27:39 -0400

    update

--- DeathStarBench/social_network_rust_lite_2machine/machine-1/WriteUserTimeline/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust_lite_2machine/machine-1/WriteUserTimeline/template/rust/function/src/main.rs
@@ -7,7 +7,7 @@ use memcache::Client as memcached_client;
 
 fn main() {
   let input: String = get_arg_from_caller();
-  let now = Instant::now();
+  //let now = Instant::now();
   let timeline_info: WriteUserTimelineArgs = serde_json::from_str(&input).unwrap();
 
   // connect to redis
@@ -22,8 +22,8 @@ fn main() {
   let post_id_str = timeline_info.post_id.to_string();
   let res: isize = con.zadd(&real_name[..], &post_id_str[..], timeline_info.timestamp).unwrap();
   
-  let new_now =  Instant::now();
-  println!("{:?}", new_now.duration_since(now));
+  //let new_now =  Instant::now();
+  //println!("{:?}", new_now.duration_since(now));
 
   send_return_value_to_caller("".to_string());
 }
