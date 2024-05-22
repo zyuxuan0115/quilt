cfa67a974f212858193f9ba03aa1964bf4d44461
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-22 12:31:38 -0400

    update

---
+++ openfaas-test/wrk2/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+WRK_BIN=./wrk
+WRK_SCRIPT=compose-post.lua
+ENTRY_HOST=130.127.133.16
+QPS=1000
+
+$WRK_BIN -t 4 -c 48 -d 30 -L -U \
+	 -s $WRK_SCRIPT \
+	 http://$ENTRY_HOST:8080/function/compose-post -R $QPS 2>/dev/null > wrk_warmup.log
