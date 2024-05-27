b689ce9f6a963e07ae73110052b933f1f4358fc6
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-27 17:05:27 -0400

    update

--- DeathStarBench/social_network_rust_lite_2machine_merge_test/merge/merge.sh
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/merge/merge.sh
@@ -27,37 +27,38 @@ function merge {
   && cp -r DbInterface $CALLEE_FUNC/template/rust \
   && cd $CALLER_FUNC/template/rust/function \
   && RUSTFLAGS="--emit=llvm-ir" cargo build \
-  && cd ../../../../$CALLEE_FUNC/template/rust/function \
+  && cd ../../../../ && mv target $(CALLER}_ir \ 
+  && cd $CALLEE_FUNC/template/rust/function \
   && RUSTFLAGS="--emit=llvm-ir" cargo build \
-  && cd ../../../../
+  && cd ../../../../ && mv target ${CALLEE}_ir
 
-  CALLEE_FUNC_LL=$(echo $CALLEE_FUNC | tr '-' '_') 
-  CALLEE_IR=$(ls $CALLEE_FUNC/template/rust/function/target/debug/deps/$CALLEE_FUNC_LL-*.ll)
-  echo $CALLER_IR
-  $LLVM_DIR/opt -S $CALLEE_IR -passes=merge-rust-func -rename-callee-rr -o callee_rename.ll
-  cp $CALLEE_FUNC/template/rust/function/target/debug/deps/*.ll $CALLER_FUNC/template/rust/function/target/debug/deps
-  cp $CALLEE_FUNC/template/rust/function/target/debug/deps/*.so $CALLER_FUNC/template/rust/function/target/debug/deps
-  cp callee_rename.ll $CALLER_FUNC/template/rust/function/target/debug/deps
-  rm -rf $CALLER_FUNC/template/rust/function/target/debug/deps/$CALLEE_FUNC_LL-*.ll
-  $LLVM_DIR/llvm-link $CALLER_FUNC/template/rust/function/target/debug/deps/*.ll -S -o merge.ll
-  $LLVM_DIR/opt merge.ll -strip-debug -o merge_nodebug.ll -S
-  $LLVM_DIR/opt -S merge_nodebug.ll -passes=merge-rust-func -o merge_new.ll
-  $LLVM_DIR/llc -filetype=obj merge_new.ll -o function.o
+#  CALLEE_FUNC_LL=$(echo $CALLEE_FUNC | tr '-' '_') 
+#  CALLEE_IR=$(ls $CALLEE_FUNC/template/rust/function/target/debug/deps/$CALLEE_FUNC_LL-*.ll)
+#  echo $CALLER_IR
+#  $LLVM_DIR/opt -S $CALLEE_IR -passes=merge-rust-func -rename-callee-rr -o callee_rename.ll
+#  cp $CALLEE_FUNC/template/rust/function/target/debug/deps/*.ll $CALLER_FUNC/template/rust/function/target/debug/deps
+#  cp $CALLEE_FUNC/template/rust/function/target/debug/deps/*.so $CALLER_FUNC/template/rust/function/target/debug/deps
+#  cp callee_rename.ll $CALLER_FUNC/template/rust/function/target/debug/deps
+#  rm -rf $CALLER_FUNC/template/rust/function/target/debug/deps/$CALLEE_FUNC_LL-*.ll
+#  $LLVM_DIR/llvm-link $CALLER_FUNC/template/rust/function/target/debug/deps/*.ll -S -o merge.ll
+#  $LLVM_DIR/opt merge.ll -strip-debug -o merge_nodebug.ll -S
+#  $LLVM_DIR/opt -S merge_nodebug.ll -passes=merge-rust-func -o merge_new.ll
+#  $LLVM_DIR/llc -filetype=obj merge_new.ll -o function.o
 
-  STATIC_RING_LIB_DIR=$(find $CALLER_FUNC/template/rust/function/target/debug/build/ -type d -name ring-*)
-  STATIC_RING_LIBS=""
-  for entry in $STATIC_RING_LIB_DIR
-  do 
-    for dir in $entry/*
-    do
-      BASE_NAME=$(basename $dir)
-      if [[ "$BASE_NAME" = "out" ]] ; then
-        STATIC_RING_LIBS=$(ls $dir/libring_*.a)
-      fi
-    done
-  done
+#  STATIC_RING_LIB_DIR=$(find $CALLER_FUNC/template/rust/function/target/debug/build/ -type d -name ring-*)
+#  STATIC_RING_LIBS=""
+#  for entry in $STATIC_RING_LIB_DIR
+#  do 
+#    for dir in $entry/*
+#    do
+#      BASE_NAME=$(basename $dir)
+#      if [[ "$BASE_NAME" = "out" ]] ; then
+#        STATIC_RING_LIBS=$(ls $dir/libring_*.a)
+#      fi
+#    done
+#  done
 
-  $LLVM_DIR/clang -no-pie -L$RUST_LIB function.o $STATIC_LINKER_FLAGS -o function $LINKER_FLAGS $STATIC_RING_LIBS
+#  $LLVM_DIR/clang -no-pie -L$RUST_LIB function.o $STATIC_LINKER_FLAGS -o function $LINKER_FLAGS $STATIC_RING_LIBS
 }
 
 function clean {
