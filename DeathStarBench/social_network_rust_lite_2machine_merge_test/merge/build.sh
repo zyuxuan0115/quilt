8f162a601de82f0d3b635fb82b4d2815e7d43e76
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-27 11:37:20 -0400

    update

--- DeathStarBench/social_network_rust_lite_2machine_merge_test/merge/build.sh
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/merge/build.sh
@@ -4,29 +4,53 @@
 ROOT_DIR=$(pwd)
 echo $ROOT_DIR
 
-function build_rust_test {
-    sudo docker build --no-cache -t zyuxuan0115/llvm-17:latest \
+function build_llvm {
+  sudo docker build --no-cache -t zyuxuan0115/llvm-17:latest \
         -f Dockerfile.llvm \
         .
 }
 
-function push_rust_test {
-    sudo docker push zyuxuan0115/llvm-17:latest
+function build_merge {
+  CALLER=$1
+  CALLEE=$2
+  mkdir temp
+  cp -r ../machine-1/$CALLER temp
+  cp -r ../machine-1/$CALLEE temp
+  cp -r ../OpenFaaSRPC temp
+  cp -r ../DbInterface temp
+  sudo docker build --no-cache -t zyuxuan0115/deathstartbench-$CALLER_NAME-merged:latest \
+    -f Dockerfile.merge \
+    temp
+  rm -rf temp
+}
+
+function test_build {
+  echo $ROOT_DIR
+}
+
+function push_llvm {
+  sudo docker push zyuxuan0115/llvm-17:latest
 }
 
 function build {
-    build_rust_test
+  build_llvm
 }
 
 function push {
-    push_rust_test
+  push_llvm
 }
 
 case "$1" in
-build)
-    build
+llvm)
+    build_llvm
+    ;;
+merge)
+    build_merge $2 $3
     ;;
 push)
-    push
+    push_llvm
+    ;;
+test)
+    test_build
     ;;
 esac
