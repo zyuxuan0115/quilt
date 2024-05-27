6e9b794579a374713c090f03b298605276bdbbed
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-27 11:06:35 -0400

    update

--- DeathStarBench/social_network_rust_lite_2machine_merge_test/merge/build.sh
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/merge/build.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(pwd)
 echo $ROOT_DIR
 
 function build_rust_test {
-    sudo docker build -t zyuxuan0115/llvm-17:latest \
+    sudo docker build --no-cache -t zyuxuan0115/llvm-17:latest \
         -f Dockerfile.llvm \
         .
 }
