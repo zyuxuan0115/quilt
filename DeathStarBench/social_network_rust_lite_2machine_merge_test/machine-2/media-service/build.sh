df14fb7b50791ec93c60f328f1199125659f4700
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-29 10:35:47 -0400

    update

--- DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-2/media-service/build.sh
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-2/media-service/build.sh
@@ -6,7 +6,7 @@ echo $ROOT_DIR
 
 function build_rust_test {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/deathstarbench-mediaservice:latest \
+    sudo docker build --no-cache -t zyuxuan0115/deathstarbench-mediaservice:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
--- DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-2/register-user/build.sh
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-2/register-user/build.sh
@@ -7,7 +7,7 @@ echo $ROOT_DIR
 function build_rust_test {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/deathstarbench-registeruser:latest \
+    sudo docker build --no-cache -t zyuxuan0115/deathstarbench-registeruser:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
