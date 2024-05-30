6f57560a9a4ecbc0fea0278a74e461f8dba5a215
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-05-29 21:20:34 -0400

    update

--- DeathStarBench/social_network_rust_lite_2machine_merge_test/merge/build.sh
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/merge/build.sh
@@ -22,11 +22,11 @@ function build_merge {
   cp -r ../OpenFaaSRPC temp
   cp -r ../DbInterface temp
   cp merge.sh temp
-  sudo docker build --no-cache -t zyuxuan0115/deathstartbench-$CALLER-tmp:latest \
+  sudo docker build --no-cache -t zyuxuan0115/deathstarbench-$CALLER-merged:latest \
     -f Dockerfile.merge \
     temp
   rm -rf temp
-  sudo docker push zyuxuan0115/deathstartbench-$CALLER-tmp:latest
+  sudo docker push zyuxuan0115/deathstarbench-$CALLER-merged:latest
 }
 
 function test_build {
