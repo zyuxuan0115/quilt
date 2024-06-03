6b24fdf37b4607a0a6ac0880bd3ce5fbbe75a0ba
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-06-03 18:38:30 -0400

    update

--- merge_func/merge-rust-func/llvm-pass/MergeRustFunc.cpp
+++ merge_func/merge-rust-func/llvm-pass/MergeRustFunc.cpp
@@ -74,11 +74,11 @@ PreservedAnalyses MergeRustFuncPass::run(Module &M,
     mainFunc->setName("old_main");
     NewMainFunc->setName("main");
 
-//    InvokeInst* OutputFuncCall = findInvokeByCalleePrefix(NewMainFunc, "OpenFaaSRPC::send_return_value_to_caller");
-//    llvm::Type *i32_type = llvm::IntegerType::getInt32Ty(M.getContext());
-//    llvm::Constant *i32_val = llvm::ConstantInt::get(i32_type, 0/*value*/, true);
-//    ReturnInst* newRet = ReturnInst::Create(M.getContext(), i32_val, OutputFuncCall);
-
+    InvokeInst* OutputFuncCall = findInvokeByCalleePrefix(NewMainFunc, "OpenFaaSRPC::send_return_value_to_caller");
+    llvm::Type *i32_type = llvm::IntegerType::getInt32Ty(M.getContext());
+    llvm::Constant *i32_val = llvm::ConstantInt::get(i32_type, 0/*value*/, true);
+    ReturnInst* newRet = ReturnInst::Create(M.getContext(), i32_val, OutputFuncCall);
+    OutputFuncCall->eraseFromParent();
   }
   else {
     Function *mainFunc = M.getFunction("main");
