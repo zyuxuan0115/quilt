3a690bd83389a90803e38531fb8e14f2e3978259
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-06-08 09:21:00 -0400

    update

--- DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-1/merge2.sh
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-1/merge2.sh
@@ -41,30 +41,37 @@ function merge {
   && RUSTFLAGS="--emit=llvm-ir" cargo build \
   && cd ../../../../$CALLEE_FUNC1/template/rust/function \
   && RUSTFLAGS="--emit=llvm-ir" cargo build \
-  && cd ../../../../$CALLEE_FUNC1/template/rust/function \
+  && cd ../../../../$CALLEE_FUNC2/template/rust/function \
   && RUSTFLAGS="--emit=llvm-ir" cargo build \
-
   && cd ../../../../
-
-
-<<'###BLOCK-COMMENT'
 
   # prepare for merging
   CALLEE_FUNC_LL=$(echo $CALLEE_FUNC | tr '-' '_') 
-  CALLEE_IR=$(ls $CALLEE_FUNC/template/rust/function/target/debug/deps/function-*.ll)
   CALLER_IR=$(ls $CALLER_FUNC/template/rust/function/target/debug/deps/function-*.ll)
-  $LLVM_DIR/opt -S $CALLER_IR -passes=merge-rust-func -rename-caller-rr -o caller.ll
-  $LLVM_DIR/opt -S $CALLEE_IR -passes=merge-rust-func -rename-callee-rr -o callee.ll
-  mv $CALLEE_IR old_callee_ir.ll
-  mv $CALLER_IR old_caller_ir.ll
+  CALLEE_IR1=$(ls $CALLEE_FUNC1/template/rust/function/target/debug/deps/function-*.ll)
+  CALLEE_IR2=$(ls $CALLEE_FUNC2/template/rust/function/target/debug/deps/function-*.ll)
+  mv $CALLER_IR caller.ll
+#  $LLVM_DIR/opt -S $CALLER_IR -passes=merge-rust-func -rename-caller-rr -o caller.ll
+  $LLVM_DIR/opt -S $CALLEE_IR1 -passes=merge-rust-func -rename-callee-rr -o callee1.ll
+  $LLVM_DIR/opt -S $CALLEE_IR2 -passes=merge-rust-func -rename-callee-rr -o callee2.ll
+#  mv $CALLER_IR old_caller_ir.ll
+  mv $CALLEE_IR1 old_callee_ir1.ll
+  mv $CALLEE_IR2 old_callee_ir2.ll
 
   # merge caller and callee
-  $LLVM_DIR/llvm-link caller.ll callee.ll -S -o caller_and_callee.ll
+  $LLVM_DIR/llvm-link caller.ll callee1.ll -S -o caller_and_callee.ll
   $LLVM_DIR/opt caller_and_callee.ll -strip-debug -o caller_and_callee_nodebug.ll -S
-  $LLVM_DIR/opt -S caller_and_callee_nodebug.ll -passes=merge-rust-func -callee-name-rr=social-graph-get-followers -o merged.ll
+  $LLVM_DIR/opt -S caller_and_callee_nodebug.ll -passes=merge-rust-func -callee-name-rr=$CALLEE_FUNC1 -o merged1.ll
+  
+  $LLVM_DIR/llvm-link merged1.ll callee2.ll -S -o caller_and_callee.ll
+  $LLVM_DIR/opt caller_and_callee.ll -strip-debug -o caller_and_callee_nodebug.ll -S
+  $LLVM_DIR/opt -S caller_and_callee_nodebug.ll -passes=merge-rust-func -callee-name-rr=$CALLEE_FUNC2 -o merged.ll
 
   # merge the rest lib code  
-  cp $CALLEE_FUNC/template/rust/function/target/debug/deps/*.ll $CALLER_FUNC/template/rust/function/target/debug/deps
+  cp $CALLEE_FUNC1/template/rust/function/target/debug/deps/*.ll $CALLER_FUNC/template/rust/function/target/debug/deps
+  cp $CALLEE_FUNC2/template/rust/function/target/debug/deps/*.ll $CALLER_FUNC/template/rust/function/target/debug/deps
+  cp -r $CALLEE_FUNC1/template/rust/function/target/debug/build/* $CALLER_FUNC/template/rust/function/target/debug/build/
+  cp -r $CALLEE_FUNC2/template/rust/function/target/debug/build/* $CALLER_FUNC/template/rust/function/target/debug/build/
   $LLVM_DIR/llvm-link $CALLER_FUNC/template/rust/function/target/debug/deps/*.ll -S -o lib_with_debug_info.ll
   $LLVM_DIR/opt lib_with_debug_info.ll -strip-debug -o lib.ll -S
 
@@ -85,6 +92,7 @@ function merge {
   done
 
   $LLVM_DIR/clang -no-pie -L$RUST_LIB function.o $STATIC_LINKER_FLAGS -o function $LINKER_FLAGS $STATIC_RING_LIBS
+<<'###BLOCK-COMMENT'
 
 ###BLOCK-COMMENT
 }
@@ -92,12 +100,15 @@ function merge {
 function clean {
   rm -rf $CALLER_FUNC/template/rust/OpenFaaSRPC \
   && rm -rf $CALLER_FUNC/template/rust/DbInterface \
-  && rm -rf $CALLEE_FUNC/template/rust/OpenFaaSRPC \
-  && rm -rf $CALLEE_FUNC/template/rust/DbInterface \
+  && rm -rf $CALLEE_FUNC1/template/rust/OpenFaaSRPC \
+  && rm -rf $CALLEE_FUNC1/template/rust/DbInterface \
+  && rm -rf $CALLEE_FUNC2/template/rust/OpenFaaSRPC \
+  && rm -rf $CALLEE_FUNC2/template/rust/DbInterface \
   && cd $CALLER_FUNC/template/rust/function && cargo clean \
-  && cd ../../../../$CALLEE_FUNC/template/rust/function && cargo clean \
+  && cd ../../../../$CALLEE_FUNC1/template/rust/function && cargo clean \
+  && cd ../../../../$CALLEE_FUNC2/template/rust/function && cargo clean \
   && cd ../../../../
-  rm -rf *.ll *.o function
+  rm -rf *.ll *.o function *.txt
 }
 
 case "$1" in
