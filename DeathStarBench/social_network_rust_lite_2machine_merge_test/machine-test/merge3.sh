66f9c16c158d50722fe7260c4661cecfc62bf2d2
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-06-09 12:10:07 -0400

    update

---
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-test/merge3.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+LLVM_DIR=/proj/zyuxuanssf-PG0/llvm-project-17/build/bin
+RUST_LIB=/users/zyuxuan/.rustup/toolchains/1.76-x86_64-unknown-linux-gnu/lib
+CODE_LIB=/proj/zyuxuanssf-PG0/faas-test/DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-1/write-home-timeline/template/rust/function/target/debug/deps
+RUST_LIBSTD_PATH=$(ls $RUST_LIB/libstd-*.so)
+RUST_LIBSTD_NAME=$(basename $RUST_LIBSTD_PATH)
+RUST_LIBSTD_LINKER_FLAG=${RUST_LIBSTD_NAME#"libstd"}
+RUST_LIBSTD_LINKER_FLAG=${RUST_LIBSTD_LINKER_FLAG%".so"}
+
+RUST_LIBRUSTC_PATH=$(ls $RUST_LIB/librustc_driver-*.so)
+RUST_LIBRUSTC_NAME=$(basename $RUST_LIBRUSTC_PATH)
+RUST_LIBRUSTC_LINKER_FLAG=${RUST_LIBRUSTC_NAME#"librustc_driver"}
+RUST_LIBRUSTC_LINKER_FLAG=${RUST_LIBRUSTC_LINKER_FLAG%".so"}
+
+RUST_LIBTEST_PATH=$(ls $RUST_LIB/libtest-*.so)
+RUST_LIBTEST_NAME=$(basename $RUST_LIBTEST_PATH)
+RUST_LIBTEST_LINKER_FLAG=${RUST_LIBTEST_NAME#"libtest"}
+RUST_LIBTEST_LINKER_FLAG=${RUST_LIBTEST_LINKER_FLAG%".so"}
+
+LINKER_FLAGS="-lstd$RUST_LIBSTD_LINKER_FLAG -lcurl -lcrypto -lm -lssl -lz -lrustc_driver$RUST_LIBRUSTC_LINKER_FLAG -ltest$RUST_LIBTEST_LINKER_FLAG "
+
+ARGS=("$@")
+NUM_ARGS=$#
+CALLER_FUNC=${ARGS[1]}
+
+
+function merge {
+  for i in $(seq 1 $(($NUM_ARGS-1)) );
+  do
+    FUNC_NAME=${ARGS[$i]}
+    cp -r ../OpenFaaSRPC $FUNC_NAME/template/rust \
+    && cp -r ../DbInterface $FUNC_NAME/template/rust \
+    && cd $FUNC_NAME/template/rust/function \
+    && RUSTFLAGS="--emit=llvm-ir" cargo build \
+    && cd ../../../../
+  done
+
+  # prepare for merging
+  CALLEE_FUNC_LL=$(echo $CALLEE_FUNC | tr '-' '_') 
+  CALLER_IR=$(ls $CALLER_FUNC/template/rust/function/target/debug/deps/function-*.ll)
+  mv $CALLER_IR caller.ll
+  cp caller.ll merged.ll
+
+  for i in $(seq 2 $(($NUM_ARGS-1)) );
+  do
+    CALLEE_FUNC=${ARGS[$i]}
+    CALLEE_IR=$(ls $CALLEE_FUNC/template/rust/function/target/debug/deps/function-*.ll)
+    $LLVM_DIR/opt -S $CALLEE_IR -passes=merge-rust-func -rename-callee-rr -o callee.ll
+    mv $CALLEE_IR old_callee_ir_$i.ll
+    $LLVM_DIR/llvm-link merged.ll callee.ll -S -o caller_and_callee.ll
+    $LLVM_DIR/opt caller_and_callee.ll -strip-debug -o caller_and_callee_nodebug.ll -S
+    $LLVM_DIR/opt -S caller_and_callee_nodebug.ll -passes=merge-rust-func -callee-name-rr=$CALLEE_FUNC -o merged.ll
+    cp $CALLEE_FUNC/template/rust/function/target/debug/deps/*.ll $CALLER_FUNC/template/rust/function/target/debug/deps
+    cp -r $CALLEE_FUNC/template/rust/function/target/debug/build/* $CALLER_FUNC/template/rust/function/target/debug/build/
+  done
+
+  $LLVM_DIR/llvm-link $CALLER_FUNC/template/rust/function/target/debug/deps/*.ll -S -o lib_with_debug_info.ll
+  $LLVM_DIR/opt lib_with_debug_info.ll -strip-debug -o lib.ll -S
+
+  $LLVM_DIR/llvm-link lib.ll merged.ll -S -o function.ll
+  $LLVM_DIR/llc -filetype=obj function.ll -o function.o
+
+  STATIC_RING_LIB_DIR=$(find $CALLER_FUNC/template/rust/function/target/debug/build/ -type d -name ring-*)
+  STATIC_RING_LIBS=""
+  for entry in $STATIC_RING_LIB_DIR
+  do 
+    for dir in $entry/*
+    do
+      BASE_NAME=$(basename $dir)
+      if [[ "$BASE_NAME" = "out" ]] ; then
+        STATIC_RING_LIBS=$(ls $dir/libring_*.a)
+      fi
+    done
+  done
+
+  $LLVM_DIR/clang -no-pie -L$RUST_LIB function.o $STATIC_LINKER_FLAGS -o function $LINKER_FLAGS $STATIC_RING_LIBS
+<<'###BLOCK-COMMENT'
+
+###BLOCK-COMMENT
+}
+
+function clean {
+  for i in $(seq 1 $(($NUM_ARGS-1)) );
+  do
+    FUNC_NAME=${ARGS[$i]}
+    rm -rf $FUNC_NAME/template/rust/OpenFaaSRPC \
+    && rm -rf $FUNC_NAME/template/rust/DbInterface \
+    && cd $FUNC_NAME/template/rust/function && cargo clean \
+    && cd ../../../../
+  done
+  rm -rf *.ll *.o function *.txt
+}
+
+case "$1" in
+merge)
+    merge
+    ;;
+clean)
+    clean
+    ;;
+esac
