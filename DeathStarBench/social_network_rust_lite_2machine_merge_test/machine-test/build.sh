f75b056ba628f2809bad837b755c766e82129762
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-06-09 16:37:30 -0400

    merge multiple layers

---
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-test/build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+  
+search_dir=$(pwd)
+
+function build {
+  for entry in "$search_dir"/*
+  do
+    BASE_NAME=$(basename $entry)
+    if [[ "$BASE_NAME" = "build.sh" ]] ; then 
+      continue
+    elif [[ "$BASE_NAME" = "merge.sh" ]] ; then 
+      continue
+    else
+      cd $entry
+      ./build.sh build
+      ./build.sh push
+    fi
+  done
+}
+
+function deploy {
+  for entry in "$search_dir"/*
+  do
+    BASE_NAME=$(basename $entry)
+    if [[ "$BASE_NAME" = "build.sh" ]] ; then 
+      continue
+    elif [[ "$BASE_NAME" = "merge.sh" ]] ; then
+      continue
+    else
+      cd $entry
+      YAML_FILE=$(ls *.yml)
+      faas-cli deploy -f deployFunc.yml
+    fi
+  done
+}
+
+
+function nuke {
+  for entry in "$search_dir"/*
+  do
+    BASE_NAME=$(basename $entry)
+    if [[ "$BASE_NAME" = "build.sh" ]] ; then
+      continue
+    elif [[ "$BASE_NAME" = "merge.sh" ]] ; then
+      continue
+    else
+      cd $entry
+      faas-cli remove $entry
+      cd ..
+    fi
+  done
+  sudo docker image rm -f $(sudo docker images -aq)
+  sudo docker system prune
+}
+
+
+
+
+case "$1" in
+build)
+    build
+    ;;
+deploy)
+    deploy
+    ;;
+nuke)
+    nuke
+    ;;
+esac
