78f90d8618dbfbb216882edbf29c147d66f02045
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-06-25 17:00:34 -0400

    update

--- DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-2/compose-creator-with-username/build.sh
+++ DeathStarBench/social_network_rust_lite_2machine_merge_test/machine-2/compose-creator-with-username/build.sh
@@ -7,7 +7,7 @@ echo $ROOT_DIR
 function build_rust_test {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/deathstarbench-composecreatorwithusername:latest \
+    sudo docker build --no-cache -t zyuxuan0115/sn-compose-creator-with-username:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
@@ -15,7 +15,7 @@ function build_rust_test {
 }
 
 function push_rust_test {
-    sudo docker push zyuxuan0115/deathstarbench-composecreatorwithusername:latest
+    sudo docker push zyuxuan0115/sn-compose-creator-with-username:latest
 }
 
 function build {
