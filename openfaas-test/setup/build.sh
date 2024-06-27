c2f98c82a106e3d223b2c6376451e31db9466053
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-06-27 10:00:37 -0400

    update

--- openfaas-test/setup/build.sh
+++ openfaas-test/setup/build.sh
@@ -92,14 +92,15 @@ EOF
 }
 
 function setup_ingress_nginx {
-  kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/baremetal/deploy.yaml
+  NGINX_YAML=https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/baremetal/deploy.yaml 
+  curl $NGINX_YAML | python3 gen_yaml.py nginx
+
+  kubectl apply -f $NGINX_YAML
   while ! kubectl get secret ingress-nginx-admission --namespace ingress-nginx; 
   do 
     echo "Waiting for ingress-nginx-admission. CTRL-C to exit."; 
     sleep 10;
   done 
-
-  curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/baremetal/deploy.yaml | python3 gen_yaml.py nginx
 
   kubectl apply -f ingress-nginx-values.yaml 
   kubectl wait --namespace ingress-nginx \
@@ -150,6 +151,60 @@ EOF
 
 }
 
+function setup_ingress_nginx2 {
+  kubectl apply -f ingress-nginx2.yaml
+  while ! kubectl get secret ingress-nginx-admission --namespace ingress-nginx2; 
+  do 
+    echo "Waiting for ingress-nginx-admission. CTRL-C to exit."; 
+    sleep 10;
+  done 
+#  kubectl apply -f ingress-nginx2-values.yaml
+  kubectl apply -f new.yaml
+  echo '
+    apiVersion: v1
+    kind: ConfigMap
+    data:
+      enable-opentelemetry: "true"
+      opentelemetry-config: "/etc/ingress-controller/telemetry/opentelemetry.toml"
+      opentelemetry-trust-incoming-span: "true"
+      otlp-collector-host: "otel-collector-opentelemetry-collector.sn-otel.svc.cluster.local"
+      otlp-collector-port: "4317"
+      otel-schedule-delay-millis: "5000"
+      otel-max-export-batch-size: "512"
+      otel-sampler: "AlwaysOn"
+      otel-sampler-ratio: "1.0"
+      otel-sampler-parent-based: "false"
+    metadata:
+      name: ingress-nginx-controller
+      namespace: ingress-nginx2
+  ' | kubectl replace -f -
+
+  kubectl wait --namespace ingress-nginx2 \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=120s
+
+  kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: ingress-nginx-controller
+  namespace: ingress-nginx2
+spec:
+  type: NodePort
+  ports:
+    - port: 20080
+      nodePort: 30081
+      name: http
+    - port: 20443
+      nodePort: 30442
+      name: https  
+EOF
+
+
+
+}
+
 function setup_openfaas {
   arkade install openfaas --max-inflight 8 --queue-workers 4 
   kubectl rollout status -n openfaas deploy/gateway
@@ -195,12 +250,32 @@ function setup_openfaas2 {
   mv openfaas2.yaml ../
   cd ..
   rm -rf faas-netes
-  curl https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml | python3 gen_yaml.py
+  curl https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.yml | python3 gen_yaml.py openfaas
   kubectl apply -f openfaas2.yml,openfaas2.yaml
 
   kubectl rollout status -n openfaas2 deploy/gateway
   kubectl port-forward -n openfaas2 svc/gateway 8081:8080 &
   kubectl get svc -o wide gateway-external -n openfaas2
+
+  kubectl -n openfaas2 apply -f - <<EOF
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: ingress-gateway2
+spec:
+  ingressClassName: nginx2
+  rules:
+  - http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service:
+            name: gateway
+            port:
+              number: 8081
+EOF
+
 }
 
 function setup_db {  
@@ -230,6 +305,7 @@ function setup {
   setup_grafana_tempo
   setup_otel
   setup_ingress_nginx
+  setup_ingress_nginx2
   setup_openfaas
   setup_openfaas2
   setup_db # mongodb, redis and memcached
@@ -240,10 +316,10 @@ function setup {
 function killa {
   ssh -q $SERVER_HOST -- sudo sh /usr/local/bin/k3s-killall.sh
   ssh -q $SERVER_HOST -- sudo sh /usr/local/bin/k3s-uninstall.sh
-  ssh -q $SERVER_HOST -- npx kill-port 30080 6379 27017 11211 30081 3000 
+  ssh -q $SERVER_HOST -- npx kill-port 30080 6379 27017 11211 30081 3000 30443 30442
   ssh -q $AGENT_HOST -- sudo sh /usr/local/bin/k3s-killall.sh
   ssh -q $AGENT_HOST -- sudo sh /usr/local/bin/k3s-agent-uninstall.sh
-  ssh -q $AGENT_HOST -- npx kill-port 30080  6379 27017 11211 30081 3000 
+  ssh -q $AGENT_HOST -- npx kill-port 30080  6379 27017 11211 30081 3000 30443 30442
   rm -rf *.txt kubeconfig
 }
 
