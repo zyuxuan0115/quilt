b5a032896eb8d8c8aedd0a6f8cce3d0ef1a85496
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-06-30 16:06:56 -0400

    update

--- openfaas-test/call-graph-gen/call-graph-gen.py
+++ openfaas-test/call-graph-gen/call-graph-gen.py
@@ -4,70 +4,69 @@ import sys
 import subprocess
 import select
 
-p = subprocess.Popen(["curl", "-G", "-s", "http://localhost:8082/api/search", 
-                      "--data-urlencode", "tags=service.name=nginx", 
-                      "--data-urlencode", "tags=http.method=POST"],
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+cmd = "curl -G -s http://localhost:8082/api/search --data-urlencode 'tags=service.name=nginx' --data-urlencode 'tags=http.method=POST' "
+process = subprocess.Popen(cmd, shell=True,
+                           stdout=subprocess.PIPE, 
+                           stderr=subprocess.PIPE)
 
-stdout = []
-stderr = []
+# wait for the process to terminate
+out, err = process.communicate()
+errcode = process.returncode
 
-while True:
-  reads = [p.stdout.fileno(), p.stderr.fileno()]
-  ret = select.select(reads, [], [])
-
-  for fd in ret[0]:
-    if fd == p.stdout.fileno():
-      read = p.stdout.readline()
-      stdout.append(read)
-    if fd == p.stderr.fileno():
-      read = p.stderr.readline()
-      stderr.append(read)
-
-  if p.poll() != None:
-    break
-res = ""
-for item in stdout:
-  stdout_str = str(item,'utf-8')
-  res = res + stdout_str
-traces = yaml.safe_load(res)
+res = str(out,'utf-8')
+traces = json.loads(res)
 caller_callee_info = []
 for item in traces['traces']:
   trace_id = item['traceID']
-  p =  subprocess.Popen(["curl", "-G", "-s", "http://localhost:8082/api/traces/"+trace_id], 
-                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  stdout = []
-  stderr = []
-  while True:
-    reads = [p.stdout.fileno(), p.stderr.fileno()]
-    ret = select.select(reads, [], [])
-
-    for fd in ret[0]:
-      if fd == p.stdout.fileno():
-        read = p.stdout.readline()
-        stdout.append(read)
-      if fd == p.stderr.fileno():
-        read = p.stderr.readline()
-        stderr.append(read)
-
-    if p.poll() != None:
-        break
-  res = ""
-  for item in stdout:
-    stdout_str = str(item,'utf-8')
-    res = res + stdout_str
-  trace = yaml.safe_load(res)
+  cmd = "curl -G -s http://localhost:8082/api/traces/"+trace_id
+  process = subprocess.Popen(cmd, shell=True,
+                             stdout=subprocess.PIPE, 
+                             stderr=subprocess.PIPE)
+  # wait for the process to terminate
+  out, err = process.communicate()
+  errcode = process.returncode
+  res = str(out,'utf-8')
+  trace = json.loads(res)
   span_attrs = trace['batches'][0]['scopeSpans'][0]['spans'][0]['attributes']
   caller = ""
   callee = ""
   for attr in span_attrs :
     if attr['key'] == 'http.target':
-      caller = attr['value']['stringValue']
-    elif attr['key'] == 'net.peer.ip':
       callee = attr['value']['stringValue']
+    elif attr['key'] == 'net.peer.ip':
+      caller = attr['value']['stringValue']
   caller_callee = []
   caller_callee.append(caller)
   caller_callee.append(callee)
   caller_callee_info.append(caller_callee)
 print(caller_callee_info)
+
+cmd = "kubectl get -n openfaas-fn pods -o yaml"
+process = subprocess.Popen(cmd, shell=True,
+                           stdout=subprocess.PIPE, 
+                           stderr=subprocess.PIPE)
+# wait for the process to terminate
+out, err = process.communicate()
+errcode = process.returncode
+res = str(out,'utf-8')
+func_info = yaml.safe_load(res)
+ip_func_map = {}
+for item in func_info['items']:
+  if 'status' in item and 'podIP' in item['status']:
+    ip_func_map[item['status']['podIP']] = item['metadata']['labels']['faas_function']
+
+cmd = "kubectl get -n openfaas2-fn pods -o yaml"
+process = subprocess.Popen(cmd, shell=True,
+                           stdout=subprocess.PIPE, 
+                           stderr=subprocess.PIPE)
+# wait for the process to terminate
+out, err = process.communicate()
+errcode = process.returncode
+res = str(out,'utf-8')
+func_info = yaml.safe_load(res)
+
+for item in func_info['items']:
+  if 'status' in item and 'podIP' in item['status']:
+    ip_func_map[item['status']['podIP']] = item['metadata']['labels']['faas_function']
+print(ip_func_map)
 
