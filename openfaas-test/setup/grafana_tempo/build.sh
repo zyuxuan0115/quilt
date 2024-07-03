6ec3e5bd8e686cf592c4982b8e95abd5bcf1e47f
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-03 10:13:34 -0400

    update

---
+++ openfaas-test/setup/grafana_tempo/build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+function add_repo_to_helm {
+  helm repo add grafana https://grafana.github.io/helm-charts
+}
+
+function setup_grafana_tempo {
+  ### install Grafana, the GUI of Tempo.
+  ### export the IP of Grafana to external, port 3000
+  kubectl create namespace sn-tempo
+  helm -n sn-tempo install grafana grafana/grafana --set grafana.ingress.enabled=true --values - <<EOF
+  datasources:
+    datasources.yaml:
+      apiVersion: 1
+      datasources:
+      - name: Tempo
+        type: tempo
+        url: http://tempo-query-frontend.sn-tempo.svc.cluster.local:3100
+EOF
+
+  GRAFANA_PASSWORD=$(kubectl get secret --namespace sn-tempo grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo)
+  echo $GRAFANA_PASSWORD > grafana_password.txt
+  kubectl wait --for=condition=Ready -n sn-tempo pod -l "app.kubernetes.io/name=grafana,app.kubernetes.io/instance=grafana" --timeout=3600s
+  kubectl -n sn-tempo expose deployment grafana --type=LoadBalancer --port=3000 --target-port=3000 --name=grafana-external
+
+  ### install Tempo, which collect the trace from open-telemetry
+  ### and expose the IP to external, port 3100 
+  helm -n sn-tempo install tempo grafana/tempo-distributed \
+    --set traces.otlp.grpc.enabled=true \
+    --set traces.otlp.http.enabled=true \
+    --set ingester.zoneAwareReplication.enabled=true \
+    --set ingester.config.complete_block_timeout="24h" \
+    --set ingester.config.max_block_duration="24h" \
+    --values - << EOF
+distributor:
+  receivers:
+    otlp:
+      protocols:
+        grpc:
+        http: 
+EOF
+
+  kubectl wait --for=condition=Ready -n sn-tempo pod -l "app.kubernetes.io/instance=tempo" --timeout=3600s
+  kubectl port-forward -n sn-tempo svc/tempo-query-frontend 8082:3100 &
+}
+
+function setup {
+  add_repo_to_helm # only need to run for the first time
+  helm repo update
+  setup_grafana_tempo
+}
+
+function killa {
+  rm -rf *.txt *.yaml *.yml
+}
+
+case "$1" in
+setup)
+    setup
+    ;;
+kill)
+    killa
+    ;;
+esac
+
