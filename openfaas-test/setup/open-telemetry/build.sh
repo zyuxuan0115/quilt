991f12de4c30fa4179ff677ff6451e0090b3e775
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-03 09:32:43 -0400

    update

---
+++ openfaas-test/setup/open-telemetry/build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+function add_repo_to_helm {
+  helm repo add grafana https://grafana.github.io/helm-charts
+  helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+  helm repo add bitnami https://charts.bitnami.com/bitnami
+}
+
+
+function setup_otel {  
+  ### install open-telemetry for distributed tracing
+  kubectl create namespace sn-otel
+  helm -n sn-otel install otel-collector open-telemetry/opentelemetry-collector \
+      --values - <<EOF
+mode: deployment
+image:
+  repository: otel/opentelemetry-collector-contrib
+
+replicaCount: 1
+
+presets:
+  clusterMetrics:
+    enabled: true
+  kubernetesEvents:
+    enabled: true
+config:
+  exporters:
+    otlphttp:
+      endpoint: "http://tempo-distributor.sn-tempo.svc.cluster.local:4318"
+      tls:
+        insecure: true
+  service:
+    pipelines:
+      traces:
+        exporters: [ otlphttp ]
+      logs:
+        exporters: [ debug ]
+EOF
+}
+
+function setup {
+  add_repo_to_helm # only need to run for the first time
+  helm repo update
+  setup_otel
+}
+
+function killa {
+}
+
+case "$1" in
+setup)
+    setup
+    ;;
+kill)
+    killa
+    ;;
+esac
+
