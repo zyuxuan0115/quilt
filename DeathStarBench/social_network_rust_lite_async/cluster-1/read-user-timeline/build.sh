1d8c8a6e8f06271ea70df377b13935af38c7cfea
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-04 11:31:04 -0400

    update

--- DeathStarBench/social_network_rust_lite_async/cluster-1/read-user-timeline/build.sh
+++ DeathStarBench/social_network_rust_lite_async/cluster-1/read-user-timeline/build.sh
@@ -7,7 +7,7 @@ echo $ROOT_DIR
 function build_rust_test {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/sn-read-user-timeline:latest \
+    sudo docker build -t zyuxuan0115/sn-read-user-timeline-async:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
@@ -15,7 +15,7 @@ function build_rust_test {
 }
 
 function push_rust_test {
-    sudo docker push zyuxuan0115/sn-read-user-timeline:latest
+    sudo docker push zyuxuan0115/sn-read-user-timeline-async:latest
 }
 
 function build {
