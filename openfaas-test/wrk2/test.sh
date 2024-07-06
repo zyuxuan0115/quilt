06ce4425002e7525cf55ca08d497237d4f6aee0e
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-06 14:03:56 -0400

    update

--- openfaas-test/wrk2/test.sh
+++ openfaas-test/wrk2/test.sh
@@ -4,15 +4,17 @@ WRK_BIN=./wrk
 WRK_SCRIPT="$1.lua"
 CLUSTER_ID=$2
 # cluster 1 IP
-ENTRY_HOST=130.127.133.29
+ENTRY_HOST="http://130.127.133.29:30080"
 if [[ $CLUSTER_ID -eq 2 ]]
 then
   # cluster 2 IP
-  ENTRY_HOST=130.127.133.89
+  ENTRY_HOST="http://130.127.133.29:30081"
 fi
 
 QPS=1000
 
 $WRK_BIN -t 1 -c 1 -d 30 -L -U \
 	 -s $WRK_SCRIPT \
-	 http://$ENTRY_HOST:30080 -R $QPS 2>/dev/null > output_$1.log
+	 $ENTRY_HOST -R $QPS 
+
+#2>/dev/null > output_$1.log
