515a49f2b6ac55a8b5716b8348833f7940426cc7
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-06 14:19:40 -0400

    update

--- DeathStarBench/social_network_rust_lite_async/cluster-1/social-graph-follow-with-username/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust_lite_async/cluster-1/social-graph-follow-with-username/template/rust/function/src/main.rs
@@ -3,15 +3,16 @@ use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use std::time::{Duration, Instant};
 use futures::executor::block_on;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+  let http_client = reqwest::Client::new();
   let input: String = get_arg_from_caller();
 //  let time_0 = Instant::now();
   let follow_info: SocialGraphFollowWithUsernameArgs = serde_json::from_str(&input).unwrap();
 //  let time_1 = Instant::now();
-  let future =  make_rpc("get-user-id", follow_info.user_name);
-  let future1 = make_rpc("get-user-id", follow_info.followee_name);
-  let user_id_str:String = block_on(future);
-  let followee_id_str:String = block_on(future1);
+  let future =  make_rpc("get-user-id", follow_info.user_name, &http_client);
+  let future1 = make_rpc("get-user-id", follow_info.followee_name, &http_client);
+  let (user_id_str, followee_id_str): (String, String) = futures::join!(future, future1);
   //  let time_2 = Instant::now();
   let user_id: i64 = user_id_str.parse::<i64>().unwrap();  
   let followee_id: i64 = followee_id_str.parse::<i64>().unwrap();  
@@ -23,7 +24,7 @@ fn main() {
 //  let time_3 = Instant::now();
 //  println!("{:?}", time_1.duration_since(time_0));
 //  println!("{:?}", time_3.duration_since(time_2));
-  let future2 = make_rpc("social-graph-follow", serialized); 
+  let future2 = make_rpc("social-graph-follow", serialized, &http_client); 
   let result = block_on(future2); 
   send_return_value_to_caller(result);
 }
