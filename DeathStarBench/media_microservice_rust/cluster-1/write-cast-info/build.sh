36282594f6264bf1e9d8f7a3a2339345b331dcd4
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-07 13:10:31 -0400

    update

--- DeathStarBench/media_microservice_rust/cluster-1/write-cast-info/build.sh
+++ DeathStarBench/media_microservice_rust/cluster-1/write-cast-info/build.sh
@@ -7,7 +7,7 @@ echo $ROOT_DIR
 function build_rust_test {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/mm-read-movie-info:latest \
+    sudo docker build -t zyuxuan0115/mm-write-cast-info:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
@@ -15,7 +15,7 @@ function build_rust_test {
 }
 
 function push_rust_test {
-    sudo docker push zyuxuan0115/mm-read-movie-info:latest
+    sudo docker push zyuxuan0115/mm-write-cast-info:latest
 }
 
 function build {
