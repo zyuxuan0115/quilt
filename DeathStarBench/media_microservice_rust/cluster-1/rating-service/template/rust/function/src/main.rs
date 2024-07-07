da0aa27ceb787fef23dfc58f44dc01fbd2d5bf8a
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-06 22:51:22 -0400

    update

--- DeathStarBench/media_microservice_rust/cluster-1/rating-service/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust/cluster-1/rating-service/template/rust/function/src/main.rs
@@ -2,53 +2,32 @@ use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::{collections::HashMap, time::{SystemTime,Duration, Instant}};
 use mongodb::{bson::doc,sync::Client};
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
 
   //let now = Instant::now();
-  let movie_info: UploadMovieIdArgs = serde_json::from_str(&input).unwrap();
-
-  let mut movie_id: String = String::new(); 
-
-  let memcache_uri = get_memcached_uri();
-  let memcache_client = memcache::connect(&memcache_uri[..]).unwrap(); 
-  let result: Option<String> = memcache_client.get(&movie_info.title[..]).unwrap();
-
-  match result {
-    Some(id) => {
-      movie_id = id;
-    },
-    None => {
-      let mongodb_uri = get_mongodb_uri();
-      let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-      let mongodb_database = mongodb_client.database("movie-id");
-      let mongodb_collection = mongodb_database.collection::<MovieIdEntry>("movie-id");
-
-      let query = doc!{"title":movie_info.title.clone()};
-      let result = mongodb_collection.find_one(query, None).unwrap();
-      match result {
-        Some(x) => {
-          movie_id = x.movie_id;
-          memcache_client.set(&movie_info.title[..], &movie_id[..], 0).unwrap();
-        },
-        None => {
-          println!("Movie {} is not found in MongoDB;", movie_info.title);
-          panic!("Movie {} is not found in MongoDB;", movie_info.title);
-        },
-      }
-    },
-  }; 
-
-  let upload_rating_args = UploadRatingArgs {
-    movie_id: movie_id,
-    rating: movie_info.rating,
+  let rating_info: RatingServiceArgs = serde_json::from_str(&input).unwrap();
+  let compose_review_rating_info = ComposeReviewUploadRatingArgs {
+    rating: rating_info.rating,
+    req_id: rating_info.req_id,
   };
+  let compose_review_rating_str = serde_json::to_string(&compose_review_rating_info).unwrap();
+  
+  let _ = make_rpc("compose-review-upload-rating", compose_review_rating_str);
 
-  let upload_rating_args_str = serde_json::to_string(&upload_rating_args).unwrap();
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  let _ = make_rpc("upload-rating", upload_rating_args_str);
-
+  let mut movie_id1 = rating_info.movie_id.clone();
+  movie_id1.push_str(":uncommit_sum");
+  let _: isize = con.incr(&movie_id1[..], rating_info.rating).unwrap();
+  
+  let mut movie_id2 = rating_info.movie_id.clone();
+  movie_id2.push_str(":uncommit_num");
+  let _: isize = con.incr(&movie_id2[..], 1).unwrap();
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
