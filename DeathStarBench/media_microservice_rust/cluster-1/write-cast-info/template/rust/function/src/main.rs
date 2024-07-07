5f2ef63957607263a632a194809013580a2f6e43
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-07 15:38:10 -0400

    update

--- DeathStarBench/media_microservice_rust/cluster-1/write-cast-info/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust/cluster-1/write-cast-info/template/rust/function/src/main.rs
@@ -6,25 +6,18 @@ use mongodb::{bson::doc,sync::Client};
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
-  let movie_info: WriteMovieInfoArgs = serde_json::from_str(&input).unwrap();
+  let cast_info: WriteMovieInfoArgs = serde_json::from_str(&input).unwrap();
 
   let mongodb_uri = get_mongodb_uri();
   let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("movie-info");
-  let mongodb_collection = mongodb_database.collection::<MovieInfoEntry>("movie-info");
+  let mongodb_database = mongodb_client.database("cast-info");
+  let mongodb_collection = mongodb_database.collection::<CastInfoEntry>("cast-info");
 
-  let avg_r: f64 = movie_info.avg_rating.parse().unwrap();
-
-  let doc = MovieInfoEntry {
-    movie_id: movie_info.movie_id,
-    title: movie_info.title,
-    plot_id: movie_info.plot_id,
-    avg_rating: avg_r,
-    num_rating: movie_info.num_rating,
-    casts: movie_info.casts,
-    thumbnail_ids: movie_info.thumbnail_ids,
-    photo_ids: movie_info.photo_ids,
-    video_ids: movie_info.video_ids,
+  let doc = CastInfoEntry {
+    cast_info_id: cast_info.cast_info_id,
+    name: cast_info.name,
+    gender: cast_info.gender,
+    intro: cast_info.intro,
   };
   mongodb_collection.insert_one(doc, None).unwrap();
   //let new_now =  Instant::now();
