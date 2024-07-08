5e9c2cff83cbc2f1477f8e7896ab362a71a1fbaf
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-07 23:38:19 -0400

    update

--- DeathStarBench/media_microservice_rust/cluster-1/read-plot/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust/cluster-1/read-plot/template/rust/function/src/main.rs
@@ -6,34 +6,35 @@ use mongodb::{bson::doc,sync::Client};
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
-  let movie_id = input;
+  let plot_id = input;
 
   let memcache_uri = get_memcached_uri();
   let memcache_client = memcache::connect(&memcache_uri[..]).unwrap(); 
 
-  let result: Option<String> = memcache_client.get(&movie_id[..]).unwrap();
+  let result: Option<String> = memcache_client.get(&plot_id[..]).unwrap();
 
-  let mut movie_info = String::new();
+  let mut plot = String::new();
   match result {
     Some(x) => {
-      movie_info = x;
+      plot = x;
     },
     None => {
       let mongodb_uri = get_mongodb_uri();
       let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-      let mongodb_database = mongodb_client.database("movie-info");
-      let mongodb_collection = mongodb_database.collection::<MovieInfoEntry>("movie-info");
+      let mongodb_database = mongodb_client.database("plot");
+      let mongodb_collection = mongodb_database.collection::<PlotEntry>("plot");
 
-      let query = doc!{"movie_id":movie_id.clone()};
+      let plot_id_num: i64 = plot_id[..].parse::<i64>().unwrap();
+      let query = doc!{"plot_id": plot_id_num};
       let res = mongodb_collection.find_one(query, None).unwrap();
       match res {
         Some(x) => {
-          movie_info = serde_json::to_string(&x).unwrap();
-          memcache_client.set(&movie_id[..], &movie_info[..], 0).unwrap();
+          plot = x.plot;
+          memcache_client.set(&plot_id[..], &plot[..], 0).unwrap();
         },
         None => {
-          println!("Movie {} is not found in MongoDB;", movie_id);
-          panic!("Movie {} is not found in MongoDB;", movie_id);
+          println!("Plot {} is not found in MongoDB;", plot_id);
+          panic!("Plot {} is not found in MongoDB;", plot_id);
         },
       }
     },
@@ -41,6 +42,6 @@ fn main() {
 
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
-  send_return_value_to_caller(movie_info);
+  send_return_value_to_caller(plot);
 }
 
