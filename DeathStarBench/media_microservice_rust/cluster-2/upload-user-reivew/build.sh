4a6fd7e743fb2f8642fa3398495961e432ac82a3
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-10 18:14:59 -0400

    update

--- DeathStarBench/media_microservice_rust/cluster-2/upload-user-reivew/build.sh
+++ DeathStarBench/media_microservice_rust/cluster-2/upload-user-reivew/build.sh
@@ -5,9 +5,9 @@ ROOT_DIR=$(pwd)
 echo $ROOT_DIR
 
 function build_rust_test {
-    cp -r $ROOT_DIR/../OpenFaaSRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/sn-social-graph-unfollow:latest \
+    cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
+    sudo docker build -t zyuxuan0115/mm-upload-user-review:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
@@ -15,7 +15,7 @@ function build_rust_test {
 }
 
 function push_rust_test {
-    sudo docker push zyuxuan0115/sn-social-graph-unfollow:latest
+    sudo docker push zyuxuan0115/mm-upload-user-review:latest
 }
 
 function build {
--- DeathStarBench/media_microservice_rust/cluster-2/upload-user-reivew/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust/cluster-2/upload-user-reivew/template/rust/function/src/main.rs
@@ -1,5 +1,4 @@
 use mongodb::{bson::doc,sync::Client};
-use serde::{Deserialize, Serialize};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::{fs::read_to_string, collections::HashMap, time::{SystemTime,Duration, Instant}};
@@ -8,41 +7,46 @@ use redis::{Commands};
 fn main() {
   let input: String = get_arg_from_caller();
 //  let now = Instant::now();
-  let follow_info: SocialGraphFollowArgs = serde_json::from_str(&input).unwrap();
+  let review_info: UploadUserReviewArgs = serde_json::from_str(&input).unwrap();
 
   let time_stamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
 
   let uri = get_mongodb_uri();
   let client = Client::with_uri_str(&uri[..]).unwrap();
-  let database = client.database("social-graph");
-  let collection = database.collection::<SocialGraphEntry>("social-graph");
+  let database = client.database("user-review");
+  let collection = database.collection::<UserReviewEntry>("user-review");
 
-  // Update follower->followee edges
-  let search_query = doc!{"user_id": follow_info.user_id};
-  let update_query = doc!{"$pull": doc!{"followees":doc!{"user_id":follow_info.followee_id}}};
-  let res = collection.update_many(search_query, update_query, None).unwrap();
-  // TODO: check if the update is successful
+  let query = doc!{"user_id":review_info.user_id};
+  let mongodb_result = collection.find_one(query, None).unwrap();
+  
+  match mongodb_result {
+    Some(x) => {
+      let search_query = doc!{"user_id":review_info.user_id};
+      let update_query = doc!{"$push":doc!{"reviews":doc!{"$each":[{"review_id":review_info.user_id,"timestamp":review_info.timestamp}],"$position":0}}};
+      let res = collection.update_one(search_query, update_query, None).unwrap();
+    },
+    None => {
+      let review = Review {
+        review_id: review_info.review_id,
+        timestamp: review_info.timestamp,
+      };
+      let new_user_review = UserReviewEntry {
+        user_id: review_info.user_id,
+        reviews: vec![review],
+      };
+      collection.insert_one(new_user_review, None).unwrap();
+    }
+  }
 
-  // Update followee->follower edges
-  let search_query = doc!{"user_id": follow_info.followee_id};
-  let update_query = doc!{"$pull": doc!{"followers":doc!{"user_id":follow_info.user_id}}};
-  let res = collection.update_many(search_query, update_query, None).unwrap();
-  // TODO: check if the update is successful
-
-  // update redis
-  let mut user_id_str: String = follow_info.user_id.to_string();
-  user_id_str.push_str(":followees");
-  let mut followee_id_str: String = follow_info.followee_id.to_string();
-  followee_id_str.push_str(":followers");
 
   let redis_uri = get_redis_rw_uri();
   let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
   let mut con = redis_client.get_connection().unwrap();
 
-  let (k1, k2) : (isize, isize) = redis::pipe()
-                                     .cmd("ZREM").arg(&user_id_str[..]).arg(follow_info.followee_id)
-                                     .cmd("ZREM").arg(&followee_id_str[..]).arg(follow_info.user_id)
-                                     .query(&mut con).unwrap();
+//  let (k1, k2) : (isize, isize) = redis::pipe()
+//                                     .cmd("ZREM").arg(&user_id_str[..]).arg(follow_info.followee_id)
+//                                     .cmd("ZREM").arg(&followee_id_str[..]).arg(follow_info.user_id)
+//                                     .query(&mut con).unwrap();
 
 //  let new_now =  Instant::now();
 //  println!("SocialGraphUnfollow: {:?}", new_now.duration_since(now));
