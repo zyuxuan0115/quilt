e0555e101b9fd6e9715a9831767b7d105496bfb9
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-10 21:36:56 -0400

    update

--- DeathStarBench/media_microservice_rust/cluster-2/upload-user-reivew/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust/cluster-2/upload-user-reivew/template/rust/function/src/main.rs
@@ -43,11 +43,12 @@ fn main() {
   let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
   let mut con = redis_client.get_connection().unwrap();
 
-//  let (k1, k2) : (isize, isize) = redis::pipe()
-//                                     .cmd("ZREM").arg(&user_id_str[..]).arg(follow_info.followee_id)
-//                                     .cmd("ZREM").arg(&followee_id_str[..]).arg(follow_info.user_id)
-//                                     .query(&mut con).unwrap();
+  let user_id_str: String = review_info.user_id.to_string();
+  let review_id_str: String = review_info.review_id.to_string();
 
+  // TODO set the options to be NX
+  let _: usize = con.zadd(&user_id_str[..], &review_id_str[..], review_info.timestamp).unwrap();
+  
 //  let new_now =  Instant::now();
 //  println!("SocialGraphUnfollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
