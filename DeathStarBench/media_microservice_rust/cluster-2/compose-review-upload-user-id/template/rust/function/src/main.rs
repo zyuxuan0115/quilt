2c634359717c88d8a13c7b7678cb3a36e8f5d4ae
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-12 17:24:32 -0400

    update

---
+++ DeathStarBench/media_microservice_rust/cluster-2/compose-review-upload-user-id/template/rust/function/src/main.rs
@@ -0,0 +1,28 @@
+use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
+use DbInterface::*;
+use std::time::{Duration, Instant};
+
+fn main() {
+  let input: String = get_arg_from_caller();
+//  let now = Instant::now();
+  let args: ComposeReviewUploadMovieIdArgs = serde_json::from_str(&input).unwrap();
+  let mut key_counter:String = args.req_id.to_string();
+  key_counter.push_str(":counter"); 
+
+  let memcache_uri = get_memcached_uri();
+  let memcache_client = memcache::connect(&memcache_uri[..]).unwrap(); 
+  memcache_client.add(&key_counter[..], 0, 0);
+
+  let mut key_movie_id: String = args.req_id.to_string();
+  key_movie_id.push_str(":movie_id");
+ 
+  memcache_client.add(&key_movie_id[..], 0, 0);
+  let counter_value:u64 = memcache_client.increment(&key_counter[..], 1).unwrap();
+
+  if counter_value == NUM_COMPONENTS {
+    make_rpc("compose-and-upload", args.req_id.to_string());
+  }
+//  let new_now =  Instant::now();
+//  println!("{:?}", new_now.duration_since(now));
+  send_return_value_to_caller("".to_string());
+}
