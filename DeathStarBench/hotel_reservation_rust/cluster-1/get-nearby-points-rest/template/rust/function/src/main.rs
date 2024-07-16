1a9f2f483208fd124c3825171bc1b963f51f0b68
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-16 12:30:04 -0400

    update

--- DeathStarBench/hotel_reservation_rust/cluster-1/get-nearby-points-rest/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust/cluster-1/get-nearby-points-rest/template/rust/function/src/main.rs
@@ -3,13 +3,12 @@ use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
 use mongodb::{bson::doc,sync::Client};
 use knn::PointCloud;
-
-
+use std::collections::HashMap;
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
-  let cast_info: WriteCastInfoArgs = serde_json::from_str(&input).unwrap();
+  let cast_info: GetNearbyPointsRestArgs = serde_json::from_str(&input).unwrap();
 
   let mongodb_uri = get_mongodb_uri();
   let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
@@ -19,12 +18,30 @@ fn main() {
   let cursor = mongodb_collection.find(doc!{}, None).unwrap();
 
 
-  let compute_dist = |p: &[f64;2], q: &[f64;2]| -> f64 {((p[0] - q[0])*(p[0]-q[0])+(p[1] - q[1])*(p[1] - q[1])).sqrt() as i64};
+  let compute_dist = |p: &[f64;2], q: &[f64;2]| -> f64 { ((p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])).sqrt() as f64};
   let mut pc = PointCloud::new(compute_dist);
 
-  for 
+  let mut restaurants: Vec<Restaurant> = Vec::new();
+  for doc in cursor {
+    let doc_ = doc.unwrap();
+    restaurants.push(doc_.clone());
+  } 
 
-  pc.add_point(&p);  
+  let restaurant_hashmap: HashMap<String, String> = restaurants.iter().map(|x| {
+    let new_p: (f64,f64) = (x.latitude, x.longitude); 
+    let new_p_str = serde_json::to_string(&new_p).unwrap();
+    (new_p_str, x.restaurant_id.clone())
+  }).collect::<HashMap<String, String>>();
+
+  let restaurant_p: Vec<[f64;2]> = restaurants.iter().map(|x|{
+    let new_v: [f64;2] = [x.latitude, x.longitude]; new_v}).collect();
+
+  for i in 0..restaurant_p.len() {
+    pc.add_point(&restaurant_p[i]); 
+  }
+
+  let d = pc.get_nearest_n(&[2.1, 2.1], 2);
+//  pc.add_point(&p);  
 
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
--- DeathStarBench/hotel_reservation_rust/cluster-1/nearby-rest/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust/cluster-1/nearby-rest/template/rust/function/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
   let mongodb_database = mongodb_client.database("attractions-db");
   let mongodb_collection = mongodb_database.collection::<Point>("hotels");
 
-  let res = mongodb_collection.find_one(doc! { "hotel_id": &hotel_id[..] }, None).unwrap();
+  let res = mongodb_collection.find_one(doc! { "id": &hotel_id[..] }, None).unwrap();
 
   let mut rest_pids: Vec<String> = Vec::new(); 
   match res {
@@ -26,7 +26,7 @@ fn main() {
       let serialized = serde_json::to_string(&args).unwrap();
       let rest_points_str = make_rpc("get-nearby-points-rest", serialized); 
       let rest_points: Vec<Point> = serde_json::from_str(&rest_points_str).unwrap();
-      rest_pids = rest_points.iter().map(|x| x.hotel_id.clone()).collect();
+      rest_pids = rest_points.iter().map(|x| x.id.clone()).collect();
     },
     None => {
       println!("Hotel {} already existed", hotel_id);
