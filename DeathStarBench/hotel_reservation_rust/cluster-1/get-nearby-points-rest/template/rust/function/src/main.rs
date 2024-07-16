48715a0d0de8c1f5e33cf450ecebf939023ffb33
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-16 13:04:30 -0400

    update

--- DeathStarBench/hotel_reservation_rust/cluster-1/get-nearby-points-rest/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust/cluster-1/get-nearby-points-rest/template/rust/function/src/main.rs
@@ -14,13 +14,7 @@ fn main() {
   let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
   let mongodb_database = mongodb_client.database("attractions-db");
   let mongodb_collection = mongodb_database.collection::<Restaurant>("restaurants");
-
   let cursor = mongodb_collection.find(doc!{}, None).unwrap();
-
-
-  let compute_dist = |p: &[f64;2], q: &[f64;2]| -> f64 { ((p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])).sqrt() as f64};
-  let mut pc = PointCloud::new(compute_dist);
-
   let mut restaurants: Vec<Restaurant> = Vec::new();
   for doc in cursor {
     let doc_ = doc.unwrap();
@@ -36,16 +30,30 @@ fn main() {
   let restaurant_p: Vec<[f64;2]> = restaurants.iter().map(|x|{
     let new_v: [f64;2] = [x.latitude, x.longitude]; new_v}).collect();
 
+  let compute_dist = |p: &[f64;2], q: &[f64;2]| -> f64 { ((p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])).sqrt() as f64};
+  let mut pc = PointCloud::new(compute_dist);
   for i in 0..restaurant_p.len() {
     pc.add_point(&restaurant_p[i]); 
   }
-
   let center: [f64;2] = [hotel_loc.latitude, hotel_loc.longitude];
-  let d = pc.get_nearest_n(&center, maxSearchResults);
-//  pc.add_point(&p);  
+  let results = pc.get_nearest_k(&center, maxSearchResults);
 
+  let mut rest_points: Vec<Point> = Vec::new();
+  for item in results {
+    let new_p = (item.1[0], item.1[1]);
+    let new_p_str = serde_json::to_string(&new_p).unwrap();
+    let id: String = restaurant_hashmap.get(&new_p_str).unwrap().to_owned();
+    let new_p = Point {
+      id: id,
+      latitude: item.1[0],
+      longitude: item.1[1],
+    };
+    rest_points.push(new_p);
+  }
+
+  let serialized = serde_json::to_string(&rest_points).unwrap();
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
-  send_return_value_to_caller("".to_string());
+  send_return_value_to_caller(serialized);
 }
 
