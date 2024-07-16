20482ee628a87fe960e73f1634c2f1e1cabf6377
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-16 12:41:39 -0400

    update

--- DeathStarBench/hotel_reservation_rust/OpenFaaSRPC/src/lib.rs
+++ DeathStarBench/hotel_reservation_rust/OpenFaaSRPC/src/lib.rs
@@ -2,7 +2,7 @@ use curl::easy::{Easy};
 use std::{io::{self, Read, Write, BufReader}, error::Error, fs::{File, read_to_string}, path::Path, collections::HashMap};
 use serde::{Deserialize, Serialize};
 
-pub static NUM_COMPONENTS: u64 = 5;
+pub static maxSearchResults: usize = 5;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GetNearbyPointsRestArgs {
