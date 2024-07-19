7744fbc85b035961b0cff9a1ffc68ebc5748023c
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-19 15:25:01 -0400

    update

--- DeathStarBench/hotel_reservation_rust/cluster-1/set-rest/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust/cluster-1/set-rest/template/rust/function/src/main.rs
@@ -7,21 +7,22 @@ use std::collections::HashMap;
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
-  let rate_info: SetRateArgs = serde_json::from_str(&input).unwrap();
+  let rest_info: SetRestArgs = serde_json::from_str(&input).unwrap();
   let mongodb_uri = get_mongodb_uri();
   let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("rate-db");
-  let mongodb_collection = mongodb_database.collection::<RatePlan>("inventory");
+  let mongodb_database = mongodb_client.database("attractions-db");
+  let mongodb_collection = mongodb_database.collection::<Restaurant>("restaurants");
 
-  let new_rate = RatePlan {
-    hotel_id: rate_info.hotel_id,
-    code: rate_info.code,
-    in_date: rate_info.in_date,
-    out_date: rate_info.out_date,
-    room_type: rate_info.room_type,
+  let new_rest = Restaurant {
+    restaurant_id: rest_info.restaurant_id,
+    latitude: rest_info.latitude,
+    longitude: rest_info.longitude,
+    restaurant_name: rest_info.restaurant_name,
+    rating: rest_info.rating,
+    restaurant_type: rest_info.restaurant_type,
   };
 
-  let mut cursor = mongodb_collection.insert_one(new_rate, None).unwrap();
+  let mut cursor = mongodb_collection.insert_one(new_rest, None).unwrap();
    
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
