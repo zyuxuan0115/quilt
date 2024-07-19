f3e25adbd4cf286ae71a44639bfffda627cd7c54
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-19 14:26:16 -0400

    update

--- DeathStarBench/hotel_reservation_rust/cluster-1/set-rates/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust/cluster-1/set-rates/template/rust/function/src/main.rs
@@ -7,19 +7,18 @@ use std::collections::HashMap;
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
-  let profile_info: SetProfileArgs = serde_json::from_str(&input).unwrap();
+  let rate_info: SetRateArgs = serde_json::from_str(&input).unwrap();
   let mongodb_uri = get_mongodb_uri();
   let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("profile-db");
-  let mongodb_collection = mongodb_database.collection::<HotelProfile>("hotels");
+  let mongodb_database = mongodb_client.database("rate-db");
+  let mongodb_collection = mongodb_database.collection::<RatePlan>("inventory");
 
-  let new_profile = HotelProfile {
-    hotel_id: profile_info.hotel_id,
-    name: profile_info.name,
-    phone_number: profile_info.phone_number,
-    description: profile_info.description,
-    address: profile_info.address,
-    images: profile_info.images,
+  let new_rate = RatePlan {
+    hotel_id: rate_info.hotel_id,
+    code: rate_info.code,
+    in_date: rate_info.in_date,
+    out_date: rate_info.out_date,
+    room_type: rate_info.room_type,
   };
 
   let mut cursor = mongodb_collection.insert_one(new_profile, None).unwrap();
