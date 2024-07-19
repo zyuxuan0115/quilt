6e719121af4b476da2360d7596e2c76f1228fabe
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-19 13:50:21 -0400

    update

--- DeathStarBench/hotel_reservation_rust/cluster-1/set-profiles/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust/cluster-1/set-profiles/template/rust/function/src/main.rs
@@ -8,12 +8,12 @@ fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let hotel_ids: Vec<String> = serde_json::from_str(&input).unwrap();
-  let hotel_id_mmc: Vec<String> = hotel_ids.iter().map(|x| {let mut new_x = x.clone(); new_x.push_str(":rate"); new_x}).collect();
+  let hotel_id_mmc: Vec<String> = hotel_ids.iter().map(|x| {let mut new_x = x.clone(); new_x.push_str(":profile"); new_x}).collect();
   let mut hotel_id_not_cached: HashMap<String, String> = HashMap::new();
  
   for hotel_id in &hotel_ids {
     let mut hotelid = hotel_id.to_owned();
-    hotelid.push_str(":rate");
+    hotelid.push_str(":profile");
     hotel_id_not_cached.insert(hotelid, hotel_id.to_owned());
   }
 
@@ -24,39 +24,39 @@ fn main() {
   let keys: &[&str] = &hotel_id_strslice;
   let results: std::collections::HashMap<String, String> = memcache_client.gets(keys).unwrap();
 
-  let mut hotel_rates: Vec<RatePlan> = Vec::new();
+  let mut hotel_profiles: Vec<HotelProfile> = Vec::new();
 
   for (key, value) in results {
     hotel_id_not_cached.remove(&key);
-    let rate: RatePlan = serde_json::from_str(&value).unwrap();
-    hotel_rates.push(rate);
+    let profile: HotelProfile = serde_json::from_str(&value).unwrap();
+    hotel_profiles.push(profile);
   }
 
-  let mut rate_not_cached: Vec<String> = Vec::new();
+  let mut profile_not_cached: Vec<String> = Vec::new();
   for (_, value) in &hotel_id_not_cached {
-    rate_not_cached.push(value.to_owned());
+    profile_not_cached.push(value.to_owned());
   }
 
-  if rate_not_cached.len() != 0 {
+  if profile_not_cached.len() != 0 {
     let mongodb_uri = get_mongodb_uri();
     let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-    let mongodb_database = mongodb_client.database("rate-db");
-    let mongodb_collection = mongodb_database.collection::<RatePlan>("inventory");
-    let query = doc!{"id": doc!{"$in": &rate_not_cached}};
+    let mongodb_database = mongodb_client.database("profile-db");
+    let mongodb_collection = mongodb_database.collection::<HotelProfile>("hotels");
+    let query = doc!{"id": doc!{"$in": &profile_not_cached}};
     let mut cursor = mongodb_collection.find(query, None).unwrap();
    
     for doc in cursor {
       let doc_ = doc.unwrap();
       // update memcached
       let mut key: String = doc_.hotel_id.to_owned();
-      key.push_str(":rate");
+      key.push_str(":profile");
       let value = serde_json::to_string(&doc_).unwrap();
       memcache_client.set(&key[..],&value[..],0).unwrap();
-      hotel_rates.push(doc_);
+      hotel_profiles.push(doc_);
     }
   }
 
-  let serialized = serde_json::to_string(&hotel_rates).unwrap();
+  let serialized = serde_json::to_string(&hotel_profiles).unwrap();
 
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
