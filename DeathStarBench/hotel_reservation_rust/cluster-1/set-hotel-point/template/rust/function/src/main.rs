8d8db88f5d69f4fdd58b41b6298e93206634cc2b
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-19 18:54:13 -0400

    update

--- DeathStarBench/hotel_reservation_rust/cluster-1/set-hotel-point/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust/cluster-1/set-hotel-point/template/rust/function/src/main.rs
@@ -7,22 +7,19 @@ use std::collections::HashMap;
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
-  let rest_info: SetRestArgs = serde_json::from_str(&input).unwrap();
+  let hotel_info: SetHotelArgs = serde_json::from_str(&input).unwrap();
   let mongodb_uri = get_mongodb_uri();
   let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("attractions-db");
-  let mongodb_collection = mongodb_database.collection::<Restaurant>("restaurants");
+  let mongodb_database = mongodb_client.database("geo-db");
+  let mongodb_collection = mongodb_database.collection::<Point>("geo");
 
-  let new_rest = Restaurant {
-    restaurant_id: rest_info.restaurant_id,
-    latitude: rest_info.latitude,
-    longitude: rest_info.longitude,
-    restaurant_name: rest_info.restaurant_name,
-    rating: rest_info.rating,
-    restaurant_type: rest_info.restaurant_type,
+  let new_hotel = Point {
+    id: hotel_info.id,
+    latitude: hotel_info.latitude,
+    longitude: hotel_info.longitude,
   };
 
-  let mut cursor = mongodb_collection.insert_one(new_rest, None).unwrap();
+  let mut cursor = mongodb_collection.insert_one(new_hotel, None).unwrap();
    
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
