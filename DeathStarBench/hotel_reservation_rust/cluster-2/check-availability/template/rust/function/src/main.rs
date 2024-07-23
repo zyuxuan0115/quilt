f5791bf3c594c36f8e5dfc8467c1297e0dbed5dc
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-22 21:44:33 -0400

    update

--- DeathStarBench/hotel_reservation_rust/cluster-2/check-availability/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust/cluster-2/check-availability/template/rust/function/src/main.rs
@@ -7,41 +7,54 @@ use std::collections::HashMap;
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
-  let hotel_id: String = input.clone();
-  let mut hotel_id_mmc: String = input.clone();
-  hotel_id_mmc.push_str(":review");
+  let args: CheckAvailabilityArgs = serde_json::from_str(&input).unwrap();
+  let hotel_ids: Vec<String> = args.hotel_id;
+  let hotel_id_mmc: Vec<String> = hotel_ids.iter().map(|x| {let mut y = x.clone(); y.push_str("_cap"); y}).collect();
+  let hotel_id_strslice: Vec<&str> = hotel_id_mmc.iter().map(|x| &**x).collect();
+  let keys: &[&str] = &hotel_id_strslice;
 
+  let mut hotel_ids_not_cached: HashMap<String, bool> = HashMap::new();
+  for item in &hotel_ids {
+    let hotel_id = item.to_owned();
+    hotel_ids_not_cached.insert(hotel_id, false);
+  }
   // connect to memcached
   let memcache_uri = get_memcached_uri();
-  let memcache_client = memcache::connect(&memcache_uri[..]).unwrap(); 
-  let result = memcache_client.get(&hotel_id_mmc[..]).unwrap();
+  let memcache_client = memcache::connect(&memcache_uri[..]).unwrap();
+  let result: std::collections::HashMap<String, String> = memcache_client.gets(keys).unwrap();
 
-  let mut review_str = String::new();
-  match result {
-    Some(x) => {
-      review_str = x;
-    },
-    None => { 
-      // fetch data from mongodb
-      let mongodb_uri = get_mongodb_uri();
-      let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-      let mongodb_database = mongodb_client.database("review-db");
-      let mongodb_collection = mongodb_database.collection::<ReviewComm>("reviews");
-      let query = doc!{"hotel_id": &hotel_id[..]};
-      let cursor = mongodb_collection.find(query, None).unwrap();
-      let mut reviews: Vec<ReviewComm> = Vec::new();
-      for doc in cursor {
-        let doc_ = doc.unwrap();
-        reviews.push(doc_.clone()); 
-      }
-      review_str = serde_json::to_string(&reviews).unwrap();
+  let mut capacity_info: Vec<HotelCapacity> = Vec::new();
+  for (key, value) in result {
+    hotel_ids_not_cached.remove(&key);
+    let cap_info: HotelCapacity = serde_json::from_str(&value).unwrap();
+    capacity_info.push(cap_info);
+  } 
+
+  let mut hotel_not_cached: Vec<String> = hotel_ids_not_cached.into_iter().map(|(k,v)| k).collect();
+
+  if hotel_not_cached.len() != 0 {
+    // fetch data from mongodb
+    let mongodb_uri = get_mongodb_uri();
+    let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
+    let mongodb_database = mongodb_client.database("reservation-db");
+    let num_collection = mongodb_database.collection::<HotelCapacity>("number");
+
+    let query = doc!{"hotel_id": doc!{"$in": &hotel_not_cached}};
+    let mut cursor = num_collection.find(query, None).unwrap();
+
+    for doc in cursor {
+      let doc_ = doc.unwrap();
       // update memcached
-      memcache_client.set(&hotel_id_mmc[..], &review_str[..], 0).unwrap();
+      let mut key: String = doc_.hotel_id.to_owned();
+      key.push_str("_cap");
+      let value = serde_json::to_string(&doc_).unwrap();
+      memcache_client.set(&key[..],&value[..],0).unwrap();
+      capacity_info.push(doc_);
     }
-  }
 
+  }
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
-  send_return_value_to_caller(review_str);
+  send_return_value_to_caller("".to_string());
 }
 
