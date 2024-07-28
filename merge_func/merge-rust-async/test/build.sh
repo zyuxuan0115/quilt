e912357d0fe4be77ac866954adf987dd36718d27
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-07-28 12:47:05 -0400

    update

--- merge_func/merge-rust-async/test/build.sh
+++ merge_func/merge-rust-async/test/build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 LLVM_DIR=/proj/zyuxuanssf-PG0/llvm-project-17/build/bin
-RUST_LIB=/users/zyuxuan/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib
-LINKER_FLAGS='-lstd-320ebc7037fb8f95 -lcurl -lcrypto -lm -lssl'
+RUST_LIB=/users/zyuxuan/.rustup/toolchains/1.76-x86_64-unknown-linux-gnu/lib
+
+RUST_LIBSTD_PATH=$(ls $RUST_LIB/libstd-*.so)
+RUST_LIBSTD_NAME=$(basename $RUST_LIBSTD_PATH)
+RUST_LIBSTD_LINKER_FLAG=${RUST_LIBSTD_NAME#"libstd"}
+RUST_LIBSTD_LINKER_FLAG=${RUST_LIBSTD_LINKER_FLAG%".so"}
+
+RUST_LIBRUSTC_PATH=$(ls $RUST_LIB/librustc_driver-*.so)
+RUST_LIBRUSTC_NAME=$(basename $RUST_LIBRUSTC_PATH)
+RUST_LIBRUSTC_LINKER_FLAG=${RUST_LIBRUSTC_NAME#"librustc_driver"}
+RUST_LIBRUSTC_LINKER_FLAG=${RUST_LIBRUSTC_LINKER_FLAG%".so"}
+
+RUST_LIBTEST_PATH=$(ls $RUST_LIB/libtest-*.so)
+RUST_LIBTEST_NAME=$(basename $RUST_LIBTEST_PATH)
+RUST_LIBTEST_LINKER_FLAG=${RUST_LIBTEST_NAME#"libtest"}
+RUST_LIBTEST_LINKER_FLAG=${RUST_LIBTEST_LINKER_FLAG%".so"}
+
+LINKER_FLAGS="-lstd$RUST_LIBSTD_LINKER_FLAG -lcurl -lcrypto -lm -lssl -lz -lrustc_driver$RUST_LIBRUSTC_LINKER_FLAG -ltest$RUST_LIBTEST_LINKER_FLAG "
 
 function merge {
   cp -r OpenFaaSRPC caller/template/rust/ && cd caller/template/rust/function \
@@ -14,10 +30,17 @@ function merge {
   && RUSTFLAGS="--emit=llvm-ir" cargo build \
   && cd ../../../..
 
-#  CALLEE_IR=$(ls callee/target/debug/deps/function-*.ll)
-  $LLVM_DIR/opt -S $CALLEE_IR -passes=merge-rust-func-async -o callee_rename.ll
+  CALLER_IR=$(ls caller/template/rust/function/target/debug/deps/function-*.ll)
+  CALLEE_IR=$(ls callee1/template/rust/function/target/debug/deps/function-*.ll)
+  $LLVM_DIR/opt -S $CALLER_IR -strip-debug -o caller_no_debug.ll
+  $LLVM_DIR/opt -S caller_no_debug.ll -passes=merge-rust-func-async -o caller.ll
+  mv caller.ll $CALLER_IR
+  $LLVM_DIR/llvm-link caller/template/rust/function/target/debug/deps/*.ll -S -o caller_all.ll
+  $LLVM_DIR/llc -filetype=obj caller_all.ll -o function.o
+  $LLVM_DIR/clang -no-pie -L$RUST_LIB  function.o -o function $LINKER_FLAGS
+#  $LLVM_DIR/opt -S $CALLEE_IR -passes=merge-rust-func-async -rename-callee-rra -o callee_rename.ll
 #  cp callee_rename.ll caller/target/debug/deps/
-#  $LLVM_DIR/llvm-link caller/target/debug/deps/*.ll -S -o merge.ll
+#  $LLVM_DIR/llvm-link caller.ll callee_rename.ll -S -o merge.ll
 #  $LLVM_DIR/opt merge.ll -strip-debug -o merge_nodebug.ll -S
 #  $LLVM_DIR/opt -S merge_nodebug.ll -passes=merge-rust-func -o merge_new.ll
 #  $LLVM_DIR/llc -filetype=obj merge_new.ll -o function.o
@@ -26,12 +49,13 @@ function merge {
 
 function clean {
   cd caller/template/rust/function && cargo clean \
-  && cd ../../../../callee1/template/rust/function && cargo clean \
-  && cd ../../../../callee2/template/rust/function && cargo clean \
+  && cd ../../../../callee1/template/rust/function && cargo clean && rm -rf Cargo.lock \
+  && cd ../../../../callee2/template/rust/function && cargo clean && rm -rf Cargo.lock \
   && cd ../../../../
   rm -rf caller/template/rust/OpenFaaSRPC
   rm -rf callee1/template/rust/OpenFaaSRPC
   rm -rf callee2/template/rust/OpenFaaSRPC
+  rm *.ll *.o function
 }
 
 case "$1" in
