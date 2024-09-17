0569ef1184aba0e56f4f17657de783ffd7785809
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-17 14:14:10 -0400

    update

--- DeathStarBench/media_microservice_rust_lite/cluster-1/write-cast-info/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust_lite/cluster-1/write-cast-info/template/rust/function/src/main.rs
@@ -1,25 +1,25 @@
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{bson::doc,sync::Client};
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let cast_info: WriteCastInfoArgs = serde_json::from_str(&input).unwrap();
 
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("cast-info");
-  let mongodb_collection = mongodb_database.collection::<CastInfoEntry>("cast-info");
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  let doc = CastInfoEntry {
-    cast_info_id: cast_info.cast_info_id,
-    name: cast_info.name,
-    gender: cast_info.gender,
-    intro: cast_info.intro,
-  };
-  mongodb_collection.insert_one(doc, None).unwrap();
+  let mut cast_id: String = "cast_info:".to_string();
+  cast_id.push_str(&cast_info.cast_info_id.to_string()[..]);
+
+  let _: isize = con.hset(&cast_id[..], "cast_info_id", cast_info.cast_info_id).unwrap();
+  let _: isize = con.hset(&cast_id[..], "name", cast_info.name).unwrap();
+  let _: isize = con.hset(&cast_id[..], "gender", cast_info.gender).unwrap();
+  let _: isize = con.hset(&cast_id[..], "intro", cast_info.intro).unwrap();
+
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
