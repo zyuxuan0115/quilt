334797e5ab20088a8319293a02471f74eec40601
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-24 15:15:40 -0400

    update

--- DeathStarBench/media_microservice_rust_lite/cluster-1/register-movie-id/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust_lite/cluster-1/register-movie-id/template/rust/function/src/main.rs
@@ -2,31 +2,32 @@ use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::{collections::HashMap, time::{SystemTime,Duration, Instant}};
 use redis::{Commands};
-use memcache::Client as memcached_client;
-use mongodb::{bson::doc,sync::Client};
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let movie_info: RegisterMovieIdArgs = serde_json::from_str(&input).unwrap();
 
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("movie-id");
-  let mongodb_collection = mongodb_database.collection::<MovieIdEntry>("movie-id");
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap(); 
+  let mut con = redis_client.get_connection().unwrap();
 
-  let query = doc!{"title":movie_info.title.clone()};
-  let mut cursor = mongodb_collection.find(query, None).unwrap();
-  if cursor.count() == 0 {
-    let doc = MovieIdEntry {
-      title: movie_info.title,
-      movie_id: movie_info.movie_id,
-    };
-    mongodb_collection.insert_one(doc, None).unwrap();
+  let mut mtitle = "movie_id:".to_string();
+  mtitle.push_str(&movie_info.title[..]);
+
+  let res: redis::RedisResult<String> = con.hget(&mtitle[..], "movie_id");
+
+  match res {
+    Ok(x) => {
+      println!("Movie {} is already in reids;", movie_info.title);
+      panic!("Movie {} is already in reids;", movie_info.title);
+    },
+    Err(_) => {
+      let _: isize = con.hset(&mtitle[..], "movie_id", &movie_info.movie_id[..]).unwrap();
+      let _: isize = con.hset(&mtitle[..], "title", &movie_info.title[..]).unwrap();
+    },
   }
-  else {
-    println!("Movie {} already existed in MongoDB", movie_info.title);
-  }
+
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
