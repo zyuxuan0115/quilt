e800889f8ae0468201ac268986cdcaeb8014009a
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-26 17:48:44 -0400

    update

--- DeathStarBench/social_network_rust_lite_async/merge_local/merge.sh
+++ DeathStarBench/social_network_rust_lite_async/merge_local/merge.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-LLVM_DIR=/proj/zyuxuanssf-PG0/llvm-project-17/build/bin
+LLVM_DIR=/proj/zyuxuanssf-PG0/llvm-project-19/build/bin
 RUST_LIB=/users/zyuxuan/.rustup/toolchains/1.76-x86_64-unknown-linux-gnu/lib
+WORK_DIR=template/rust/function/target/x86_64-unknown-linux-gnu/debug/deps
 
-RUST_LIBSTD_PATH=$(ls $RUST_LIB/libstd-*.so)
+RUST_LIBSTD_PATH=$(ls $RUST_LIB/libstd*.so)
 RUST_LIBSTD_NAME=$(basename $RUST_LIBSTD_PATH)
 RUST_LIBSTD_LINKER_FLAG=${RUST_LIBSTD_NAME#"libstd"}
 RUST_LIBSTD_LINKER_FLAG=${RUST_LIBSTD_LINKER_FLAG%".so"}
@@ -20,7 +21,8 @@ RUST_LIBTEST_LINKER_FLAG=${RUST_LIBTEST_LINKER_FLAG%".so"}
 RUST_LIBLLVM_PATH=$(ls $RUST_LIB/libLLVM-*.so)
 
 #LINKER_FLAGS="-lstd$RUST_LIBSTD_LINKER_FLAG -lcurl -lcrypto -lm -lssl -lz -lrustc_driver$RUST_LIBRUSTC_LINKER_FLAG -ltest$RUST_LIBTEST_LINKER_FLAG "
-LINKER_FLAGS="-lstd$RUST_LIBSTD_LINKER_FLAG -lcurl -lcrypto -lm -lssl -lz -ldl"
+#LINKER_FLAGS="-lstd$RUST_LIBSTD_LINKER_FLAG -lcurl -lcrypto -lm -lssl -lz -ldl"
+LINKER_FLAGS="-lcurl -lcrypto -lm -lssl -lz -ldl -lpthread"
 
 ARGS=("$@")
 NUM_ARGS=$#
@@ -33,7 +35,8 @@ function compile_to_ir {
     cp -r ../OpenFaaSRPC $FUNC_NAME/template/rust \
     && cp -r ../DbInterface $FUNC_NAME/template/rust \
     && cd $FUNC_NAME/template/rust/function \
-    && RUSTFLAGS="--emit=llvm-ir" cargo build \
+    && RUSTFLAGS="-C save-temps -Zlocation-detail=none -Zfmt-debug=none --emit=llvm-bc" cargo +nightly build \
+    -Z build-std=std,panic_abort -Z build-std-features="optimize_for_size" --target x86_64-unknown-linux-gnu \
     && cd ../../../../
   done
 }
@@ -41,25 +44,36 @@ function compile_to_ir {
 
 function merge {
   # prepare for merging
-  CALLER_IR=$(ls $CALLER_FUNC/template/rust/function/target/debug/deps/function-*.ll)
+  rm $CALLER_FUNC/$WORK_DIR/panic_abort-*.*
+  rm $CALLER_FUNC/$WORK_DIR/*no-opt*
+  ./rm_redundant_bc.py $CALLER_FUNC/$WORK_DIR
+  CALLER_IR=$(find $CALLER_FUNC/$WORK_DIR/ -type f -name "function-*.bc" -not -name "*.*.*")
+  #CALLER_IR=$(ls $CALLER_FUNC/$WORK_DIR/function-*.ll)
   echo $CALLER_IR
-  mv $CALLER_IR caller.ll
-  cp caller.ll merged.ll
+  mv $CALLER_IR caller.bc
+  $LLVM_DIR/llvm-dis caller.bc -o merged.ll
 
   for i in $(seq 2 $(($NUM_ARGS-1)) );
   do
     CALLEE_FUNC=${ARGS[$i]}
-    CALLEE_IR=$(ls $CALLEE_FUNC/template/rust/function/target/debug/deps/function-*.ll)
+#    CALLEE_IR=$(ls $CALLEE_FUNC/$WORK_DIR/function-*.ll)
+    CALLEE_IR=$(find $CALLEE_FUNC/$WORK_DIR/ -type f -name "function-*.bc" -not -name "*.*.*")
+    rm $CALLEE_FUNC/$WORK_DIR/std-*.bc
+    rm $CALLEE_FUNC/$WORK_DIR/panic_abort-*.bc
+    rm $CALLEE_FUNC/$WORK_DIR/panic_unwind-*.bc
+    rm $CALLEE_FUNC/$WORK_DIR/*no-opt*
+    ./rm_redundant_bc.py $CALLEE_FUNC/$WORK_DIR
     $LLVM_DIR/opt -S $CALLEE_IR -passes=merge-rust-func-async -rename-callee-rra -callee-name-rra=$CALLEE_FUNC -o callee.ll
-    mv $CALLEE_IR $CALLEE_FUNC.ll
+    mv $CALLEE_IR $CALLEE_FUNC.bc
     $LLVM_DIR/llvm-link merged.ll callee.ll -S -o caller_and_callee.ll
     $LLVM_DIR/opt caller_and_callee.ll -strip-debug -o caller_and_callee_nodebug.ll -S
-    $LLVM_DIR/opt -S caller_and_callee_nodebug.ll -passes=merge-rust-func-async -callee-name-rra=$CALLEE_FUNC -o merged.ll
-    cp $CALLEE_FUNC/template/rust/function/target/debug/deps/*.ll $CALLER_FUNC/template/rust/function/target/debug/deps
-    cp -r $CALLEE_FUNC/template/rust/function/target/debug/build/* $CALLER_FUNC/template/rust/function/target/debug/build/
+    $LLVM_DIR/opt caller_and_callee_nodebug.ll -passes=merge-rust-func-async -callee-name-rra=$CALLEE_FUNC -o merged.bc
+    $LLVM_DIR/llvm-dis merged.bc -o merged.ll
+    cp $CALLEE_FUNC/$WORK_DIR/*.bc $CALLER_FUNC/$WORK_DIR
+    #cp -r $CALLEE_FUNC/template/rust/function/target/debug/build/* $CALLER_FUNC/template/rust/function/target/debug/build/
   done
 
-  mv merged.ll $CALLER_IR
+  mv merged.bc $CALLER_IR
 }
 
 
@@ -77,26 +91,26 @@ function wrap_shared_lib {
 
 
 function link {
-  $LLVM_DIR/llvm-link $CALLER_FUNC/template/rust/function/target/debug/deps/*.ll -S -o lib_with_debug_info.ll
-  $LLVM_DIR/opt lib_with_debug_info.ll -strip-debug -o lib.ll -S
-  $LLVM_DIR/opt -S lib_with_debug_info.ll -passes=strip-dead-prototypes -o function.ll
+  $LLVM_DIR/llvm-link $CALLER_FUNC/$WORK_DIR/*.bc -o lib_with_debug_info.bc
+  $LLVM_DIR/opt lib_with_debug_info.bc -strip-debug -o lib.bc
+  $LLVM_DIR/opt lib.bc -passes=strip-dead-prototypes -o function.bc
 
-  $LLVM_DIR/llc -filetype=obj -O3 --function-sections --data-sections function.ll -o function.o
+  $LLVM_DIR/llc -filetype=obj -O3 --function-sections --data-sections function.bc -o function.o
 
   wrap_shared_lib
 
-  STATIC_RING_LIB_DIR=$(find $CALLER_FUNC/template/rust/function/target/debug/build/ -type d -name ring-*)
-  STATIC_RING_LIBS=""
-  for entry in $STATIC_RING_LIB_DIR
-  do 
-    for dir in $entry/*
-    do
-      BASE_NAME=$(basename $dir)
-      if [[ "$BASE_NAME" = "out" ]] ; then
-        STATIC_RING_LIBS=$(ls $dir/libring_*.a)
-      fi
-    done
-  done
+#  STATIC_RING_LIB_DIR=$(find $CALLER_FUNC/template/rust/function/target/x86_64-unknown-linux-gnu/debug/build/ -type d -name ring-*)
+#  STATIC_RING_LIBS=""
+#  for entry in $STATIC_RING_LIB_DIR
+#  do 
+#    for dir in $entry/*
+#    do
+#      BASE_NAME=$(basename $dir)
+#      if [[ "$BASE_NAME" = "out" ]] ; then
+#        STATIC_RING_LIBS=$(ls $dir/libring_*.a)
+#      fi
+#    done
+#  done
 
 #  $LLVM_DIR/clang -no-pie -Wl,--strip-debug -Wl,--gc-sections -Wl,--as-needed -L$RUST_LIB function.o -o function $LINKER_FLAGS $STATIC_RING_LIBS
   gcc -no-pie -Wl,--strip-debug -Wl,--gc-sections -Wl,--as-needed -L$RUST_LIB *.o -o function $LINKER_FLAGS
@@ -114,7 +128,7 @@ function clean {
     && cd ../../../../ \
     && rm -rf $FUNC_NAME/template/rust/function/Cargo.lock
   done
-  rm -rf *.ll *.o function *.txt
+  rm -rf *.ll *.o function *.txt Implib.so
 }
 
 case "$1" in
