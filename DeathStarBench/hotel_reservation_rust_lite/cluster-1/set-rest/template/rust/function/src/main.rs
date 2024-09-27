124b4a3740ac7626503c4b2fa11dcc96cbae3f81
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-26 21:47:13 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-1/set-rest/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-1/set-rest/template/rust/function/src/main.rs
@@ -1,29 +1,26 @@
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{bson::doc,sync::Client};
-use std::collections::HashMap;
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let rest_info: SetRestArgs = serde_json::from_str(&input).unwrap();
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("attractions-db");
-  let mongodb_collection = mongodb_database.collection::<Restaurant>("restaurants");
 
-  let new_rest = Restaurant {
-    restaurant_id: rest_info.restaurant_id,
-    latitude: rest_info.latitude,
-    longitude: rest_info.longitude,
-    restaurant_name: rest_info.restaurant_name,
-    rating: rest_info.rating,
-    restaurant_type: rest_info.restaurant_type,
-  };
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  let mut cursor = mongodb_collection.insert_one(new_rest, None).unwrap();
-   
+  let rid: String = format!("restaurant:{}",rest_info.restaurant_id);
+
+  let _: isize = con.hset(&rid[..], "restaurant_id", rest_info.restaurant_id).unwrap();
+  let _: isize = con.hset(&rid[..], "latitude", rest_info.latitude).unwrap();
+  let _: isize = con.hset(&rid[..], "longitude", rest_info.longitude).unwrap();
+  let _: isize = con.hset(&rid[..], "restaurant_name", rest_info.restaurant_name).unwrap();
+  let _: isize = con.hset(&rid[..], "rating", rest_info.rating).unwrap();
+  let _: isize = con.hset(&rid[..], "restaurant_type", rest_info.restaurant_type).unwrap();
+
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
