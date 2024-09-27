6a52881206c4dffe2ee464963deee2c264c95b08
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-26 22:50:55 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-1/set-hotel-point/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-1/set-hotel-point/template/rust/function/src/main.rs
@@ -1,26 +1,22 @@
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{bson::doc,sync::Client};
-use std::collections::HashMap;
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let hotel_info: SetHotelArgs = serde_json::from_str(&input).unwrap();
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("geo-db");
-  let mongodb_collection = mongodb_database.collection::<Point>("geo");
 
-  let new_hotel = Point {
-    id: hotel_info.id,
-    latitude: hotel_info.latitude,
-    longitude: hotel_info.longitude,
-  };
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  let mut cursor = mongodb_collection.insert_one(new_hotel, None).unwrap();
-   
+  let hid: String = format!("geo:{}", hotel_info.id);
+  let _: isize = con.hset(&hid[..], "id", hotel_info.id).unwrap();
+  let _: isize = con.hset(&hid[..], "latitude", hotel_info.latitude).unwrap();
+  let _: isize = con.hset(&hid[..], "longitude", hotel_info.longitude).unwrap();
+  
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
