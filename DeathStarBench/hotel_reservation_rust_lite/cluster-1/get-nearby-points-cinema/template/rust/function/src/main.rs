8d8de887e83520d4c1a937ee7d31f216a23eb431
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-28 00:25:35 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-1/get-nearby-points-cinema/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-1/get-nearby-points-cinema/template/rust/function/src/main.rs
@@ -1,25 +1,56 @@
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{bson::doc,sync::Client};
 use knn::PointCloud;
 use std::collections::HashMap;
+use redis::{Commands, Iter};
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let hotel_loc: GetNearbyPointsCinemaArgs = serde_json::from_str(&input).unwrap();
 
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("attractions-db");
-  let mongodb_collection = mongodb_database.collection::<Cinema>("cinemas");
-  let cursor = mongodb_collection.find(doc!{}, None).unwrap();
   let mut cinemas: Vec<Cinema> = Vec::new();
-  for doc in cursor {
-    let doc_ = doc.unwrap();
-    cinemas.push(doc_.clone());
-  } 
+
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
+
+  // Start scanning for keys with a specific prefix
+  let mut cursor: u64 = 0;
+  let prefix = "cinema:*"; // Change to your actual prefix
+
+  // Use SCAN command to get matching keys
+  let result: redis::RedisResult<Iter<String>> = con.scan_match(prefix);
+  let mut keys: Vec<String> = Vec::new();
+  match result {
+    Ok(iter) => {
+      keys = iter.map(|x| x).collect();
+    },
+    Err(err) => {
+      println!("Error: finding any of the cinema record");
+      panic!("Error: finding any of the cinema record");
+    },
+  }
+  for key in keys {
+    let result: redis::RedisResult<(String, f64, f64, String, String)> = redis::cmd("HMGET").arg(&key[..]).arg("cinema_id").arg("latitude").arg("longitude").arg("cinema_name").arg("cinema_type").query(&mut con);
+    match result {
+      Ok((id,lat,long,name,mtype)) => {
+        let cinema_info = Cinema {
+            cinema_id: id,
+            latitude: lat,
+            longitude: long,
+            cinema_name: name,
+            cinema_type: mtype,
+        };
+        cinemas.push(cinema_info);
+      },
+      Err(_) => {
+        println!("error in loading cinema info, with id: {}", key);
+        panic!("error in loading cinema info, with id: {}", key);
+      }
+    }
+  }
 
   let cinema_hashmap: HashMap<String, String> = cinemas.iter().map(|x| {
     let new_p: (f64,f64) = (x.latitude, x.longitude); 
