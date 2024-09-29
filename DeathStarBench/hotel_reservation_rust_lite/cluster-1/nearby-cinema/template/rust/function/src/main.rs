b87552b920c0ddf42d069b25431ad001a26247c4
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-28 20:52:44 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-1/nearby-cinema/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-1/nearby-cinema/template/rust/function/src/main.rs
@@ -1,38 +1,38 @@
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{bson::doc,sync::Client};
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let hotel_id: String = input; 
 
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("geo-db");
-  let mongodb_collection = mongodb_database.collection::<Point>("geo");
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  let res = mongodb_collection.find_one(doc! { "id": &hotel_id[..] }, None).unwrap();
-
+  let mut hid = format!("geo:{}", hotel_id);
   let mut cinema_pids: Vec<String> = Vec::new(); 
-  match res {
-    Some(x) => {
-      // call another function to return the K-nearest cinemas
+
+  let result: redis::RedisResult<(f64, f64)> = redis::cmd("HMGET").arg(&hid[..]).arg("latitude").arg("longititude").query(&mut con);
+  match result {
+    Ok((lat,long)) => {
       let args = GetNearbyPointsCinemaArgs {
-        latitude: x.latitude,
-        longitude: x.longitude,
+        latitude: lat,
+        longitude: long,
       };
       let serialized = serde_json::to_string(&args).unwrap();
       let cinema_points_str = make_rpc("get-nearby-points-cinema", serialized); 
       let cinema_points: Vec<Point> = serde_json::from_str(&cinema_points_str).unwrap();
       cinema_pids = cinema_points.iter().map(|x| x.id.clone()).collect();
     },
-    None => {
+    Err(_) => {
       println!("Hotel {} does not exist", hotel_id);
       panic!("Hotel {} does not exist", hotel_id);
     }
   }
+
   let cinema_pids_str = serde_json::to_string(&cinema_pids).unwrap();
 
   //let new_now =  Instant::now();
