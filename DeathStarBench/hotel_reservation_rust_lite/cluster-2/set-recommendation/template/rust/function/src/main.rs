7b60f41bb98fa015810edff13acc6eea2a493472
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-30 22:10:52 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-2/set-recommendation/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-2/set-recommendation/template/rust/function/src/main.rs
@@ -1,28 +1,26 @@
 use OpenFaaSRPC::{get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{sync::Client};
 use std::collections::HashMap;
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let recommendation_info: SetRecommendationArgs = serde_json::from_str(&input).unwrap();
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("recommendation-db");
-  let mongodb_collection = mongodb_database.collection::<HotelRecomm>("recommendation");
 
-  let new_recomm = HotelRecomm {
-    hotel_id: recommendation_info.hotel_id,
-    latitude: recommendation_info.latitude,
-    longitude: recommendation_info.longitude,
-    rate: recommendation_info.rate,
-    price: recommendation_info.price,
-  };
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  let _res = mongodb_collection.insert_one(new_recomm, None).unwrap();
-   
+  let key = format!("recommendation:{}", recommendation_info.hotel_id);
+
+  let _: isize = con.hset(&key[..], "hotel_id", recommendation_info.hotel_id).unwrap();
+  let _: isize = con.hset(&key[..], "latitude", recommendation_info.latitude).unwrap();
+  let _: isize = con.hset(&key[..], "longitude", recommendation_info.longitude).unwrap();
+  let _: isize = con.hset(&key[..], "rate", recommendation_info.rate).unwrap();
+  let _: isize = con.hset(&key[..], "price", recommendation_info.price).unwrap();
+
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
