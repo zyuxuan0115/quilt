9ef7846478731d35a8676d15bb685cffdd2a78b2
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-30 20:57:55 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-2/register-user/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-2/register-user/template/rust/function/src/main.rs
@@ -1,37 +1,34 @@
-use mongodb::{bson::doc,sync::Client};
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use rand::{distributions::Alphanumeric, Rng};
 use sha256::digest;
 use std::time::{Duration, Instant};
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
 //  let now = Instant::now();
   let new_user_info: RegisterUserArgs = serde_json::from_str(&input).unwrap();
-  let uri = get_mongodb_uri();
-  let client = Client::with_uri_str(&uri[..]).unwrap();
-  let database = client.database("user-db");
-  let collection = database.collection::<User>("user");
 
-  let result = collection.find_one(doc! { "username": &new_user_info.username[..] }, None).unwrap();
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  match result {
-    Some(_) => {
+  let key = format!("user:{}", new_user_info.username);
+
+  let res: redis::RedisResult<String> = con.hget(&key[..], "password");
+  match res {
+    Ok(x) => {
       println!("User {} already existed", new_user_info.username);
       panic!("User {} already existed", new_user_info.username);
     },
-    None => (),
+    Err(_) => {
+      let mut pw_sha: String = String::from(&new_user_info.password[..]);
+      pw_sha = digest(pw_sha);
+      let _: isize = con.hset(&key[..], "username", &new_user_info.username[..]).unwrap();
+      let _: isize = con.hset(&key[..], "password", &pw_sha[..]).unwrap();
+    },
   } 
-
-  let mut pw_sha: String = String::from(&new_user_info.password[..]);
-  pw_sha = digest(pw_sha);
-  let user_entry = User {
-    username: new_user_info.username.clone(),
-    password: pw_sha, 
-  };
-
-  collection.insert_one(user_entry, None).unwrap();
 
 //  let new_now =  Instant::now();
 //  println!("{:?}", new_now.duration_since(now));
