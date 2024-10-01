52d2a6452e548b22e69ebf86ac39448d85041cda
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-09-30 21:23:02 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-2/set-rate/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-2/set-rate/template/rust/function/src/main.rs
@@ -1,27 +1,27 @@
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{bson::doc,sync::Client};
 use std::collections::HashMap;
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let rate_info: SetRateArgs = serde_json::from_str(&input).unwrap();
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("rate-db");
-  let mongodb_collection = mongodb_database.collection::<RatePlan>("inventory");
 
-  let new_rate = RatePlan {
-    hotel_id: rate_info.hotel_id,
-    code: rate_info.code,
-    in_date: rate_info.in_date,
-    out_date: rate_info.out_date,
-    room_type: rate_info.room_type,
-  };
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  let mut cursor = mongodb_collection.insert_one(new_rate, None).unwrap();
+  let key = format!("rate:{}", rate_info.hotel_id);
+
+  let room_type_str = serde_json::to_string(&rate_info.room_type).unwrap();
+
+  let _: isize = con.hset(&key[..], "hotel_id", rate_info.hotel_id).unwrap();
+  let _: isize = con.hset(&key[..], "code", rate_info.code).unwrap();
+  let _: isize = con.hset(&key[..], "in_date", rate_info.in_date).unwrap();
+  let _: isize = con.hset(&key[..], "out_date", rate_info.out_date).unwrap();
+  let _: isize = con.hset(&key[..], "room_type", room_type_str).unwrap();
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
