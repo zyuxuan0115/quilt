f2ce958dcce40cd4a2dbecbe88c898a090d3e09c
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-01 22:21:57 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-2/set-capacity/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-2/set-capacity/template/rust/function/src/main.rs
@@ -1,25 +1,23 @@
 use OpenFaaSRPC::{get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{sync::Client};
 use std::collections::HashMap;
+use redis::Commands;
 
 fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
+
   let cap_info: SetCapacityArgs = serde_json::from_str(&input).unwrap();
-  let mongodb_uri = get_mongodb_uri();
-  let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-  let mongodb_database = mongodb_client.database("reservation-db");
-  let mongodb_collection = mongodb_database.collection::<HotelCapacity>("number");
 
-  let new_cap = HotelCapacity {
-    hotel_id: cap_info.hotel_id,
-    capacity: cap_info.capacity,
-  };
+  let redis_uri = get_redis_rw_uri();
+  let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+  let mut con = redis_client.get_connection().unwrap();
 
-  let _res = mongodb_collection.insert_one(&new_cap, None).unwrap();
-   
+  let key = format!("number:{}",cap_info.hotel_id);
+
+  let _: isize  = con.set(&key[..], cap_info.capacity).unwrap();
+  
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
   send_return_value_to_caller("".to_string());
