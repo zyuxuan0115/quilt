ab0977812cf6dec23d7c9fabad639a99df7fa892
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-02 19:16:28 -0400

    update

--- DeathStarBench/social_network_rust_lite_async/cluster-2/media-service/build.sh
+++ DeathStarBench/social_network_rust_lite_async/cluster-2/media-service/build.sh
@@ -6,11 +6,13 @@ echo $ROOT_DIR
 
 function build_rust_test {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
     sudo docker build --no-cache -t zyuxuan0115/sn-media-service-async:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
-    sudo docker system prune
+    rm -rf $ROOT_DIR/template/rust/DbInterface
+    sudo docker system prune -f
 }
 
 function push_rust_test {
