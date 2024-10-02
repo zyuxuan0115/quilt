55900a7b54872e59c827f35eb807c6d31419c3d3
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-01 22:04:20 -0400

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-2/get-reviews/template/rust/function/src/main.rs
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-2/get-reviews/template/rust/function/src/main.rs
@@ -1,8 +1,8 @@
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use std::time::{SystemTime,Duration, Instant};
-use mongodb::{bson::doc,sync::Client};
 use std::collections::HashMap;
+use redis::{Iter, Commands};
 
 fn main() {
   let input: String = get_arg_from_caller();
@@ -22,17 +22,53 @@ fn main() {
       review_str = x;
     },
     None => { 
-      // fetch data from mongodb
-      let mongodb_uri = get_mongodb_uri();
-      let mongodb_client = Client::with_uri_str(&mongodb_uri[..]).unwrap();
-      let mongodb_database = mongodb_client.database("review-db");
-      let mongodb_collection = mongodb_database.collection::<ReviewComm>("reviews");
-      let query = doc!{"hotel_id": &hotel_id[..]};
-      let cursor = mongodb_collection.find(query, None).unwrap();
+      let redis_uri = get_redis_rw_uri();
+      let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
+      let mut con = redis_client.get_connection().unwrap();
+
+      let prefix = format!("review:{}:*", hotel_id);
+
+      let result: redis::RedisResult<Iter<String>> = con.scan_match(prefix);
+      let mut keys: Vec<String> = Vec::new();
+      match result {
+        Ok(iter) => {
+          keys = iter.map(|x| x).collect();
+        },
+        Err(err) => {
+          println!("Error: finding any of the hotel record");
+          panic!("Error: finding any of the hotel record");
+        },
+      }
+
       let mut reviews: Vec<ReviewComm> = Vec::new();
-      for doc in cursor {
-        let doc_ = doc.unwrap();
-        reviews.push(doc_.clone()); 
+      for key in keys {
+        let result: redis::RedisResult<(String,String,String,f32,String,String)> 
+                      = redis::cmd("HMGET")
+                        .arg(&key[..])
+                        .arg("review_id")
+                        .arg("hotel_id")
+                        .arg("name")
+                        .arg("rating")
+                        .arg("description")
+                        .arg("image").query(&mut con);
+        match result {
+          Ok((rid,hid,name,r,desc,img_str)) => {
+            let img: Image = serde_json::from_str(&img_str).unwrap();
+            let review_info = ReviewComm {
+              review_id: rid,
+              hotel_id: hid,
+              name: name,
+              rating: r,
+              description: desc,
+              image: img,
+            };
+            reviews.push(review_info);
+          },
+          Err(_) => {
+            println!("error in loading hotel info, with id: {}", key);
+            panic!("error in loading hotel info, with id: {}", key);
+          }          
+        }
       }
       review_str = serde_json::to_string(&reviews).unwrap();
       // update memcached
