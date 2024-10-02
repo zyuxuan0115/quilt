d62149dc9c2945f6bca1f41b17f76dcbf656a93e
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-02 19:37:07 -0400

    update

--- DeathStarBench/social_network_rust_lite/cluster-1/social-graph-follow-with-username/build.sh
+++ DeathStarBench/social_network_rust_lite/cluster-1/social-graph-follow-with-username/build.sh
@@ -6,10 +6,13 @@ echo $ROOT_DIR
 
 function build_rust_test {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
     sudo docker build -t zyuxuan0115/sn-social-graph-follow-with-username:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface
+    sudo docker system prune -f
 }
 
 function push_rust_test {
