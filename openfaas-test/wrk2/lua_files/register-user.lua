7460a91de7361c078e81bbda3c6ed044c21a56eb
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-04 17:53:05 -0400

    update

--- openfaas-test/wrk2/lua_files/register-user.lua
+++ openfaas-test/wrk2/lua_files/register-user.lua
@@ -2,6 +2,8 @@
 --math.randomseed(socket.gettime()*1000)
 math.random(); math.random(); math.random()
 
+counter = 0
+
 local charset = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
   'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q',
   'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H',
@@ -27,50 +29,25 @@ local function decRandom(length)
 end
 
 request = function(req_id)
-  local user_index = math.random(1, 962)
+  counter = counter + 1
+  local user_index = counter % 1000
   local username = "username_" .. tostring(user_index)
-  local user_id = tostring(user_index)
-  local text = stringRandom(256)
-  local num_user_mentions = math.random(0, 5)
-  local num_urls = math.random(0, 5)
-  local num_media = math.random(0, 4)
-  local media_ids = '['
-  local media_types = '['
+  local first_name = stringRandom(8)
+  local last_name = stringRandom(8)
+  local password = "123456"
 
-  for i = 0, num_user_mentions, 1 do
-    local user_mention_id
-    while (true) do
-      user_mention_id = math.random(1, 962)
-      if user_index ~= user_mention_id then
-        break
-      end
-    end
-    text = text .. " @username_" .. tostring(user_mention_id)
-  end
-
-  for i = 0, num_urls, 1 do
-    text = text .. " http://" .. stringRandom(64)
-  end
-
-  for i = 0, num_media, 1 do
-    local media_id = decRandom(10)
-    media_ids = media_ids .. media_id .. ","
-    media_types = media_types .. "\"png\","
-  end
-
-  media_ids = media_ids:sub(1, #media_ids - 1) .. "]"
-  media_types = media_types:sub(1, #media_types - 1) .. "]"
+  content = '{"username":"' .. username .. '","first_name":"' .. first_name .. 
+            '","last_name":"' .. last_name .. '","password":"' .. password .. '"}' 
 
   local method = "POST"
-  local path = "/function/text-service"
+  local path = "/function/register-user"
   local headers = {}
-  local body
-  headers["Content-Type"] = "application/x-www-form-urlencoded"
-  body = text 
+  local body = content
 
---  file = io.open('req_data_log.txt', 'w')
---  file:write(body)
---  file:close()
+  headers["Content-Type"] = "application/x-www-form-urlencoded"
+  file = io.open('req_data_log.txt', 'w')
+  file:write(body)
+  file:close()
 
   if req_id ~= "" then
     headers["Req-Id"] = req_id
@@ -80,13 +57,13 @@ request = function(req_id)
 end
 
 response = function(status, headers, body)
---  if status ~= 200 then
+  if status ~= 200 then
       io.write("------------------------------\n")
       io.write("Response with status: ".. status .."\n")
       io.write("------------------------------\n")
       io.write("[response] Body:\n")
       io.write(body .. "\n")
---  end
+  end
 end
 
 function init(rand_seed)
