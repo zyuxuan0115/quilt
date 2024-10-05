c7604a65e0fc2384a1da3f12364ca509d38fcadc
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-05 15:26:40 -0400

    update

--- DeathStarBench/media_microservice_rust_lite/merge_local/merge.sh
+++ DeathStarBench/media_microservice_rust_lite/merge_local/merge.sh
@@ -37,7 +37,8 @@ function merge {
   ./rm_redundant_bc.py $CALLER_FUNC/$WORK_DIR
   CALLER_IR=$(find $CALLER_FUNC/$WORK_DIR/ -type f -name "function-*.bc" -not -name "*.*.*")
   mv $CALLER_IR caller.bc
-  $LLVM_DIR/llvm-dis caller.bc -o merged.ll
+  #$LLVM_DIR/llvm-dis caller.bc -o merged.ll
+  cp caller.bc merged.bc
 
   for i in $(seq 2 $(($NUM_ARGS-1)) );
   do
@@ -48,17 +49,45 @@ function merge {
     rm -rf $CALLEE_FUNC/$WORK_DIR/panic_unwind-*.bc
     rm -rf $CALLEE_FUNC/$WORK_DIR/*no-opt*
     ./rm_redundant_bc.py $CALLEE_FUNC/$WORK_DIR
-    $LLVM_DIR/opt -S $CALLEE_IR -passes=merge-rust-func -rename-callee-rr -callee-name-rr=$CALLEE_FUNC -o callee.ll
+    $LLVM_DIR/opt $CALLEE_IR -passes=merge-rust-func -rename-callee-rr -callee-name-rr=$CALLEE_FUNC -o callee.bc
     mv $CALLEE_IR $CALLEE_FUNC.bc
-    $LLVM_DIR/llvm-link merged.ll callee.ll -S -o caller_and_callee.ll
-    $LLVM_DIR/opt caller_and_callee.ll -strip-debug -o caller_and_callee_nodebug.ll -S
-    $LLVM_DIR/opt caller_and_callee_nodebug.ll -passes=merge-rust-func -callee-name-rr=$CALLEE_FUNC -o merged.bc
-    $LLVM_DIR/llvm-dis merged.bc -o merged.ll
+    $LLVM_DIR/llvm-link merged.bc callee.bc -o caller_and_callee.bc
+    $LLVM_DIR/opt caller_and_callee.bc -strip-debug -o caller_and_callee_nodebug.bc
+    $LLVM_DIR/opt caller_and_callee_nodebug.bc -passes=merge-rust-func -callee-name-rr=$CALLEE_FUNC -o merged.bc
     cp $CALLEE_FUNC/$WORK_DIR/*.bc $CALLER_FUNC/$WORK_DIR
+    mv callee.bc $CALLEE_IR
   done
 
   mv merged.bc $CALLER_IR
 }
+
+
+function merge_existing {
+  # prepare for merging
+  rm -rf $CALLER_FUNC/$WORK_DIR/panic_abort-*.*
+  rm -rf $CALLER_FUNC/$WORK_DIR/*no-opt*
+  ./rm_redundant_bc.py $CALLER_FUNC/$WORK_DIR
+  CALLER_IR=$(find $CALLER_FUNC/$WORK_DIR/ -type f -name "function-*.bc" -not -name "*.*.*")
+  mv $CALLER_IR caller.bc
+  cp caller.bc merged.bc
+
+  for i in $(seq 2 $(($NUM_ARGS-1)) );
+  do
+    CALLEE_FUNC=${ARGS[$i]}
+    CALLEE_IR=$(find $CALLEE_FUNC/$WORK_DIR/ -type f -name "function-*.bc" -not -name "*.*.*")
+    mv $CALLEE_IR callee.bc
+    $LLVM_DIR/llvm-link merged.bc callee.bc -o caller_and_callee.bc
+    $LLVM_DIR/opt caller_and_callee.bc -strip-debug -o caller_and_callee_nodebug.bc
+    $LLVM_DIR/opt caller_and_callee_nodebug.bc -passes=merge-rust-func -merge-existing -callee-name-rr=$CALLEE_FUNC -o merged.bc
+    cp $CALLEE_FUNC/$WORK_DIR/*.bc $CALLER_FUNC/$WORK_DIR
+    mv callee.bc $CALLEE_IR
+  done
+
+  mv merged.bc $CALLER_IR
+}
+
+
+
 
 
 function wrap_shared_lib {
@@ -69,9 +98,6 @@ function wrap_shared_lib {
   && gcc -c *.S && gcc -c *.c && rm *.S *.c \
   && ./implib-gen.py $C_LIB/libssl.so.1.1 2>/dev/null \
   && gcc -c *.S && gcc -c *.c && rm *.S *.c 
-
-#  && ./implib-gen.py $C_LIB/libcurl.so.4 2>/dev/null \
-#  && gcc -c *.S && gcc -c *.c && rm *.S *.c \
 
   cd .. && cp Implib.so/*.o .  && rm -rf Implib.so
 }
@@ -84,7 +110,6 @@ function link {
   $LLVM_DIR/opt func.bc -passes=remove-redundant -o function.bc
   $LLVM_DIR/llc -filetype=obj -O3 --function-sections --data-sections function.bc -o function.o
   wrap_shared_lib
-  #gcc -no-pie -flto -Wl,--strip-debug -Wl,--gc-sections -Wl,--as-needed -L$RUST_LIB *.o -o function $LINKER_FLAGS
   gcc -no-pie -flto -Wl,--strip-debug -Wl,--gc-sections -Wl,--as-needed *.o -o function $LINKER_FLAGS
 }
 
@@ -108,6 +133,9 @@ compile)
     ;;
 merge)
     merge
+    ;;
+merge_existing)
+    merge_existing
     ;;
 link)
     link
