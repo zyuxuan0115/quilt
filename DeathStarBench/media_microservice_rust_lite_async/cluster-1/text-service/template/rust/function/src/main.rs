596175ae9ff0fe733cd4ef5a182dc2b45df42b73
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-10 14:56:34 -0600

    update

--- DeathStarBench/media_microservice_rust_lite_async/cluster-1/text-service/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust_lite_async/cluster-1/text-service/template/rust/function/src/main.rs
@@ -5,7 +5,12 @@ use std::{collections::HashMap, time::{SystemTime,Duration, Instant}};
 fn main() {
   let input: String = get_arg_from_caller();
   //let now =  Instant::now();
-  let _ = make_rpc("compose-review-upload-text", input);
+  
+  let handle = thread::spawn(move || {
+    make_rpc("compose-review-upload-text", input)
+  });
+
+  handle.join().unwrap();
 
   //let new_now =  Instant::now();
   //println!("SocialGraphFollow: {:?}", new_now.duration_since(now));
