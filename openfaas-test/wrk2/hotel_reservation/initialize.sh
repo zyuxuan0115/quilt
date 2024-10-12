6019430cf09bdf491156c8d97936b4352f95499b
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-11 18:39:37 -0600

    update

---
+++ openfaas-test/wrk2/hotel_reservation/initialize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WRK_BIN=../wrk
+WRK_SCRIPT="lua_files/$1.lua"
+CLUSTER_ID=$2
+# cluster 1 IP
+ENTRY_HOST="http://128.110.96.54:30080"
+if [[ $CLUSTER_ID -eq 2 ]]
+then
+  # cluster 2 IP
+  ENTRY_HOST="http://128.110.96.54:30081"
+fi
+
+QPS=1000
+
+$WRK_BIN -t 1 -c 1 -d 60 -L -U \
+	 -s $WRK_SCRIPT \
+	 $ENTRY_HOST -R $QPS 2>/dev/null > output_$1.log
