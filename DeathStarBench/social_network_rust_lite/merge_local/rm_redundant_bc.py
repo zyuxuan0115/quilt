92cbc707966273ebd609869e6d8e949fe3c91145
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-21 19:56:46 -0400

    update

--- DeathStarBench/social_network_rust_lite/merge_local/rm_redundant_bc.py
+++ DeathStarBench/social_network_rust_lite/merge_local/rm_redundant_bc.py
@@ -8,10 +8,6 @@ def list_files_with_suffix(directory, suffix):
     files = glob.glob(os.path.join(directory, f"*{suffix}"))
     return files
 
-
-args = sys.argv
-
-
 def main():
     files = list_files_with_suffix(sys.argv[1], ".rcgu.bc")
     for file in files:
@@ -24,10 +20,7 @@ def main():
         if len(words)>2 and words[2]=="__rust_alloc" and words[1]=="T":
           print(file)
           os.system("mv "+file+" "+sys.argv[1]+"/function_keep.bc")
-    
     os.system("rm -rf "+sys.argv[1]+"/*.rcgu.bc");
-    os.system("rm -rf "+sys.argv[1]+"/*.rcgu.o");
-
 
 if __name__ == "__main__":
     main()
