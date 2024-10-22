814403dda4601b9e7cb634e8d939d1193b3fe753
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-21 22:34:11 -0400

    update

--- openfaas-test/wrk2/media_microservice/test.sh
+++ openfaas-test/wrk2/media_microservice/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+IP=130.127.133.69
 WRK_BIN=../wrk
 WRK_SCRIPT="lua_files/$1.lua"
 CLUSTER_ID=$2
 # cluster 1 IP
-ENTRY_HOST="http://130.127.133.69:30080"
+ENTRY_HOST=http://$IP:30080
 if [[ $CLUSTER_ID -eq 2 ]]
 then
   # cluster 2 IP
-  ENTRY_HOST="http://130.127.133.69:30081"
+  ENTRY_HOST=http://$IP:30081
 fi
 
 QPS=1000
