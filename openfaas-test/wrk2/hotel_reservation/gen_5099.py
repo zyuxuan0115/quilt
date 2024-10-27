8ae0bebf15b6ae9f874bb337418d5e3ba80ed462
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-10-27 17:58:25 -0400

    update

--- openfaas-test/wrk2/hotel_reservation/gen_5099.py
+++ openfaas-test/wrk2/hotel_reservation/gen_5099.py
@@ -87,10 +87,14 @@ x = np.arange(len(fifty_latency_normalized))
 
 fig, ax = plt.subplots()
 width = 0.18
-bars1 = ax.bar(x - width*1.5, fifty_latency_normalized, width, label='50% latency sync')
-bars2 = ax.bar(x - width/2, [0,0,0], width, label='50% latency async')
-bars3 = ax.bar(x + width/2, ninety_latency_normalized, width, label='99% latency sync')
-bars4 = ax.bar(x + width*1.5, [0,0,0], width, label='99% latency async')
+bars1 = ax.bar(x - width*1.5, fifty_latency_normalized, width, label='50% latency sync', color='darkviolet', linewidth=1, edgecolor='black', zorder=3)
+
+bars2 = ax.bar(x - width/2, [0,0,0], width, label='50% latency async', color='violet', linewidth=1, edgecolor='black', zorder=3)
+
+bars3 = ax.bar(x + width/2, ninety_latency_normalized, width, label='99% latency sync', color='plum', linewidth=1, edgecolor='black', zorder=3)
+
+bars4 = ax.bar(x + width*1.5, [0,0,0], width, label='99% latency async', color='lavenderblush', linewidth=1, edgecolor='black', zorder=3)
+
 
 label = ["search-handler(10)", "reservation-handler(3)", "nearby-cinema(2)"]
 
@@ -100,9 +104,9 @@ ax.set_title('HotelReservation Normalized Latency')
 ax.set_xticks(x)
 ax.set_xticklabels(label)
 ax.legend()
+ax.grid(True, linestyle='--', zorder=0)  
 
 plt.ylim(0.2, 1.2)
-plt.grid(True, linestyle='--')  
-plt.savefig("5099latency.png", dpi=300)  
+plt.savefig("5099latencyhr.png", dpi=300)  
 
 plt.show()
--- openfaas-test/wrk2/hotel_reservation/gen_tput.py
+++ openfaas-test/wrk2/hotel_reservation/gen_tput.py
@@ -91,10 +91,13 @@ x = np.arange(len(tput_normalized))
 
 fig, ax = plt.subplots()
 width = 0.18
-bars1 = ax.bar(x - width*1.5, tput_normalized, width, label='request/sec sync')
-bars2 = ax.bar(x - width/2, [0,0,0], width, label='request/sec async')
-bars3 = ax.bar(x + width/2, tput_kb_normalized, width, label='transfer/sec (KB) sync')
-bars4 = ax.bar(x + width*1.5, [0,0,0], width, label='transfer/sec (KB) async')
+bars1 = ax.bar(x - width*1.5, tput_normalized, width, label='request/sec sync', color='darkviolet', linewidth=1, edgecolor='black', zorder=3)
+
+bars2 = ax.bar(x - width/2, [0,0,0], width, label='request/sec async', color='violet', linewidth=1, edgecolor='black', zorder=3)
+
+bars3 = ax.bar(x + width/2, tput_kb_normalized, width, label='transfer/sec (KB) sync', color='plum', linewidth=1, edgecolor='black', zorder=3)
+
+bars4 = ax.bar(x + width*1.5, [0,0,0], width, label='transfer/sec (KB) async', color='lavenderblush', linewidth=1, edgecolor='black', zorder=3)
 
 label = ["search-handler(6)", "reservation-handler(3)", "nearby-cinema(2)"]
 
@@ -104,8 +107,8 @@ ax.set_title('HotelReservation Normalized Throughput')
 ax.set_xticks(x)
 ax.set_xticklabels(label)
 ax.legend()
+ax.grid(True, linestyle='--', zorder=0)  
 
-plt.grid(True, linestyle='--')  
-plt.savefig("tput.png", dpi=300)  
+plt.savefig("tputhr.png", dpi=300)  
 
 plt.show()
