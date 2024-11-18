89e002c47c60bef7afd4bc269cdcbff962dd7939
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-17 19:54:24 -0500

    update

--- DeathStarBench/social_network_rust_lite/cluster-1/social-graph-insert-user/build.sh
+++ DeathStarBench/social_network_rust_lite/cluster-1/social-graph-insert-user/build.sh
@@ -15,23 +15,30 @@ function build_rust_test {
     sudo docker system prune -f
 }
 
-function push_rust_test {
-    sudo docker push zyuxuan0115/sn-social-graph-insert-user:latest
-}
-
-function build {
-    build_rust_test
+function build_openwhisk {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/sn-social-graph-insert-user:latest \
+        -f Dockerfile.wsk \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    sudo docker system prune -f
 }
 
 function push {
-    push_rust_test
+    sudo docker push zyuxuan0115/sn-social-graph-insert-user:latest
 }
 
 case "$1" in
-build)
-    build
+openfaas)
+    build_openfaas
+    ;;
+openwhisk)
+    build_openwhisk
     ;;
 push)
     push
     ;;
 esac
+
