9ab08bf522301a87921a5d7dd598da7e71e7d237
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-18 22:18:12 -0500

    update

--- DeathStarBench/social_network_rust_lite/cluster-1/social-graph-get-followees/build.sh
+++ DeathStarBench/social_network_rust_lite/cluster-1/social-graph-get-followees/build.sh
@@ -3,11 +3,12 @@
 #ROOT_DIR=`realpath $(dirname $0)/..`
 ROOT_DIR=$(pwd)
 echo $ROOT_DIR
+FUNC=social-graph-get-followees
 
-function build_rust_test {
+function build_openfaas {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/sn-social-graph-get-followees:latest \
+    sudo docker build -t zyuxuan0115/sn-$FUNC:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
@@ -15,21 +16,27 @@ function build_rust_test {
     sudo docker system prune -f
 }
 
-function push_rust_test {
-    sudo docker push zyuxuan0115/sn-social-graph-get-followees:latest
-}
-
-function build {
-    build_rust_test
+function build_openwhisk {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
+        -f Dockerfile.wsk \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    sudo docker system prune -f
 }
 
 function push {
-    push_rust_test
+    sudo docker push zyuxuan0115/sn-$FUNC:latest
 }
 
 case "$1" in
-build)
-    build
+build_openfaas)
+    build_openfaas
+    ;;
+build_openwhisk)
+    build_openwhisk
     ;;
 push)
     push
