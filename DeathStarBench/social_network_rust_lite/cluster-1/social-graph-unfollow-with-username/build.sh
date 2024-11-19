e5833af9ffd9720409c5f7387f3aa57c2c585513
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-18 23:36:30 -0500

    update

--- DeathStarBench/social_network_rust_lite/cluster-1/social-graph-unfollow-with-username/build.sh
+++ DeathStarBench/social_network_rust_lite/cluster-1/social-graph-unfollow-with-username/build.sh
@@ -6,39 +6,54 @@ echo $ROOT_DIR
 FUNC=social-graph-unfollow-with-username
 
 function build_openfaas {
-    cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/sn-$FUNC:latest \
+  cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
+  cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
+  sudo docker build -t zyuxuan0115/sn-$FUNC:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
-    rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
-    rm -rf $ROOT_DIR/template/rust/DbInterface
-    sudo docker system prune -f
+  rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
+  rm -rf $ROOT_DIR/template/rust/DbInterface
+  sudo docker system prune -f
 }
 
 function build_openwhisk {
-    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
+  cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+  cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+  sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
         -f Dockerfile.wsk \
         $ROOT_DIR/template/rust
-    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
-    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
-    sudo docker system prune -f
+  rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+  rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+  sudo docker system prune -f
 }
 
 function push {
-    sudo docker push zyuxuan0115/sn-$FUNC:latest
+  sudo docker push zyuxuan0115/sn-$FUNC:latest
+}
+
+function deploy_openfaas {
+  faas-cli deploy -f deployFunc.yml
+}
+
+function deploy_openwhisk {
+  wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC:latest    
 }
 
 case "$1" in
-build_openfaas)
+openfaas)
     build_openfaas
     ;;
-build_openwhisk)
+openwhisk)
     build_openwhisk
     ;;
 push)
     push
     ;;
+deploy_openfaas)
+    deploy_openfaas
+    ;;
+deploy_openwhisk)
+    deploy_openwhisk
+    ;;
 esac
+
