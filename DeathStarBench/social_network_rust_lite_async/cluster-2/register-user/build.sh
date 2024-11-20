cd0d758e63a895d92cdadba72659ffb897d5ce35
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-20 12:10:58 -0500

    update

--- DeathStarBench/social_network_rust_lite_async/cluster-2/register-user/build.sh
+++ DeathStarBench/social_network_rust_lite_async/cluster-2/register-user/build.sh
@@ -3,35 +3,56 @@
 #ROOT_DIR=`realpath $(dirname $0)/..`
 ROOT_DIR=$(pwd)
 echo $ROOT_DIR
+FUNC=register-user
 
-function build_rust_test {
+function build_openfaas {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/sn-register-user-async:latest \
+    sudo docker build -t zyuxuan0115/sn-$FUNC-async:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
     rm -rf $ROOT_DIR/template/rust/DbInterface
+    sudo docker system prune -f 
+}
+
+function build_openwhisk {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC-async:latest \
+        -f Dockerfile.wsk \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
     sudo docker system prune -f
 }
 
-function push_rust_test {
-    sudo docker push zyuxuan0115/sn-register-user-async:latest
-}
-
-function build {
-    build_rust_test
-}
-
 function push {
-    push_rust_test
+    sudo docker push zyuxuan0115/sn-$FUNC-async:latest
+}
+
+function deploy_openfaas {
+  faas-cli deploy -f deployFunc.yml
+}
+
+function deploy_openwhisk {
+  wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC-async:latest    
 }
 
 case "$1" in
-build)
-    build
+openfaas)
+    build_openfaas
+    ;;
+openwhisk)
+    build_openwhisk
     ;;
 push)
     push
+    ;;
+deploy_openfaas)
+    deploy_openfaas
+    ;;
+deploy_openwhisk)
+    deploy_openwhisk
     ;;
 esac
