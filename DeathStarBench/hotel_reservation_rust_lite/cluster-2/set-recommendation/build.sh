cb981b23eb0121a6dcc11eeac87f2de6c13a93a0
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-21 20:30:32 -0500

    update

--- DeathStarBench/hotel_reservation_rust_lite/cluster-2/set-recommendation/build.sh
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-2/set-recommendation/build.sh
@@ -3,11 +3,12 @@
 #ROOT_DIR=`realpath $(dirname $0)/..`
 ROOT_DIR=$(pwd)
 echo $ROOT_DIR
+FUNC=set-recommendation
 
-function build_rust_test {
+function build_openfaas {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/hr-set-recommendation:latest \
+    sudo docker build -t zyuxuan0115/hr-$FUNC:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
@@ -15,23 +16,43 @@ function build_rust_test {
     sudo docker system prune -f
 }
 
-function push_rust_test {
-    sudo docker push zyuxuan0115/hr-set-recommendation:latest
-}
-
-function build {
-    build_rust_test
+function build_openwhisk {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/hr-$FUNC:latest \
+        -f Dockerfile.wsk \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    sudo docker system prune -f
 }
 
 function push {
-    push_rust_test
+    sudo docker push zyuxuan0115/hr-$FUNC:latest
+}
+
+function deploy_openfaas {
+  faas-cli deploy -f deployFunc.yml
+}
+
+function deploy_openwhisk {
+  wsk action create $FUNC --docker zyuxuan0115/hr-$FUNC:latest    
 }
 
 case "$1" in
-build)
-    build
+openfaas)
+    build_openfaas
+    ;;
+openwhisk)
+    build_openwhisk
     ;;
 push)
     push
+    ;;
+deploy_openfaas)
+    deploy_openfaas
+    ;;
+deploy_openwhisk)
+    deploy_openwhisk
     ;;
 esac
