93118026130607e3264d40be633d39c66859fd11
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-22 14:53:41 -0500

    update

---
+++ DeathStarBench/social_network_rust_lite/cluster-2/register-user/invoke.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+APIHOST=130.127.133.209:32001
+FUNC=register-user
+#wsk action delete $FUNC
+#sleep 5
+#wsk action create $FUNC --docker zyuxuan0115/sn-register-user
+curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
+-X POST -H "Content-Type: application/json" \
+-d '{"first_name":"Yuxuan","last_name":"Zhang","username":"zyuxuan","password":"umichandupenn"}'
