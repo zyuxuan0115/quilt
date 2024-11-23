269905c05cde1dc590bb672a40db8e2568609434
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-22 22:41:36 -0500

    update

--- DeathStarBench/social_network_rust_lite/cluster-1/social-graph-unfollow-with-username/invoke.sh
+++ DeathStarBench/social_network_rust_lite/cluster-1/social-graph-unfollow-with-username/invoke.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
 APIHOST=130.127.133.209:32001
-FUNC=social-graph-follow-with-username
-wsk action delete $FUNC
+FUNC=social-graph-unfollow-with-username
+#wsk action delete $FUNC
 #sleep 5
-wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC
+#wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC
 curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
 -X POST -H "Content-Type: application/json" \
 -d '{"user_name":"twenisch","followee_name":"todda"}'
--- DeathStarBench/social_network_rust_lite/cluster-1/social-graph-unfollow-with-username/template/rust/function/src/main.rs
+++ DeathStarBench/social_network_rust_lite/cluster-1/social-graph-unfollow-with-username/template/rust/function/src/main.rs
@@ -7,8 +7,18 @@ fn main() {
 //  let time_0 = Instant::now();
   let follow_info: SocialGraphFollowWithUsernameArgs = serde_json::from_str(&input).unwrap();
 //  let time_1 = Instant::now();
-  let user_id_str:String = make_rpc("get-user-id", follow_info.user_name);
-  let followee_id_str:String = make_rpc("get-user-id", follow_info.followee_name);
+  let get_user_id_args1 = GetUsrIdArgs {
+    username: follow_info.user_name,
+  };
+  let get_user_id_args1_str = serde_json::to_string(&get_user_id_args1).unwrap();
+  
+  let get_user_id_args2 = GetUsrIdArgs {
+    username: follow_info.followee_name,
+  };
+  let get_user_id_args2_str = serde_json::to_string(&get_user_id_args2).unwrap();
+ 
+  let user_id_str:String = make_rpc("get-user-id", get_user_id_args1_str);
+  let followee_id_str:String = make_rpc("get-user-id", get_user_id_args2_str);
 //  let time_2 = Instant::now();
   let user_id: i64 = user_id_str.parse::<i64>().unwrap();  
   let followee_id: i64 = followee_id_str.parse::<i64>().unwrap();  
