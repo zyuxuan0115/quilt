fd08c707eafcb9e208b07dc5dd64fb9c94951b79
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-23 15:52:21 -0500

    udpate

--- DeathStarBench/social_network_rust_lite/cluster-2/compose-creator-with-userid/invoke.sh
+++ DeathStarBench/social_network_rust_lite/cluster-2/compose-creator-with-userid/invoke.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
-APIHOST=130.127.133.209:32001
+APIHOST=localhost:9999
 FUNC=compose-creator-with-userid
 #wsk action delete $FUNC
 #sleep 5
-wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC
+#wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC
 curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
 -X POST -H "Content-Type: application/json" \
 -d '{"user_id":11028,"username":"twenisch"}'
--- DeathStarBench/social_network_rust_lite/cluster-2/url-shorten-service/invoke.sh
+++ DeathStarBench/social_network_rust_lite/cluster-2/url-shorten-service/invoke.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
-APIHOST=130.127.133.209:32001
+APIHOST=localhost:9999
 FUNC=url-shorten-service
 #wsk action delete $FUNC
 #sleep 5
