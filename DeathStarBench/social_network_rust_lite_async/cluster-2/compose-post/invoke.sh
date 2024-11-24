ffe5a43a8a5db5b45efa060f18cff1248e0c8fdc
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-24 12:18:51 -0500

    update

---
+++ DeathStarBench/social_network_rust_lite_async/cluster-2/compose-post/invoke.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+APIHOST=localhost:9999
+FUNC=compose-post
+wsk action delete $FUNC
+sleep 5
+wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC
+curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
+-X POST -H "Content-Type: application/json" \
+-d '{"username":"twenisch","user_id":11028,"text":"Hey, this is @twenisch! Nice to meet you and welcome to my personal web: https://eecs.umich.edu/~twenisch @bobk ","media_ids":[],"media_types":[],"post_type":"POST"}'
