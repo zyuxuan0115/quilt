5513ba9404b18bbb59ac5e5b6a42b0e83df672fd
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-23 21:07:51 -0500

    udpate

---
+++ DeathStarBench/hotel_reservation_rust_lite/cluster-2/check-user/invoke.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+APIHOST=localhost:9999
+FUNC=check-user
+#wsk action delete $FUNC
+#sleep 5
+#wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC
+curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
+-X POST -H "Content-Type: application/json" \
+-d '{"post_id":1723}'
