25748ac41d47fc2852846d4f59cfcbad03fa71f6
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-24 22:17:33 -0500

    update

--- DeathStarBench/media_microservice_rust_lite_async/cluster-2/upload-movie-review/invoke.sh
+++ DeathStarBench/media_microservice_rust_lite_async/cluster-2/upload-movie-review/invoke.sh
@@ -4,7 +4,7 @@ APIHOST=localhost:9999
 FUNC=upload-movie-review
 #wsk action delete $FUNC
 #sleep 5
-#wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC
+#wsk action create $FUNC --docker zyuxuan0115/mm-$FUNC-async
 curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
 -X POST -H "Content-Type: application/json" \
 -d '{"post_id":1723}'
