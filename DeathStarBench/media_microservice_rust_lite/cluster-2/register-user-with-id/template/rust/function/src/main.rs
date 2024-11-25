b789c9fa8760d5091614ff19701860fdbd88df29
Yuxuan Zhang <kate0115.zyx@gmail.com>
2024-11-25 01:00:52 -0500

    Update main.rs

--- DeathStarBench/media_microservice_rust_lite/cluster-2/register-user-with-id/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust_lite/cluster-2/register-user-with-id/template/rust/function/src/main.rs
@@ -29,8 +29,9 @@ fn main() {
   let res: redis::RedisResult<String> = con.hget(&uname[..], "user_id");
   match res {
     Ok(x) => {
-      println!("User {} already existed", new_user_info.username);
-      panic!("User {} already existed", new_user_info.username);
+      let err_msg = format!("User {} already existed", new_user_info.username);
+      send_return_value_and_err_msg("".to_string(), err_msg);
+      process::exit(0);
     },
     Err(_) => {
       let mut pw_sha: String = String::from(&new_user_info.password[..]);
