d6bdf62666c9e2cadeb67dc1e0273257f64bc906
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-24 22:06:03 -0500

    update

--- DeathStarBench/media_microservice_rust_lite/cluster-1/page-service/template/rust/function/src/main.rs
+++ DeathStarBench/media_microservice_rust_lite/cluster-1/page-service/template/rust/function/src/main.rs
@@ -5,8 +5,11 @@ fn main() {
   let input: String = get_arg_from_caller();
   //let now = Instant::now();
   let args: PageServiceArgs = serde_json::from_str(&input).unwrap();
-
-  let read_movie_info_ret = make_rpc("read-movie-info", args.movie_id.clone());
+  let read_movie_info_args = ReadMovieInfoArgs {
+    movie_id: args.movie_id.clone(),
+  };
+  let read_movie_info_args_str = serde_json::to_string(&read_movie_info_args).unwrap();
+  let read_movie_info_ret = make_rpc("read-movie-info", read_movie_info_args_str);
   let movie_info: MovieInfoEntry = serde_json::from_str(&read_movie_info_ret).unwrap();
   let read_movie_reviews_args = ReadMovieReviewArgs {
     movie_id: args.movie_id.clone(),
@@ -18,7 +21,11 @@ fn main() {
   let reviews: Vec<ReviewEntry> = serde_json::from_str(&read_movie_review_ret).unwrap();
 
   let cast_info_ids: Vec<i64> = movie_info.casts.iter().map(|x| x.cast_info_id).collect();
-  let cast_info_id_str: String = serde_json::to_string(&cast_info_ids).unwrap();
+  let cast_info_args = ReadCastInfoArgs {
+    cast_ids: cast_info_ids,
+  };
+
+  let cast_info_id_str: String = serde_json::to_string(&cast_info_args).unwrap();
   let cast_info_str = make_rpc("read-cast-info", cast_info_id_str);
   let cast_info: Vec<CastInfoEntry> = serde_json::from_str(&cast_info_str).unwrap();
 
