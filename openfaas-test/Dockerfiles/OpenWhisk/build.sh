33222f1fa293dd8c48a5eb4a52091f5ca9b17bb5
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-11-25 18:32:39 -0500

    update

--- openfaas-test/Dockerfiles/OpenWhisk/build.sh
+++ openfaas-test/Dockerfiles/OpenWhisk/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo docker build -t zyuxuan0115/docker-skeleton:latest -f Dockerfile . 
+sudo docker build -t zyuxuan0115/docker-skeleton:latest -f Dockerfile.bin_ver . 
+#sudo docker build -t zyuxuan0115/docker-skeleton:latest -f Dockerfile . 
 sudo docker push zyuxuan0115/docker-skeleton:latest
