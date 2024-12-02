3615f8f0cc118a8907fe5b970a775908cb745ea2
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-02 12:49:55 -0500

    delete useless

--- openfaas-test/test-openwhisk/actionProxy/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker build -t zyuxuan0115/docker-skeleton:latest -f Dockerfile . 
-sudo docker push zyuxuan0115/docker-skeleton:latest
--- openfaas-test/test-openwhisk/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker build -t zyuxuan0115/c-stdin-action:latest -f Dockerfile . 
-sudo docker push zyuxuan0115/c-stdin-action:latest
