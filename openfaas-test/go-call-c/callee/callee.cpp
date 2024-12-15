5697ca7866f5d643b23f1a41618b9bffc54e1785
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-14 22:56:09 -0500

    update json

--- openfaas-test/go-call-c/callee/callee.cpp
+++ openfaas-test/go-call-c/callee/callee.cpp
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <openssl/rand.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
+#include <nlohmann/json.hpp>
 
 #define LENGTH 16  
+
+struct jsonMsg {
+  std::string msg;
+  std::string err;
+};
+
+void to_json(nlohmann::json& j, const jsonMsg& s) {
+    j = nlohmann::json{{"msg", s.msg}, {"err", s.err}};
+}
+
+void send_return_value_to_caller(char* output){
+  std::string message = std::string(output);
+  jsonMsg new_ojb{message, ""};
+  nlohmann::json j = new_ojb;
+  std::string json_string = j.dump(); 
+  printf("%s", json_string.c_str());
+}
+
+char* get_arg_from_caller(){
+  char* buf;
+  buf = (char*)malloc(sizeof(char)*1000);
+  memset(buf, 0, 1000);
+  ssize_t read_len = read(STDIN_FILENO, (void*)buf, 1000*sizeof(char));
+  return buf; 
+}
 
 void generate_random_string(char *str, size_t length) {
   const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -22,10 +51,10 @@ void generate_random_string(char *str, size_t length) {
 }
 
 int main() {
+  char* input = get_arg_from_caller();
   char random_string[LENGTH + 1]; 
   generate_random_string(random_string, LENGTH);
-
-  printf("%s\n", random_string);
-
+  
+  send_return_value_to_caller(random_string);
   return 0;
 }
