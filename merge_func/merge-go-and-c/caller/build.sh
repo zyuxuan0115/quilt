413eb054c4849e6e981642f8ef92683fc64b68e7
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-16 17:50:46 -0500

    update

--- merge_func/merge-go-and-c/caller/build.sh
+++ merge_func/merge-go-and-c/caller/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker build --no-cache  -t zyuxuan0115/go-env:latest -f Dockerfile . 
-sudo docker push zyuxuan0115/go-env:latest
+sudo docker build --no-cache  -t zyuxuan0115/go-caller:latest -f Dockerfile . 
+sudo docker push zyuxuan0115/go-caller:latest
