a466f04054750088545ac499c25e9ea25f4b2006
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-16 18:14:03 -0500

    update

--- openfaas-test/Dockerfiles/OpenFaaS/build.sh
+++ openfaas-test/Dockerfiles/OpenFaaS/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function build {
-    sudo docker build -t zyuxuan0115/rust-env:latest \
+    sudo docker build -t zyuxuan0115/openfaas-skeleton:latest \
         -f Dockerfile .
-    sudo docker push zyuxuan0115/rust-env:latest
+    sudo docker push zyuxuan0115/openfaas-skeleton:latest
 }
 
 case "$1" in
