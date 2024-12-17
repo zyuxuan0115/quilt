16f74f5e62649d65d05695fc647efbed0ad88727
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-16 20:45:20 -0500

    update

--- merge_func/merge-go-and-c/caller/build.sh
+++ merge_func/merge-go-and-c/caller/build.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
-sudo docker build --no-cache  -t zyuxuan0115/go-caller:latest -f Dockerfile . 
-sudo docker push zyuxuan0115/go-caller:latest
+AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+APIHOST=localhost:9999
+FUNC=go-caller
+
+function build {
+  sudo docker build --no-cache  -t zyuxuan0115/$FUNC:latest -f Dockerfile . 
+  sudo docker push zyuxuan0115/$FUNC:latest
+}
+
+function deploy {
+  wsk action create $FUNC --docker zyuxuan0115/$FUNC:latest
+}
+
+function invoke {
+  curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
+  -X POST -H "Content-Type: application/json" \
+  -d '{"msg":""}'
+}
+
+case "$1" in
+build)
+    build
+    ;;
+deploy)
+    deploy
+    ;;
+invoke)
+    invoke
+    ;;
+esac
