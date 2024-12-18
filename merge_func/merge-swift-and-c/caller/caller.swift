d977fd980e57af9cea77306cefc1b26f9c750c7d
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-18 00:54:05 -0500

    fix bugs in swift code

--- merge_func/merge-swift-and-c/caller/caller.swift
+++ merge_func/merge-swift-and-c/caller/caller.swift
@@ -1,4 +1,5 @@
 import Foundation
+import FoundationNetworking
 
 @main
 struct Function {
@@ -24,38 +25,42 @@ struct Function {
     print("\(message)")
   }
 
-  static func make_rpc(func_name: String, jsonstr: String) -> String {
-    var url = "http://localhost:9999/api/v1/namespaces/_/actions/"
-    url += func_name
-    url += "?blocking=true&result=true"
-    guard let url = URL(string: urlString) else {
+  static func make_rpc(func_name: String, jsonStr: String) -> String {
+    var urlStr = "http://localhost:9999/api/v1/namespaces/_/actions/"
+    urlStr += func_name
+    urlStr += "?blocking=true&result=true"
+    guard let url = URL(string: urlStr) else {
       print("Invalid URL")
       return ""
     }
+
+    let bodyData = jsonStr.data(using: .utf8)
 
     // Create a POST request
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.httpBody = jsonstr
+    request.httpBody = bodyData
 
+    let group = DispatchGroup()
+    var resp = ""
+    group.enter()
     // Create a URLSession data task
     let task = URLSession.shared.dataTask(with: request) { data, response, error in
       if let res = data {
         do {
-          if let jsonData = res.data(using: .utf8) {
-            let decoder = JSONDecoder()
-            let message_err = decoder.decode(Message.self, from: jsonData)
-          } 
-          else {
-            print("Invalid JSON string")
-          }
+          let decoder = JSONDecoder()
+          let message_err = try! decoder.decode(Message.self, from: res)
+          resp = message_err.msg
         }
       }
+      group.leave()
     }
+    
 
     // Start the task
     task.resume()
-    return ""
+    group.wait() 
+    return resp
   }
 }
