111bf3b7cacf193c1cbaa11225a772c401743bd2
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-21 21:39:43 -0500

    update

--- openfaas-test/Dockerfiles/fission/binary-based/c-func-bin/build.sh
+++ openfaas-test/Dockerfiles/fission/binary-based/c-func-bin/build.sh
@@ -3,8 +3,10 @@
 FUNC=c-func-bin
 
 make 
-sudo chmod 777 function
-sudo chown root:root function
-fission function create --name $FUNC --env fission-bin-env --code function
+echo $FUNC > metadata.txt
+objcopy --add-section .metadata=metadata.txt function function_metadata
+sudo chmod 777 function_metadata
+sudo chown root:root function_metadata
+fission function create --name $FUNC --env fission-bin-env --code function_metadata
 fission route create --method POST --url /$FUNC --function $FUNC
 
