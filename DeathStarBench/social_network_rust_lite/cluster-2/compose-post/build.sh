887e2e031b43c204efff5a86d130045274185e04
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-22 15:28:35 -0500

    update

--- DeathStarBench/social_network_rust_lite/cluster-2/compose-post/build.sh
+++ DeathStarBench/social_network_rust_lite/cluster-2/compose-post/build.sh
@@ -8,12 +8,12 @@ FUNC=compose-post
 function build_openfaas {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
+    sudo docker build -t zyuxuan0115/sn-$FUNC:latest \
         -f Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
     rm -rf $ROOT_DIR/template/rust/DbInterface
-    sudo docker system prune -f
+    sudo docker system prune -f 
 }
 
 function build_openwhisk {
@@ -27,8 +27,38 @@ function build_openwhisk {
     sudo docker system prune -f
 }
 
+function build_fission_container {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
+        -f Dockerfile.fs_container \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    sudo docker system prune -f
+}
+
+function build_fission_bin {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
+        -f Dockerfile.fs_bin \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    sudo docker system prune -f
+    sudo docker create --name temp-container zyuxuan0115/sn-$FUNC:latest
+    sudo docker cp temp-container:/home/rust/function/target/release/function ./function_orig
+    sudo docker rm temp-container
+    echo $FUNC > metadata.txt
+    objcopy --add-section .metadata=metadata.txt function_orig function
+    sudo chmod 777 function
+    sudo chown root:root function
+    rm -rf metadata.txt function_orig
+}
+
 function push {
-    sudo docker push zyuxuan0115/sn-$FUNC:latest
+  sudo docker push zyuxuan0115/sn-$FUNC:latest
 }
 
 function deploy_openfaas {
@@ -39,12 +69,31 @@ function deploy_openwhisk {
   wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC:latest    
 }
 
+function deploy_fission_c {
+  fission function run-container --name $FUNC \
+    --image docker.io/zyuxuan0115/sn-$FUNC \
+    --port 8888
+  fission route create --method POST \
+    --url /$FUNC --function $FUNC
+}
+
+function deploy_fission_b {
+  fission function create --name $FUNC --env fission-bin-env --code function
+  fission route create --method POST --url /$FUNC --function $FUNC
+}
+
 case "$1" in
 openfaas)
     build_openfaas
     ;;
 openwhisk)
     build_openwhisk
+    ;;
+fission_c)
+    build_fission_container
+    ;;
+fission_b)
+    build_fission_bin
     ;;
 push)
     push
@@ -55,5 +104,10 @@ deploy_openfaas)
 deploy_openwhisk)
     deploy_openwhisk
     ;;
+deploy_fission_c)
+    deploy_fission_c
+    ;;
+deploy_fission_b)
+    deploy_fission_b
+    ;;
 esac
-
