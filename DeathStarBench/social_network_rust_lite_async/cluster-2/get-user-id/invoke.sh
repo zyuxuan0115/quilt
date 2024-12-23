6b7d9e7b21f92940c887822b87d397455b63981f
zyuxuan0115 <zyuxuan@seas.upenn.edu>
2024-12-23 15:36:58 -0500

    update

--- DeathStarBench/social_network_rust_lite_async/cluster-2/get-user-id/invoke.sh
+++ DeathStarBench/social_network_rust_lite_async/cluster-2/get-user-id/invoke.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/bash
-AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
-APIHOST=localhost:9999
+
 FUNC=get-user-id
-#wsk action delete $FUNC
-#sleep 5
-#wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC-async
-curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
--X POST -H "Content-Type: application/json" \
--d '{"username":"todda"}'
+
+function invoke_wsk {
+  AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+  APIHOST=localhost:9999
+  #wsk action delete $FUNC
+  #sleep 5
+  #wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC-async
+  curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
+  -X POST -H "Content-Type: application/json" \
+  -d '{"username":"todda"}'
+}
+
+function invoke_fission {
+  curl -XPOST http://localhost:8888/$FUNC \
+  -d '{"username":"todda"}'
+}
+
+case "$1" in
+wsk)
+    invoke_wsk
+    ;;
+fission)
+    invoke_fission
+    ;;
+esac
