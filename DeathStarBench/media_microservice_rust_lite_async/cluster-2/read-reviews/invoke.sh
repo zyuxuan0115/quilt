d0c72257f6f263b48a70893acb1c268c3739e846
zyuxuan0115 <zyuxuan@seas.upenn.edu>
2024-12-24 14:07:33 -0500

    update

--- DeathStarBench/media_microservice_rust_lite_async/cluster-2/read-reviews/invoke.sh
+++ DeathStarBench/media_microservice_rust_lite_async/cluster-2/read-reviews/invoke.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/bash
-AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
-APIHOST=localhost:9999
+
 FUNC=read-reviews
-#wsk action delete $FUNC
-#sleep 5
-#wsk action create $FUNC --docker zyuxuan0115/mm-$FUNC-async
-curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
--X POST -H "Content-Type: application/json" \
--d '{"review_ids":[98765,98766]}'
+
+function invoke_wsk {
+  AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
+  APIHOST=localhost:9999
+  #wsk action delete $FUNC
+  #sleep 5
+  #wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC
+  curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
+  -X POST -H "Content-Type: application/json" \
+  -d '{"review_ids":[98765,98766]}'
+}
+
+function invoke_fission {
+  curl -XPOST http://localhost:8888/$FUNC \
+  -d '{"review_ids":[98765,98766]}'
+}
+
+case "$1" in
+wsk)
+    invoke_wsk
+    ;;
+fission)
+    invoke_fission
+    ;;
+esac
--- DeathStarBench/media_microservice_rust_lite_async/cluster-2/read-user-review/invoke.sh
+++ DeathStarBench/media_microservice_rust_lite_async/cluster-2/read-user-review/invoke.sh
@@ -1,10 +1,113 @@
-#!/usr/bin/bash
-AUTH=23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
-APIHOST=localhost:9999
+#!/bin/bash
+
+#ROOT_DIR=`realpath $(dirname $0)/..`
+ROOT_DIR=$(pwd)
+echo $ROOT_DIR
 FUNC=read-user-review
-#wsk action delete $FUNC
-#sleep 5
-#wsk action create $FUNC --docker zyuxuan0115/mm-$FUNC-async
-curl -u $AUTH "http://$APIHOST/api/v1/namespaces/_/actions/$FUNC?blocking=true&result=true" \
--X POST -H "Content-Type: application/json" \
--d '{"user_id":11078,"start":0,"stop":1}'
+
+function build_openfaas {
+    cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
+    sudo docker build -t zyuxuan0115/mm-$FUNC:latest \
+        -f Dockerfile \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface
+    sudo docker system prune -f 
+}
+
+function build_openwhisk {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC:latest \
+        -f Dockerfile.wsk \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    sudo docker system prune -f
+}
+
+function build_fission_container {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC:latest \
+        -f Dockerfile.fs_container \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    sudo docker system prune -f
+}
+
+function build_fission_bin {
+    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC:latest \
+        -f Dockerfile.fs_bin \
+        $ROOT_DIR/template/rust
+    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    sudo docker system prune -f
+    sudo docker create --name temp-container zyuxuan0115/mm-$FUNC:latest
+    sudo docker cp temp-container:/home/rust/function/target/release/function ./function_orig
+    sudo docker rm temp-container
+    echo $FUNC > metadata.txt
+    objcopy --add-section .metadata=metadata.txt function_orig function
+    sudo chmod 777 function
+    sudo chown root:root function
+    rm -rf metadata.txt function_orig
+}
+
+function push {
+  sudo docker push zyuxuan0115/mm-$FUNC:latest
+}
+
+function deploy_openfaas {
+  faas-cli deploy -f deployFunc.yml
+}
+
+function deploy_openwhisk {
+  wsk action create $FUNC --docker zyuxuan0115/mm-$FUNC:latest    
+}
+
+function deploy_fission_c {
+  fission function run-container --name $FUNC \
+    --image docker.io/zyuxuan0115/mm-$FUNC \
+    --port 8888
+  fission route create --method POST \
+    --url /$FUNC --function $FUNC
+}
+
+function deploy_fission_b {
+  fission function create --name $FUNC --env fission-bin-env --code function
+  fission route create --method POST --url /$FUNC --function $FUNC
+}
+
+case "$1" in
+openfaas)
+    build_openfaas
+    ;;
+openwhisk)
+    build_openwhisk
+    ;;
+fission_c)
+    build_fission_container
+    ;;
+fission_b)
+    build_fission_bin
+    ;;
+push)
+    push
+    ;;
+deploy_openfaas)
+    deploy_openfaas
+    ;;
+deploy_openwhisk)
+    deploy_openwhisk
+    ;;
+deploy_fission_c)
+    deploy_fission_c
+    ;;
+deploy_fission_b)
+    deploy_fission_b
+    ;;
+esac
