974b130ea4aca3e62ad8e7ad80d3f3e9073fd179
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-25 23:31:47 -0500

    update

--- DeathStarBench/media_microservice_rust_lite_async/cluster-2/register-user/build.sh
+++ DeathStarBench/media_microservice_rust_lite_async/cluster-2/register-user/build.sh
@@ -2,14 +2,14 @@
 
 #ROOT_DIR=`realpath $(dirname $0)/..`
 ROOT_DIR=$(pwd)
-echo $ROOT_DIR
-FUNC=register-user
+DOCKERFILE_DIR=`realpath $(dirname $0)/../../../../dockerfiles`
+FUNC=$(basename $ROOT_DIR)
 
 function build_openfaas {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/mm-$FUNC-async:latest \
-        -f Dockerfile \
+    sudo docker build -t zyuxuan0115/sn-$FUNC-async:latest \
+        -f $DOCKERFILE_DIR/OpenFaaS/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
     rm -rf $ROOT_DIR/template/rust/DbInterface
@@ -18,36 +18,36 @@ function build_openfaas {
 
 function build_openwhisk {
     cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC-async:latest \
-        -f Dockerfile.wsk \
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC-async:latest \
+        -f $DOCKERFILE_DIR/OpenWhisk/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
-    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    rm -rf $ROOT_DIR/template/rust/DbInterface
     sudo docker system prune -f
 }
 
 function build_fission_container {
-    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC-async:latest \
-        -f Dockerfile.fs_container \
+    cp -r $ROOT_DIR/../../FissionRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC-async:latest \
+        -f $DOCKERFILE_DIR/Fission/container-based/rust/Dockerfile \
         $ROOT_DIR/template/rust
-    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
-    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    rm -rf $ROOT_DIR/template/rust/FissionRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface
     sudo docker system prune -f
 }
 
 function build_fission_bin {
-    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC-async:latest \
-        -f Dockerfile.fs_bin \
+    cp -r $ROOT_DIR/../../FissionRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC-async:latest \
+        -f $DOCKERFILE_DIR/Fission/binary-based/rust-bin/Dockerfile \
         $ROOT_DIR/template/rust
-    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
-    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    rm -rf $ROOT_DIR/template/rust/Fission
+    rm -rf $ROOT_DIR/template/rust/DbInterface
     sudo docker system prune -f
-    sudo docker create --name temp-container zyuxuan0115/mm-$FUNC-async:latest
+    sudo docker create --name temp-container zyuxuan0115/sn-$FUNC-async:latest
     sudo docker cp temp-container:/home/rust/function/target/release/function ./function_orig
     sudo docker rm temp-container
     echo $FUNC > metadata.txt
@@ -58,7 +58,7 @@ function build_fission_bin {
 }
 
 function push {
-  sudo docker push zyuxuan0115/mm-$FUNC-async:latest
+  sudo docker push zyuxuan0115/sn-$FUNC-async:latest
 }
 
 function deploy_openfaas {
@@ -66,12 +66,12 @@ function deploy_openfaas {
 }
 
 function deploy_openwhisk {
-  wsk action create $FUNC --docker zyuxuan0115/mm-$FUNC-async:latest    
+  wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC-async:latest    
 }
 
 function deploy_fission_c {
   fission function run-container --name $FUNC \
-    --image docker.io/zyuxuan0115/mm-$FUNC-async \
+    --image docker.io/zyuxuan0115/sn-$FUNC-async \
     --port 8888
   fission route create --method POST \
     --url /$FUNC --function $FUNC
