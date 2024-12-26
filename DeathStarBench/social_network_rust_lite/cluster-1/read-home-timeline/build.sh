a015bc9d754f6c346868577d62f1fb01b3203448
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-25 19:30:50 -0500

    update

--- DeathStarBench/social_network_rust_lite/cluster-1/read-home-timeline/build.sh
+++ DeathStarBench/social_network_rust_lite/cluster-1/read-home-timeline/build.sh
@@ -23,7 +23,7 @@ function build_openwhisk {
     cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
     sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
-        -f Dockerfile.wsk \
+        -f $DOCKERFILE_DIR/OpenWhisk/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
     rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
