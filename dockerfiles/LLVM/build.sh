7c169dec7066ae92536a0a3b13516e1a9ef3297e
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2024-12-26 14:33:38 -0500

    update

---
+++ dockerfiles/LLVM/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#ROOT_DIR=`realpath $(dirname $0)/..`
+ROOT_DIR=$(pwd)
+echo $ROOT_DIR
+
+ARGS=("$@")
+
+CALLER=${ARGS[1]}
+
+function build_llvm {
+  sudo docker build --no-cache -t zyuxuan0115/llvm-19:latest \
+       -f Dockerfile.llvm \
+       .
+  sudo docker push zyuxuan0115/llvm-19:latest
+}
+
+case "$1" in
+llvm)
+    build_llvm
+    ;;
+esac
