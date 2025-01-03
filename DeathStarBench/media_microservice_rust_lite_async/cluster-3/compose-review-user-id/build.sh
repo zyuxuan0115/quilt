4d64dd3fa96ae69823ea590158aafecc4b80e591
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-03 00:42:11 -0500

    update

--- DeathStarBench/media_microservice_rust_lite_async/cluster-3/compose-review-user-id/build.sh
+++ DeathStarBench/media_microservice_rust_lite_async/cluster-3/compose-review-user-id/build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#ROOT_DIR=`realpath $(dirname $0)/..`
-ROOT_DIR=$(pwd)
-echo $ROOT_DIR
-FUNC=compose-review-user-id
+ROOT_DIR=`realpath $(dirname $0)`
+DOCKERFILE_DIR=`realpath $(dirname $0)/../../../../dockerfiles`
+FUNC=$(basename $ROOT_DIR)
+PARENT_DIR=`realpath $(dirname $0)/..`
+CLUSTER_ID="${PARENT_DIR: -1}"
 
 function build_openfaas {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
     sudo docker build -t zyuxuan0115/mm-$FUNC-async:latest \
-        -f Dockerfile \
+        -f $DOCKERFILE_DIR/OpenFaaS/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
     rm -rf $ROOT_DIR/template/rust/DbInterface
@@ -18,34 +19,34 @@ function build_openfaas {
 
 function build_openwhisk {
     cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
     sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC-async:latest \
-        -f Dockerfile.wsk \
+        -f $DOCKERFILE_DIR/OpenWhisk/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
-    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    rm -rf $ROOT_DIR/template/rust/DbInterface
     sudo docker system prune -f
 }
 
 function build_fission_container {
-    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../FissionRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
     sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC-async:latest \
-        -f Dockerfile.fs_container \
+        -f $DOCKERFILE_DIR/Fission/container-based/rust/Dockerfile \
         $ROOT_DIR/template/rust
-    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
-    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    rm -rf $ROOT_DIR/template/rust/FissionRPC
+    rm -rf $ROOT_DIR/template/rust/DbInterface
     sudo docker system prune -f
 }
 
 function build_fission_bin {
-    cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
-    cp -r $ROOT_DIR/../../DbInterface_wsk $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../FissionRPC $ROOT_DIR/template/rust
+    cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
     sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC-async:latest \
-        -f Dockerfile.fs_bin \
+        -f $DOCKERFILE_DIR/Fission/binary-based/rust-bin/Dockerfile \
         $ROOT_DIR/template/rust
-    rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
-    rm -rf $ROOT_DIR/template/rust/DbInterface_wsk
+    rm -rf $ROOT_DIR/template/rust/Fission
+    rm -rf $ROOT_DIR/template/rust/DbInterface
     sudo docker system prune -f
     sudo docker create --name temp-container zyuxuan0115/mm-$FUNC-async:latest
     sudo docker cp temp-container:/home/rust/function/target/release/function ./function_orig
@@ -72,14 +73,44 @@ function deploy_openwhisk {
 function deploy_fission_c {
   fission function run-container --name $FUNC \
     --image docker.io/zyuxuan0115/mm-$FUNC-async \
-    --port 8888
-  fission route create --method POST \
-    --url /$FUNC --function $FUNC
+    --port 8888 \
+    --namespace fission-function
+  fission httptrigger create --method POST \
+    --url /$FUNC --function $FUNC \
+    --namespace fission-function
 }
 
 function deploy_fission_b {
-  fission function create --name $FUNC --env fission-bin-env --code function
-  fission route create --method POST --url /$FUNC --function $FUNC
+  fission function create --name $FUNC \
+    --env fission-bin-env \
+    --code function \
+    -n fission-function
+  fission httptrigger create --method POST \
+    --url /$FUNC --function $FUNC \
+    -n fission-function
+}
+
+function delete_openwhisk {
+  wsk action delete $FUNC 
+}
+
+function delete_openfaas {
+  if [ "$CLUSTER_ID" == "1" ]; then
+    faas-cli remove $FUNC
+  elif [ "$CLUSTER_ID" == "2" ]; then
+    faas-cli remove $FUNC --gateway=http://127.0.0.1:8081
+  elif [ "$CLUSTER_ID" == "3" ]; then
+    faas-cli remove $FUNC --gateway=http://127.0.0.1:8082
+  elif [ "$CLUSTER_ID" == "4" ]; then
+    faas-cli remove $FUNC --gateway=http://127.0.0.1:8083
+  fi
+}
+
+function delete_fission {
+  fission function delete --name $FUNC \
+    -n fission-function
+  fission httptrigger delete --function $FUNC \
+    -n fission-function
 }
 
 case "$1" in
@@ -109,5 +140,14 @@ deploy_fission_c)
     ;;
 deploy_fission_b)
     deploy_fission_b
+    ;;
+delete_openfaas)
+    delete_openfaas
+    ;;
+delete_openwhisk)
+    delete_openwhisk
+    ;;
+delete_fission)
+    delete_fission
     ;;
 esac
