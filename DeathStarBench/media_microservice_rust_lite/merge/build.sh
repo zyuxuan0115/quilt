0a02bbba8fc59c2d5f62168c96b248fdbfe67b95
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-07 12:17:14 -0500

    update

--- DeathStarBench/media_microservice_rust_lite/merge/build.sh
+++ DeathStarBench/media_microservice_rust_lite/merge/build.sh
@@ -3,14 +3,14 @@
 #ROOT_DIR=`realpath $(dirname $0)/..`
 ROOT_DIR=$(pwd)
 echo $ROOT_DIR
-
+DOCKERFILE_DIR=$ROOT_DIR/../../../dockerfiles/LLVM
 ARGS=("$@")
 
 CALLER=${ARGS[1]}
 
 function build_llvm {
   sudo docker build --no-cache -t zyuxuan0115/llvm-19:latest \
-       -f Dockerfile.llvm \
+       -f $DOCKERFILE_DIR/Dockerfile.llvm \
        .
   sudo docker push zyuxuan0115/llvm-19:latest
 }
@@ -25,26 +25,26 @@ function merge_openfaas {
   cp funcTree temp
   cp rm_redundant_bc.py temp 
 e sudo docker build --no-cache -t zyuxuan0115/mm-$CALLER-merged:latest \
-    -f Dockerfile \
+    -f $DOCKERFILE_DIR/Dockerfile \
     temp
   rm -rf temp
   sudo docker system prune -f
   sudo docker push zyuxuan0115/mm-$CALLER-merged:latest
 }
 
+
 function merge_openwhisk {
   rm -rf temp && mkdir temp
   ./build_helper.py ../OpenFaaSRPC/func_info.json funcTree
   cp -r ../OpenWhiskRPC temp
-  cp -r ../DbInterface_wsk temp
+  cp -r ../DbInterface temp
   mv temp/OpenWhiskRPC temp/OpenFaaSRPC
-  mv temp/DbInterface_wsk temp/DbInterface
   cp merge.sh temp
   cp merge_tree.py temp
   cp funcTree temp
   cp rm_redundant_bc.py temp
   sudo docker build --no-cache -t zyuxuan0115/mm-$CALLER-merged:latest \
-    -f Dockerfile.wsk \
+    -f $DOCKERFILE_DIR/Dockerfile.wsk \
     temp
   rm -rf temp
   sudo docker system prune -f
@@ -54,11 +54,33 @@ function merge_openwhisk {
   wsk action create $CALLER-merged --docker zyuxuan0115/mm-$CALLER-merged
 }
 
+
+function merge_fission {
+  rm -rf temp && mkdir temp
+  ./build_helper.py ../OpenFaaSRPC/func_info.json funcTree
+  cp -r ../FissionRPC temp
+  cp -r ../DbInterface temp
+  mv temp/OpenWhiskRPC temp/OpenFaaSRPC
+  cp merge.sh temp
+  cp merge_tree.py temp
+  cp funcTree temp
+  cp rm_redundant_bc.py temp
+  sudo docker build --no-cache -t zyuxuan0115/mm-$CALLER-merged:latest \
+    -f $DOCKERFILE_DIR/Dockerfile.fission \
+    temp
+  rm -rf temp
+  sudo docker system prune -f
+  sudo docker push zyuxuan0115/mm-$CALLER-merged:latest
+}
+
+
+
 function deploy_openwhisk {
   wsk action create page-service-merged --docker zyuxuan0115/mm-page-service-merged
   wsk action create compose-review-merged --docker zyuxuan0115/mm-compose-review-merged
   wsk action create read-user-review-merged --docker zyuxuan0115/mm-read-user-review-merged
 }
+
 
 case "$1" in
 llvm)
@@ -69,6 +91,9 @@ merge_openwhisk)
     ;;
 merge_openfaas)
     merge_openfaas 
+    ;;
+merge_fission)
+    merge_fission
     ;;
 deploy_openwhisk)
     deploy_openwhisk
