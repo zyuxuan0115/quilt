1feae2248b5bc46ad3f899622579c98568d83e80
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-08 21:47:21 -0500

    fission swift

---
+++ merge_func/merge-swift-and-c/caller/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function build_swift {
+  sudo docker build --no-cache -t zyuxuan0115/swift-caller:latest \
+       -f Dockerfile \
+       .
+  sudo docker push zyuxuan0115/swift-caller:latest
+}
+
+case "$1" in
+build)
+    build_swift
+    ;;
+esac
