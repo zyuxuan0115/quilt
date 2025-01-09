1a4e5c8a7f0211c9a124ab1827fcb5f8dca4fbaf
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-09 17:07:49 -0500

    update the script for merging swift and c function

--- merge_func/merge-swift-and-c/example/merge_script/merge.sh
+++ merge_func/merge-swift-and-c/example/merge_script/merge.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/bash
-cd ../caller 
-swiftc -parse-as-library -emit-ir -o caller.ll caller.swift 
-llc -filetype=obj -relocation-model=pic -o caller.o caller.ll
-clang -fPIC -L/proj/zyuxuanssf-PG0/zyuxuan/swift-6.0.3/usr/lib/swift/linux caller.o -o caller -lswiftCore -lswiftSwiftOnoneSupport -lswift_Concurrency -lswift_StringProcessing -lswift_RegexParser -lswiftGlibc -lBlocksRuntime -ldispatch -lswiftDispatch -lFoundation -lFoundationEssentials -lFoundationInternationalization -lFoundationNetworking 
-cd ../merge_script
+
+function compile {
+  swiftc -parse-as-library -emit-ir -o caller.ll ../caller/caller.swift
+  
+}
+
+function merge {
+  echo "merge is not implemented yet"
+}
+
+function link {
+  llc -filetype=obj -relocation-model=pic -o caller.o caller.ll
+  clang -fPIC -L/proj/zyuxuanssf-PG0/zyuxuan/swift-6.0.3/usr/lib/swift/linux caller.o -o caller -lswiftCore -lswiftSwiftOnoneSupport -lswift_Concurrency -lswift_StringProcessing -lswift_RegexParser -lswiftGlibc -lBlocksRuntime -ldispatch -lswiftDispatch -lFoundation -lFoundationEssentials -lFoundationInternationalization -lFoundationNetworking 
+}
+
+function build {
+  compile
+  merge
+  link
+}
+
+function clean {
+  rm caller
+  rm *.ll
+  rm *.o
+}
+
+case "$1" in
+merge)
+    build
+    ;;
+clean)
+    clean
+    ;;
+esac
