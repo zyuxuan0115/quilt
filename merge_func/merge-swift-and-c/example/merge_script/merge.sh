c06b62fe2139c5b3e06ad5db6d53ea541c47eabe
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-09 16:56:47 -0500

    update

--- merge_func/merge-swift-and-c/example/merge_script/merge.sh
+++ merge_func/merge-swift-and-c/example/merge_script/merge.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
-cd ../caller \
-  && swiftc -parse-as-library -emit-ir -o caller.ll caller.swift \
-  && cd ../merge_script
+cd ../caller 
+swiftc -parse-as-library -emit-ir -o caller.ll caller.swift 
+llc -filetype=obj -relocation-model=pic -o caller.o caller.ll
+clang -fPIC -L/proj/zyuxuanssf-PG0/zyuxuan/swift-6.0.3/usr/lib/swift/linux caller.o -o caller -lswiftCore -lswiftSwiftOnoneSupport -lswift_Concurrency -lswift_StringProcessing -lswift_RegexParser -lswiftGlibc -lBlocksRuntime -ldispatch -lswiftDispatch -lFoundation -lFoundationEssentials -lFoundationInternationalization -lFoundationNetworking 
+cd ../merge_script
