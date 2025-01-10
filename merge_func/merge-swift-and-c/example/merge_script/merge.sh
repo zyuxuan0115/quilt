52c96d6a62ce7a4e554fbdbc4fbeef1da42e7815
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-10 15:14:56 -0500

    update

--- merge_func/merge-swift-and-c/example/merge_script/merge.sh
+++ merge_func/merge-swift-and-c/example/merge_script/merge.sh
@@ -4,20 +4,20 @@ LLVM_DIR=/proj/zyuxuanssf-PG0/zyuxuan/llvm-project-17/build/bin
 
 function compile {
   swiftc -emit-ir -o caller.ll ../caller/caller.swift
-  $LLVM_DIR/clang++ -emit-llvm -S -o callee.ll ../callee/callee.cpp -std=c++17 
+  $LLVM_DIR/clang -emit-llvm -S -o callee.ll ../callee/callee.cpp -std=c++17
   swiftc -emit-ir -o wrapper.ll ../../wrapper/wrapper.swift
 }
 
 function merge {
   $LLVM_DIR/opt -passes=merge-swift-c -rename-callee-sc -S callee.ll -o callee_rename.ll
   $LLVM_DIR/opt -passes=merge-swift-c -rename-wrapper-sc -S wrapper.ll -o wrapper_rename.ll
-  $LLVM_DIR/llvm-link caller.ll callee_rename.ll wrapper_rename.ll -o caller_callee.ll
+  $LLVM_DIR/llvm-link caller.ll wrapper_rename.ll callee_rename.ll -o caller_callee.ll
   $LLVM_DIR/opt -passes=merge-swift-c -merge-callee-sc -S caller_callee.ll -o merged.ll 
 }
 
 function link {
-  llc -filetype=obj -relocation-model=pic -o merged.o merged.ll
-  $LLVM_DIR/clang -fPIC -L/proj/zyuxuanssf-PG0/zyuxuan/swift-6.0.3/usr/lib/swift/linux merged.o -o function -lswiftCore -lswiftSwiftOnoneSupport -lswift_Concurrency -lswift_StringProcessing -lswift_RegexParser -lswiftGlibc -lBlocksRuntime -ldispatch -lswiftDispatch -lFoundation -lFoundationEssentials -lFoundationInternationalization -lFoundationNetworking 
+  $LLVM_DIR/llc -filetype=obj -relocation-model=pic -o merged.o merged.ll
+  $LLVM_DIR/clang -fPIC -L/proj/zyuxuanssf-PG0/zyuxuan/swift-6.0.3/usr/lib/swift/linux merged.o -o function -lswiftCore -lswiftSwiftOnoneSupport -lswift_Concurrency -lswift_StringProcessing -lswift_RegexParser -lswiftGlibc -lBlocksRuntime -ldispatch -lswiftDispatch -lFoundation -lFoundationEssentials -lFoundationInternationalization -lFoundationNetworking -lstdc++ -lcrypto 
 }
 
 function build {
@@ -27,7 +27,7 @@ function build {
 }
 
 function clean {
-  rm caller
+  rm function
   rm *.ll
   rm *.o
 }
