e0b6a4b77d200a342462d2972cf02e8c733d8764
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-12 13:08:36 -0500

    update

--- merge_func/merge-c-and-swift/example/callee/build.sh
+++ merge_func/merge-c-and-swift/example/callee/build.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-function build {
-  sudo docker build --no-cache  -t zyuxuan0115/c-callee:latest -f Dockerfile . 
-  sudo docker push zyuxuan0115/c-callee:latest
+function build_swift {
+  sudo docker build --no-cache -t zyuxuan0115/swift-caller:latest \
+       -f Dockerfile \
+       .
+  sudo docker push zyuxuan0115/swift-caller:latest
+#  sudo docker system prune -f
 }
 
-function deploy {
-  fission function run-container --name c-callee \
-    --image docker.io/zyuxuan0115/c-callee:latest \
+function deploy_swift {
+  fission function run-container --name swift-caller \
+    --image docker.io/zyuxuan0115/swift-caller:latest \
     --port 8888 \
     --namespace fission-function
   fission httptrigger create --method POST \
-    --url /c-callee --function c-callee \
+    --url /swift-caller --function swift-caller \
     --namespace fission-function
 }
 
-function invoke {
-  curl -XPOST http://localhost:8888/c-callee \
-  -d '{"msg":""}'
+function invoke_swift {
+  curl -XPOST http://localhost:8888/swift-caller \
+  -d ''
 }
 
 case "$1" in
 build)
-    build
+    build_swift
     ;;
 deploy)
-    deploy
+    deploy_swift
     ;;
 invoke)
-    invoke
+    invoke_swift
     ;;
 esac
--- merge_func/merge-c-and-swift/example/caller/build.sh
+++ merge_func/merge-c-and-swift/example/caller/build.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
-function build_swift {
-  sudo docker build --no-cache -t zyuxuan0115/swift-caller:latest \
-       -f Dockerfile \
-       .
-  sudo docker push zyuxuan0115/swift-caller:latest
-#  sudo docker system prune -f
+function build {
+  sudo docker build --no-cache  -t zyuxuan0115/c-callee:latest -f Dockerfile . 
+  sudo docker push zyuxuan0115/c-callee:latest
 }
 
-function deploy_swift {
-  fission function run-container --name swift-caller \
-    --image docker.io/zyuxuan0115/swift-caller:latest \
+function deploy {
+  fission function run-container --name c-callee \
+    --image docker.io/zyuxuan0115/c-callee:latest \
     --port 8888 \
     --namespace fission-function
   fission httptrigger create --method POST \
-    --url /swift-caller --function swift-caller \
+    --url /c-callee --function c-callee \
     --namespace fission-function
 }
 
-function invoke_swift {
-  curl -XPOST http://localhost:8888/swift-caller \
-  -d ''
+function invoke {
+  curl -XPOST http://localhost:8888/c-callee \
+  -d '{"msg":""}'
 }
 
 case "$1" in
 build)
-    build_swift
+    build
     ;;
 deploy)
-    deploy_swift
+    deploy
     ;;
 invoke)
-    invoke_swift
+    invoke
     ;;
 esac
