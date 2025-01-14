ef3a334e9507d28deb37a2893c6e4a9fc45c5196
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-13 22:30:34 -0500

    update wrk scripts

--- test/wrk2_fission/social_network/test_qps.sh
+++ test/wrk2_fission/social_network/test_qps.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+ARGS=("$@")
+FUNC_NAME=${ARGS[1]}
+WRK_SCRIPT="lua_files/$FUNC_NAME.lua"
+WRK_BIN=../wrk
+DEATHSTARBENCH=/proj/zyuxuanssf-PG0/zyuxuan/faas-test/DeathStarBench
+SETUP_DIR=/proj/zyuxuanssf-PG0/zyuxuan/faas-test/setup
+TEST_DIR=/proj/zyuxuanssf-PG0/zyuxuan/faas-test/test
+WORKLOAD=social_network_rust_lite
+# You only need to change this line
+IP=$(kubectl get svc router -n fission -o jsonpath='{.spec.clusterIP}')
+PORT=$(kubectl get svc router -n fission -o jsonpath='{.spec.ports[0].nodePort}')
+ENTRY_HOST=http://$IP:$PORT
+QPS=1
+
+if [ "${ARGS[2]}" = "async" ]; then
+  WORKLOAD="${WORKLOAD}_async"
+fi
+
 if [ "$#" -lt 3 ]; then
   echo "Error: Missing command line argument."
   echo 'example: `./test_qps.sh perf compose-post async`'
   exit 1
 fi
 
-ARGS=("$@")
-FUNC_NAME=${ARGS[1]}
-WRK_SCRIPT="lua_files/$FUNC_NAME.lua"
-WRK_BIN=../wrk
-DEATHSTARBENCH=/proj/zyuxuanssf-PG0/faas-test/DeathStarBench
-OPENFAAS_TEST_DIR=/proj/zyuxuanssf-PG0/faas-test/openfaas-test
-WORKLOAD=social_network_rust_lite
-# You only need to change this line
-ENTRY_HOST=http://130.127.133.207:32001
-QPS=1
 
-if [ "${ARGS[2]}" = "async" ]; then
-  WORKLOAD="${WORKLOAD}_async"
-fi
 
 
 function measure_perf {
@@ -29,31 +34,31 @@ function measure_perf {
   rm -rf *.log
   for con in "${CON[@]}"; do
     echo $con
-    cd $OPENFAAS_TEST_DIR/setup/redis_memcached \
+    cd $SETUP_DIR/redis_memcached \
       && ./build.sh kill \
       && ./build.sh setup
     sleep 30
     init
     redeploy
     sleep 10
-    cd $OPENFAAS_TEST_DIR/wrk2_wsk/social_network
+    cd $TEST_DIR/wrk2_fission/social_network
     $WRK_BIN -t 1 -c $con -d 900 -L -U \
 	   -s $WRK_SCRIPT \
 	   $ENTRY_HOST -R $QPS 2>/dev/null > output_${ARGS[1]}-${ARGS[2]}_$con.log
     echo "===== Connections: $con ====="
     echo "connections: $con done"
     echo "============================"
-    cd $OPENFAAS_TEST_DIR/setup/openwhisk \
+    cd $SETUP_DIR/openwhisk \
       && ./build.sh kill \
       && ./build.sh setup
-    cd $OPENFAAS_TEST_DIR/wrk2_wsk/social_network
+    cd $TEST_DIR/wrk2_fission/social_network
   done
 }
 
 function run_wrk {
   sleep 10
   WRK_SCRIPT="lua_files/$1.lua"
-  $WRK_BIN -t 1 -c 3 -d 600 -L -U \
+  $WRK_BIN -t 1 -c 1 -d 90 -L -U \
 	   -s $WRK_SCRIPT \
 	   $ENTRY_HOST -R $QPS 2>/dev/null > output_$1.log
 }
@@ -67,7 +72,7 @@ function redeploy {
   cd $DEATHSTARBENCH/$WORKLOAD/cluster-1 && ./build.sh deploy_openwhisk
   cd $DEATHSTARBENCH/$WORKLOAD/cluster-2 && ./build.sh deploy_openwhisk
   cd $DEATHSTARBENCH/$WORKLOAD/merge && ./build.sh deploy_openwhisk
-  cd $OPENFAAS_TEST_DIR/wrk2_wsk/social_network
+  cd $OPENFAAS_TEST_DIR/wrk2_fission/social_network
 }
 
 
