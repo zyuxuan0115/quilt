49a9e8688398a6e25698e6e28af8d901d292eaf0
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-15 21:58:49 -0500

    add a simple test.sh for measuring performance

--- test/wrk2_fission/social_network/test.sh
+++ test/wrk2_fission/social_network/test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-IP=localhost
 WRK_BIN=../wrk
 WRK_SCRIPT="lua_files/$1.lua"
-ENTRY_HOST=http://$IP:9999
+IP=$(kubectl get svc router -n fission -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+PORT=$(kubectl get svc router -n fission -o jsonpath='{.spec.ports[0].nodePort}')
+ENTRY_HOST=http://$IP:$PORT
 
-QPS=300
 
-$WRK_BIN -t 1 -c 1 -d 60 -L -U \
+QPS=1000
+
+$WRK_BIN -t 1 -c 1 -d 90 -L -U \
 	 -s $WRK_SCRIPT \
 	 $ENTRY_HOST -R $QPS 2>/dev/null > output_$1.log
