1f577068696339207ac2e273b358bca107c5b034
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-21 23:52:29 -0500

    update

--- DeathStarBench/social_network_rust_lite_async/cluster-1/read-post/build.sh
+++ DeathStarBench/social_network_rust_lite_async/cluster-1/read-post/build.sh
@@ -9,7 +9,7 @@ CLUSTER_ID="${PARENT_DIR: -1}"
 function build_openfaas {
     cp -r $ROOT_DIR/../../OpenFaaSRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build -t zyuxuan0115/sn-$FUNC-async:latest \
+    sudo docker build -t zyuxuan0115/sn-$FUNC:latest \
         -f $DOCKERFILE_DIR/OpenFaaS/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenFaaSRPC
@@ -20,7 +20,7 @@ function build_openfaas {
 function build_openwhisk {
     cp -r $ROOT_DIR/../../OpenWhiskRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC-async:latest \
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
         -f $DOCKERFILE_DIR/OpenWhisk/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/OpenWhiskRPC
@@ -31,7 +31,7 @@ function build_openwhisk {
 function build_fission_container {
     cp -r $ROOT_DIR/../../FissionRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC-async:latest \
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
         -f $DOCKERFILE_DIR/Fission/container-based/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/FissionRPC
@@ -42,13 +42,13 @@ function build_fission_container {
 function build_fission_bin {
     cp -r $ROOT_DIR/../../FissionRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
-    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC-async:latest \
+    sudo docker build --no-cache -t zyuxuan0115/sn-$FUNC:latest \
         -f $DOCKERFILE_DIR/Fission/binary-based/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/FissionRPC
     rm -rf $ROOT_DIR/template/rust/DbInterface
     sudo docker system prune -f
-    sudo docker create --name temp-container zyuxuan0115/sn-$FUNC-async:latest
+    sudo docker create --name temp-container zyuxuan0115/sn-$FUNC:latest
     sudo docker cp temp-container:/home/rust/function/target/release/function ./function_orig
     sudo docker rm temp-container
     echo $FUNC > metadata.txt
@@ -59,7 +59,7 @@ function build_fission_bin {
 }
 
 function push {
-  sudo docker push zyuxuan0115/sn-$FUNC-async:latest
+  sudo docker push zyuxuan0115/sn-$FUNC:latest
 }
 
 function deploy_openfaas {
@@ -67,13 +67,42 @@ function deploy_openfaas {
 }
 
 function deploy_openwhisk {
-  wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC-async:latest    
+  wsk action create $FUNC --docker zyuxuan0115/sn-$FUNC:latest    
 }
 
 function deploy_fission_c {
+  FISSION_CONFIG=../../FissionRPC/fission.config
+  MINCPU=1
+  MAXCPU=1
+  MINMEM=1
+  MAXMEM=1
+  MINSCALE=1
+  MAXSCALE=1
+  while IFS= read -r line; do
+    first_word=$(echo "$line" | awk '{print $1}')
+    second_word=$(echo "$line" | awk '{print $2}')
+    if [[ "$first_word" == "MINCPU" ]]; then
+      MINCPU=$second_word
+    elif [[ "$first_word" == "MAXCPU" ]]; then
+      MAXCPU=$second_word
+    elif [[ "$first_word" == "MINMEM" ]]; then
+      MINMEM=$second_word
+    elif [[ "$first_word" == "MAXMEM" ]]; then
+      MAXMEM=$second_word
+    elif [[ "$first_word" == "MINSCALE" ]]; then
+      MINSCALE=$second_word
+    elif [[ "$first_word" == "MAXSCALE" ]]; then
+      MAXSCALE=$second_word
+    fi
+  done < "$FISSION_CONFIG"
+ 
+  echo $MINCPU $MAXCPU $MINMEM $MAXMEM $MINSCALE $MAXSCALE
+
   fission function run-container --name $FUNC \
-    --image docker.io/zyuxuan0115/sn-$FUNC-async \
-    --port 8888 \
+    --image docker.io/zyuxuan0115/sn-$FUNC --port 8888 \
+    --minscale=$MINSCALE --maxscale=$MAXSCALE \
+    --minmemory=$MINMEM --maxmemory=$MAXMEM \
+    --mincpu=$MINCPU  --maxcpu=$MAXCPU \
     --namespace fission-function
   fission httptrigger create --method POST \
     --url /$FUNC --function $FUNC \
