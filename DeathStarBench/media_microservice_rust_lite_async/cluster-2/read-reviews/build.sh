8a622879b134dfe14d7040a5c4fcc543d895cd32
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-23 14:46:35 -0500

    update

--- DeathStarBench/media_microservice_rust_lite_async/cluster-2/read-reviews/build.sh
+++ DeathStarBench/media_microservice_rust_lite_async/cluster-2/read-reviews/build.sh
@@ -31,11 +31,13 @@ function build_openwhisk {
 function build_fission_container {
     cp -r $ROOT_DIR/../../FissionRPC $ROOT_DIR/template/rust
     cp -r $ROOT_DIR/../../DbInterface $ROOT_DIR/template/rust
+    echo $FUNC > $ROOT_DIR/template/rust/metadata.txt
     sudo docker build --no-cache -t zyuxuan0115/mm-$FUNC-async:latest \
         -f $DOCKERFILE_DIR/Fission/container-based/rust/Dockerfile \
         $ROOT_DIR/template/rust
     rm -rf $ROOT_DIR/template/rust/FissionRPC
     rm -rf $ROOT_DIR/template/rust/DbInterface
+    rm -rf $ROOT_DIR/template/rust/metadata.txt
     sudo docker system prune -f
 }
 
