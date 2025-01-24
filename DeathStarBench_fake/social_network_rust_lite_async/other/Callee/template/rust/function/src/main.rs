a6164e70dca73eaa0fab12681439c9dc582df384
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-01-24 15:49:32 -0500

    update script

--- DeathStarBench_fake/social_network_rust_lite_async/other/Callee/template/rust/function/src/main.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
-use DbInterface::*;
-use std::time::{Duration, Instant};
-
-fn main() {
-  let input: String = get_arg_from_caller();
-  send_return_value_to_caller("I'm callee".to_string());
-}
