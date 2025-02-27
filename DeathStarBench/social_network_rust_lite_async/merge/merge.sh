cc1a31a9fc24b2de741e2c2d82e821fc33f86069
zyuxuan <zyuxuan@localhost>
2025-02-27 00:22:45 -0500

    update

--- DeathStarBench/social_network_rust_lite_async/merge/merge.sh
+++ DeathStarBench/social_network_rust_lite_async/merge/merge.sh
@@ -94,7 +94,9 @@ function wrap_shared_lib {
   && ./implib-gen.py $C_LIB/libcrypto.so.1.1 2>/dev/null \
   && gcc -c *.S && gcc -c *.c && rm *.S *.c \
   && ./implib-gen.py $C_LIB/libssl.so.1.1 2>/dev/null \
-  && gcc -c *.S && gcc -c *.c && rm *.S *.c 
+  && gcc -c *.S && gcc -c *.c && rm *.S *.c \
+  && ./implib-gen.py $C_LIB/libcurl.so.4.6.0 2>/dev/null \
+  && gcc -c *.S && gcc -c *.c && rm *.S *.c
 
   cd .. && cp Implib.so/*.o .  && rm -rf Implib.so
 }
