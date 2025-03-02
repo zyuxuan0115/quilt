b101b573d3d7e414400aabaeacb24b348775c9a1
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-03-02 16:35:12 -0500

    update merging algorithm

--- callgraph/merging_algorithm/main.cpp
+++ callgraph/merging_algorithm/main.cpp
@@ -6,7 +6,34 @@
 #include <vector>
 #include "Graph.h"
 
-int main(int argv, char** argc) {
-    
+using namespace std;
+
+int main(int argc, char** argv) {
+  // read edge info and resource info
+  if (argc != 3) {
+    printf("wrong number of arguments\n");
+    exit(0);
+  }
+
+
+  string edge_file(argv[1]);
+  string resource_file(argv[2]);
+  ifstream edge_f(edge_file);
+  
+  Graph G;
+  string line;
+
+  while (getline(edge_f, line)) {
+    istringstream iss(line);
+    vector<string> words;
+    string word; 
+    while (iss >> word) {
+      words.push_back(word);
+    }
+    G.addEdge(stoi(words[0]), stoi(words[1]));
+  }
+  
+  G.printGraph();
+
   return 0;
 }
