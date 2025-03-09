d241e7921382a9d9ac17a88bf393a9ce442bd3a8
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-03-09 14:53:03 -0400

    update

--- DeathStarBench_fakedb/social_network_rust_lite/cluster-1/read-post/template/rust/function/src/main.rs
+++ DeathStarBench_fakedb/social_network_rust_lite/cluster-1/read-post/template/rust/function/src/main.rs
@@ -5,13 +5,74 @@ use memcache::Client as memcached_client;
 use std::time::{Duration, Instant};
 use std::process;
 use redis::{Commands};
-
+use rand::{Rng, thread_rng, distributions::Alphanumeric};
+fn generate_random_string(length: usize) -> String {
+  thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(length)
+        .map(char::from)
+        .collect()
+}
+ 
 fn main() {
   let input: String = get_arg_from_caller();
 //  let time_0 = Instant::now();
   let input_args: ReadPostArgs = serde_json::from_str(&input).unwrap();
   let post_id = input_args.post_id;
 
+  let mut rng = rand::thread_rng();
+
+  let num_of_media = rng.gen_range(1..=5);
+  let mut medias: Vec<Media> = Vec::new();
+  for i in 0..num_of_media {
+    let new_media = Media {
+      media_type: generate_random_string(8),
+      media_id: rng.gen(),
+    };
+    medias.push(new_media);
+  }
+
+  let num_of_url = rng.gen_range(1..=5);
+  let mut urls: Vec<UrlPair> = Vec::new();
+  for i in 0..num_of_url {
+    let new_url = UrlPair {
+      shortened_url: generate_random_string(7),
+      expanded_url: generate_random_string(7),
+    };
+    urls.push(new_url);
+  }
+  
+  let num_of_usermention = rng.gen_range(1..=5);
+  let mut user_mentions: Vec<UserMention> = Vec::new();
+  for i in 0..num_of_usermention {
+    let user_id: i64 = rng.gen_range(1..=100);
+    let user_name = format!("username_{}",user_id);
+    let new_user_mention = UserMention {
+      user_id: user_id,
+      user_name: user_name,
+    };
+    user_mentions.push(new_user_mention);
+  }
+ 
+  let user_id: i64 = rng.gen_range(1..=100);
+  let username: String = format!("username_{}", user_id);
+  let creator = Creator {
+    user_id: user_id,
+    username: username,
+  };
+
+  let post = Post {
+    post_id: post_id,
+    creator: creator,
+    text: generate_random_string(20),
+    user_mentions: user_mentions,
+    media: medias,
+    urls: urls,
+    timestamp: rng.gen(),
+    post_type: PostType::POST,
+  };
+  let result_str = serde_json::to_string(&post).unwrap();
+/*
   let redis_uri = get_redis_rw_uri();
   let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
   let mut con = redis_client.get_connection().unwrap();
@@ -52,6 +113,7 @@ fn main() {
       process::exit(0);
     },
   };
+*/
 
 //  let time_1 = Instant::now();
 //  println!("{:?}", time_1.duration_since(time_0));
