a5b475713efdf8ccd1c06afcda77ede7feacaee4
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-03-10 15:08:00 -0400

    update

--- DeathStarBench_fakedb/media_microservice_rust_lite/cluster-2/store-review/template/rust/function/src/main.rs
+++ DeathStarBench_fakedb/media_microservice_rust_lite/cluster-2/store-review/template/rust/function/src/main.rs
@@ -1,13 +1,15 @@
 use OpenFaaSRPC::{make_rpc, get_arg_from_caller, send_return_value_to_caller,*};
 use DbInterface::*;
 use redis::Commands;
-//use std::time::{Duration, Instant};
+use std::time::{Duration, Instant};
+use std::thread;
 
 fn main() {
   let input: String = get_arg_from_caller();
 //  let now = Instant::now();
   let new_review: StoreReviewArgs = serde_json::from_str(&input).unwrap();
 
+/*
   let redis_uri = get_redis_rw_uri();
   let redis_client = redis::Client::open(&redis_uri[..]).unwrap();
   let mut con = redis_client.get_connection().unwrap();
@@ -21,6 +23,9 @@ fn main() {
   let _: isize = con.hset(&rid[..], "movie_id", &new_review.movie_id[..]).unwrap();
   let _: isize = con.hset(&rid[..], "rating", new_review.rating).unwrap();
   let _: isize = con.hset(&rid[..], "timestamp", new_review.timestamp).unwrap();
+ */
+
+  thread::sleep(Duration::from_millis(4)); 
 
 //  let new_now =  Instant::now();
 //  println!("{:?}", new_now.duration_since(now));
