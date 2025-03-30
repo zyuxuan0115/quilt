5e06104d22e7c703af66592e7e26c3510e11df28
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-03-29 20:00:57 -0400

    update

--- DeathStarBench_fakedb/hotel_reservation_rust_lite/cluster-2/reservation-handler/invoke.sh
+++ DeathStarBench_fakedb/hotel_reservation_rust_lite/cluster-2/reservation-handler/invoke.sh
@@ -18,11 +18,19 @@ function invoke_fission {
   -d '{"customer_name":"Yuxuan Zhang","username":"zyuxuan","password":"123456","hotel_id":"h1347232","in_date":"2024-09-01","out_date":"2024-09-05","room_number":5}'
 }
 
+function invoke_fission_merged {
+  curl -XPOST http://localhost:8888/$FUNC-merged \
+  -d '{"customer_name":"Yuxuan Zhang","username":"zyuxuan","password":"123456","hotel_id":"h1347232","in_date":"2024-09-01","out_date":"2024-09-05","room_number":5}'
+}
+
 case "$1" in
 wsk)
     invoke_wsk
     ;;
 fission)
     invoke_fission
+    ;;
+fission_merged)
+    invoke_fission_merged
     ;;
 esac
