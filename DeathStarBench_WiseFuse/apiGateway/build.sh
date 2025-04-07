7d06f35f7e8da3974211d1f400a42b3f54f984f8
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-04-06 22:45:20 -0400

    update

--- DeathStarBench_WiseFuse/apiGateway/build.sh
+++ DeathStarBench_WiseFuse/apiGateway/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FUNC=compose-post
+FUNC=$2
 
 function build {
   sudo docker build --no-cache  -t zyuxuan0115/wisefuse-$FUNC:latest -f Dockerfile . 
@@ -8,7 +8,7 @@ function build {
 }
 
 function deploy {
-  fission function run-container --name $FUNC \
+  fission function run-container --name wisefuse-$FUNC \
     --image docker.io/zyuxuan0115/wisefuse-$FUNC --port 8888 \
     --minscale=1 --maxscale=1 \
     --minmemory=1 --maxmemory=128 \
@@ -16,13 +16,13 @@ function deploy {
     --secret tracing \
     --namespace fission-function
   fission httptrigger create --method POST \
-    --url /$FUNC --function $FUNC \
+    --url /wisefuse-$FUNC --function wisefuse-$FUNC \
     --namespace fission-function
 }
 
 function invoke {
   curl -XPOST -d '{"username":"username_925","user_id":925,"text":"OgOYT3o2uQ2MbBWr9Eibvc2ytYvrku1Tu2KJ0ICfqzhNMeq5GB4XX8etJBhvASt7YA0s7phUa5NUzcDc6Wqxx4uQl9nZ1PV7RnIRydt79gIFgF9YCBken7tvuqFEzEZJzOso1UAhjoTQA7udVnIhyI6Ys9UYEsQUe7TWxN3CeW46a0U4pEVGjrmg2MskXXMGwvDCoXthm6KFBbZKp5k2Y5bPevqEC8tcUmqczKbZQVakZN6SfERL0eueSxVrtnmC @username_404 @username_53 http://ybES2VwfKxkarVFPKVEOZHzztyn3AkOqoZYN2eJSlgtUELcSpDPw1SUYLnRpolCb http://lB8Y1FLEe8xuWkjQ73tF46MAODLoPaZ3insxsY9cYFWMjAyouVnvJjDl3rOzO5j4 http://Wu1KzwlznpIZzQhS5NHPPpp1fH7OIHNpom5PXw6xMQMgi6toytK8fvQbfkddxBzF http://2awKmuRR9tzsY1urq27FgP5pbvq97OCMa4WkavrlyDtzdkNU7rhNPsGJr36HmP1z","media_ids":[7171256733,6485192397],"media_types":["png","png"],"post_type":"POST"}' \
-    http://localhost:8888/$FUNC
+    http://localhost:8888/wisefuse-$FUNC
 }
 
 
---
+++ test/wrk2_fission/media_microservice/lua_files/wisefuse-compose-review.lua
@@ -0,0 +1,76 @@
+--require "socket"
+--math.randomseed(socket.gettime()*1000)
+math.random(); math.random(); math.random()
+
+local charset = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
+  'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q',
+  'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H',
+  'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '1', '2', '3', '4', '5',
+  '6', '7', '8', '9', '0'}
+
+local decset = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+
+local function stringRandom(length)
+  if length > 0 then
+    return stringRandom(length - 1) .. charset[math.random(1, #charset)]
+  else
+    return ""
+  end
+end
+
+local function decRandom(length)
+  if length > 0 then
+    return decRandom(length - 1) .. decset[math.random(1, #decset-1)]
+  else
+    return ""
+  end
+end
+
+-- counter = 2000
+
+counter = 21000
+
+request = function(req_id)
+  counter = counter + 1
+  local req_id = counter
+  local user_index = math.random(1, 999)
+  local username = "username_" .. tostring(user_index)
+  local title = 'movie_' .. tostring(math.random(1,999))
+  local rating = math.random(1,5)
+  local text = stringRandom(100)
+
+  local method = "POST"
+  local path = "/wisefuse-compose-review"
+  local headers = {}
+  local body
+  headers["Content-Type"] = "application/json"
+
+  body = '{"req_id":' .. tostring(req_id) .. ',"title":"' .. title .. '","rating":'
+         .. rating .. ',"username":"' .. username .. '","password":"123456","text":"'
+         .. text .. '"}' 
+
+  local body_write = body .. '\n'
+  file = io.open('req_data_log_wisefuse-compose-review.txt', 'a')
+  file:write(body_write)
+  file:close()
+
+  if req_id ~= "" then
+    headers["Req-Id"] = req_id
+  end
+
+  return wrk.format(method, path, headers, body)
+end
+
+response = function(status, headers, body)
+--  if status ~= 200 then
+      io.write("------------------------------\n")
+      io.write("Response with status: ".. status .."\n")
+      io.write("------------------------------\n")
+      io.write("[response] Body:\n")
+      io.write(body .. "\n")
+--  end
+end
+
+function init(rand_seed)
+  math.randomseed(rand_seed)
+end
