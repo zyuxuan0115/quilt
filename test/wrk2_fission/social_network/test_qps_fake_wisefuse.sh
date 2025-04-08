175fce752ba14d3f789eae3f132c69adcb490ef0
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-04-08 14:52:41 -0400

    update

---
+++ test/wrk2_fission/social_network/test_qps_fake_wisefuse.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+ARGS=("$@")
+FUNC_NAME=${ARGS[1]}
+WRK_SCRIPT="lua_files/$FUNC_NAME.lua"
+WRK_BIN=../wrk
+DEATHSTARBENCH=/home/zyuxuan/faas-test/DeathStarBench_WiseFuse
+SETUP_DIR=/home/zyuxuan/faas-test/setup
+TEST_DIR=/home/zyuxuan/faas-test/test
+WORKLOAD=social_network_rust_lite
+# You only need to change this line
+QPS=1000
+
+if [ "${ARGS[2]}" = "async" ]; then
+  WORKLOAD="${WORKLOAD}_async"
+fi
+
+if [ "$#" -lt 3 ]; then
+  echo "Error: Missing command line argument."
+  echo 'example: `./test_qps.sh perf compose-post async`'
+  echo 'example: `./test_qps.sh init - -`'
+  exit 1
+fi
+
+
+
+
+function measure_perf {
+#  CON=(1 3 5 7 9 11 13 15 18 21 24 27 30 35 40 50 60 70 80 90 100 110)
+#  CON=(120 130 140 150 160 170 180 200)
+#   CON=(220 240 260 280 300 320 340 360 380 400)
+  CON=(1)
+  # Iterate over each element in the array
+  rm -rf *.log
+  for con in "${CON[@]}"; do
+    echo $con
+#    cd $SETUP_DIR/redis_memcached \
+#      && ./install.sh kill \
+#      && ./install.sh setup
+    redeploy
+    sleep 10
+    cd $TEST_DIR/wrk2_fission/social_network
+    IP=$(kubectl get svc router -n fission -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    PORT=$(kubectl get svc router -n fission -o jsonpath='{.spec.ports[0].nodePort}')
+    ENTRY_HOST=http://$IP:$PORT
+    $WRK_BIN -t 1 -c $con -d 600 -L -U \
+	   -s $WRK_SCRIPT \
+	   $ENTRY_HOST -R $QPS 2>/dev/null > output_${ARGS[1]}-${ARGS[2]}_$con.log
+    echo "===== Connections: $con ====="
+    echo "connections: $con done"
+    echo "============================"
+#    cd $SETUP_DIR/fission \
+#      && ./install.sh kill \
+#      && ./install.sh setup
+#    cd $TEST_DIR/wrk2_fission/social_network
+  done
+}
+
+function run_wrk {
+  sleep 10
+  IP=$(kubectl get svc router -n fission -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+  PORT=$(kubectl get svc router -n fission -o jsonpath='{.spec.ports[0].nodePort}')
+  ENTRY_HOST=http://$IP:$PORT
+  WRK_SCRIPT_0="lua_files/$1.lua"
+  $WRK_BIN -t 1 -c 1 -d $2 -L -U \
+	   -s $WRK_SCRIPT_0 \
+	   $ENTRY_HOST -R $QPS 2>/dev/null > output_$1.log
+}
+
+
+function redeploy {
+  cd $SETUP_DIR/fission \
+    && ./install.sh kill \
+    && ./install.sh setup
+  sleep 30
+  cd $DEATHSTARBENCH/apiGateway && ./build.sh deploy
+  cd $TEST_DIR/wrk2_fission/social_network
+}
+
+
+function init {
+  redeploy 
+  run_wrk register-user-with-id 60
+  run_wrk social-graph-follow-with-username 60
+  run_wrk compose-post 300
+}
+
+
+case "$1" in
+perf)
+    measure_perf
+    ;;
+init)
+    init
+    ;;
+esac
