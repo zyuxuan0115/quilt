42e5749e040eee5d748ebbdda8b1c9ed4b643324
Yuxuan Zhang <zyuxuan@seas.upenn.edu>
2025-08-29 15:39:51 -0400

    update

--- merge_func/merge-c-pthreads/example/merge_script/build.sh
+++ merge_func/merge-c-pthreads/example/merge_script/build.sh
@@ -7,32 +7,38 @@ DOCKERFILE_DIR=$ROOT_DIR/../../../dockerfiles/LLVM
 ARGS=("$@")
 
 CALLER=${ARGS[1]}
+FUNC=c-pthread-merged
 
 function merge {
   rm -rf temp && mkdir temp
   cp -r ../caller temp
   cp -r ../callee temp
   cp -r merge.sh temp
-  sudo docker build --no-cache -t zyuxuan0115/c-pthread-merged:latest \
+  sudo docker build --no-cache -t zyuxuan0115/$FUNC:latest \
     -f Dockerfile \
     temp
   rm -rf temp
   sudo docker system prune -f
-  sudo docker push zyuxuan0115/c-pthread-merged:latest
+  sudo docker push zyuxuan0115/$FUNC:latest
 }
 
 
 function deploy {
-  fission function run-container --name c-pthread-merged \
-      --image docker.io/zyuxuan0115/c-pthread-merged \
+  fission function run-container --name $FUNC \
+      --image docker.io/zyuxuan0115/$FUNC \
       --minscale=1 --maxscale=30 \
       --minmemory=1 --maxmemory=64 \
       --mincpu=1  --maxcpu=2000 \
       --port 8888 \
       --namespace fission-function
   fission httptrigger create --method POST \
-      --url /c-pthread-merged --function c-pthread-merged \
+      --url /$FUNC --function $FUNC \
       --namespace fission-function
+}
+
+function invoke {
+  curl -XPOST http://localhost:8888/$FUNC \
+  -d '{"iter_count":20}'
 }
 
 case "$1" in
@@ -41,5 +47,8 @@ merge)
     ;;
 deploy)
     deploy
+    ;;
+invoke)
+    invoke
     ;;
 esac
